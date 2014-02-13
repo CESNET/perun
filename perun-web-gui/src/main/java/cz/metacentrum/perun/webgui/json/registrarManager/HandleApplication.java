65ae779b2abc90c2e5899c82074a5305e2c297e6
Pavel Zlámal <256627@mail.muni.cz>
2014-02-13 13:14:53 +0100

    GUI: better message when approving application
    - Use better explanation when approving application and
      check finds similar users.
    - Source format.

--- perun-web-gui/src/main/java/cz/metacentrum/perun/webgui/json/registrarManager/HandleApplication.java
+++ perun-web-gui/src/main/java/cz/metacentrum/perun/webgui/json/registrarManager/HandleApplication.java
@@ -6,9 +6,11 @@ import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.json.client.JSONNumber;
 import com.google.gwt.json.client.JSONObject;
 import com.google.gwt.json.client.JSONString;
-import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.FlexTable;
+import cz.metacentrum.perun.webgui.client.PerunWebConstants;
 import cz.metacentrum.perun.webgui.client.PerunWebSession;
+import cz.metacentrum.perun.webgui.client.UiElements;
+import cz.metacentrum.perun.webgui.client.resources.Utils;
 import cz.metacentrum.perun.webgui.json.JsonCallbackEvents;
 import cz.metacentrum.perun.webgui.json.JsonPostClient;
 import cz.metacentrum.perun.webgui.json.JsonUtils;
@@ -19,7 +21,7 @@ import cz.metacentrum.perun.webgui.widgets.Confirm;
 import java.util.ArrayList;
 
 /**
- * Common request, which handles applications (VERIFY, REJECT, APPROVE)
+ * Common request, which handles applications (VERIFY, REJECT, APPROVE, DELETE)
  * 
  * @author Pavel Zlamal <256627@mail.muni.cz>
  */
@@ -33,7 +35,6 @@ public class HandleApplication {
 	final String JSON_URL_APPROVE = "registrarManager/approveApplication";
 	final String JSON_URL_REJECT = "registrarManager/rejectApplication";
     final String JSON_URL_DELETE = "registrarManager/deleteApplication";
-
 
     // custom events
 	private JsonCallbackEvents events = new JsonCallbackEvents();
@@ -62,28 +63,28 @@ public class HandleApplication {
 	 *
 	 * @param appId
 	 */
-	public void verifyApplication(int appId)
-	{
+	public void verifyApplication(int appId) {
 		
 		this.appId = appId;
 
 		// test arguments
-		if(!this.testCreating()){
+		if(!this.testApplication()){
 			return;
 		}
 
 		// new events
 		JsonCallbackEvents newEvents = new JsonCallbackEvents(){
-			public void onError(PerunError error) {
+			@Override
+            public void onError(PerunError error) {
 				session.getUiElements().setLogErrorText("Verifing application failed.");
 				events.onError(error);
 			};
-
+            @Override
 			public void onFinished(JavaScriptObject jso) {
 				session.getUiElements().setLogSuccessText("Application verified.");
 				events.onFinished(jso);
 			};
-
+            @Override
 			public void onLoadingStart() {
 				events.onLoadingStart();
 			};
@@ -92,8 +93,7 @@ public class HandleApplication {
 		// sending data
 		JsonPostClient jspc = new JsonPostClient(newEvents);
 		jspc.sendData(JSON_URL_VERIFY, prepareJSONObject());
-		
-		
+
 	}
 	
 	/**
@@ -102,14 +102,13 @@ public class HandleApplication {
 	 * @param appId
 	 * @param reason
 	 */
-	public void rejectApplication(int appId, String reason)
-	{
+	public void rejectApplication(int appId, String reason) {
 		
 		this.appId = appId;
 		this.reason = reason;
 
 		// test arguments
-		if(!this.testCreating()){
+		if(!this.testApplication()){
 			return;
 		}
 
@@ -133,8 +132,7 @@ public class HandleApplication {
 		// sending data
 		JsonPostClient jspc = new JsonPostClient(newEvents);
 		jspc.sendData(JSON_URL_REJECT, prepareJSONObject());
-		
-		
+
 	}
 	
 	/**
@@ -142,28 +140,28 @@ public class HandleApplication {
 	 *
 	 * @param appId
 	 */
-	public void approveApplication(int appId)
-	{
+	public void approveApplication(int appId) {
 		
 		this.appId = appId;
 
 		// test arguments
-		if(!this.testCreating()){
+		if(!this.testApplication()){
 			return;
 		}
 
         // new events
         final JsonCallbackEvents newEvents = new JsonCallbackEvents(){
+            @Override
             public void onError(PerunError error) {
                 session.getUiElements().setLogErrorText("Approving application failed.");
                 events.onError(error);
             };
-
+            @Override
             public void onFinished(JavaScriptObject jso) {
                 session.getUiElements().setLogSuccessText("Application approved.");
                 events.onFinished(jso);
             };
-
+            @Override
             public void onLoadingStart() {
                 events.onLoadingStart();
             };
@@ -175,11 +173,12 @@ public class HandleApplication {
         jso.put("appId", new JSONNumber(appId));
 
         JsonPostClient checkJspc = new JsonPostClient(new JsonCallbackEvents(){
+            @Override
             public void onError(PerunError error) {
                 session.getUiElements().setLogErrorText("Approving application failed.");
                 events.onError(error);
             };
-
+            @Override
             public void onFinished(JavaScriptObject jso) {
 
                 ArrayList<User> users = JsonUtils.jsoAsList(jso);
@@ -187,16 +186,20 @@ public class HandleApplication {
 
                     FlexTable ft = new FlexTable();
                     ft.setWidth("400px");
-                    ft.setHTML(0,0, "<p>Please check, if new applicant isn't the same person as listed below. " +
-                            "If so, please join his/hers identities before approving this application. If unsure, please contact <a href=\"mailto:perun@cesnet.cz\">support</a> to help you.");
+                    ft.setHTML(0, 0, "<p><strong>Following user(s) with similar name as new applicant were found in Perun system:");
 
                     for (int i=0; i<users.size(); i++) {
                         if (!users.get(i).isServiceUser()) {
-                            ft.setHTML(i+1, 0, " - "+users.get(i).getFullNameWithTitles() + " (ID: "+users.get(i).getId()+")");
+                            ft.setHTML(i+1, 0, users.get(i).getFullNameWithTitles() + " (User ID: "+users.get(i).getId()+")");
                         }
                     }
 
-                    ft.setHTML(ft.getRowCount(), 0, "<p><strong>Do you wish to approve application anyway ?</strong>");
+                    ft.setHTML(ft.getRowCount(), 0,  "<p>Please consider contacting new applicant with question, if he/she is already member of any other VO in Perun." +
+                            "<ul><li>If YES, ask user to join his identities before application approval at <a href=\""+Utils.getIdentityConsolidatorLink(false)+"\" target=\"_blank\">identity consolidator</a>"+
+                    "</li><li>If NO, you can approve this application. " +
+                            "</li><li>If UNSURE, contact <a href=\"mailto:"+ PerunWebConstants.INSTANCE.perunReportEmailAddress()+"\">support</a> to help you.</li></ul>");
+
+                    ft.setHTML(ft.getRowCount(), 0, "<strong>Do you wish to approve this application anyway ?</strong>");
 
                     Confirm c = new Confirm("Similar users found!", ft, new ClickHandler() {
                         @Override
@@ -225,7 +228,7 @@ public class HandleApplication {
                 }
 
             };
-
+            @Override
             public void onLoadingStart() {
                 events.onLoadingStart();
             };
@@ -234,68 +237,33 @@ public class HandleApplication {
 
 	}
 
-	private boolean testCreating() {
-		
-		boolean result = true;
-		String errorMsg = "";
-
-		if(appId == 0){
-			errorMsg += "AppId can't be 0.\n";
-			result = false;
-		}
-
-		if(errorMsg.length()>0){
-			Window.alert(errorMsg);
-		}
-
-		return result;
-		
-	}
-
-	/**
-	 * Prepares a JSON object.
-	 * @return JSONObject - the whole query
-	 */
-	private JSONObject prepareJSONObject()
-	{
-		// query
-		JSONObject query = new JSONObject();
-		query.put("id", new JSONNumber(appId));
-		
-		if (reason != null) {
-			query.put("reason", new JSONString(reason));
-		}
-		
-		return query;
-	}
-
     /**
      * Delete application
      *
      * @param appId
      */
-    public void deleteApplication(int appId)
-    {
+    public void deleteApplication(int appId) {
 
         this.appId = appId;
 
         // test arguments
-        if(!this.testCreating()){
+        if(!this.testApplication()){
             return;
         }
 
         // new events
         JsonCallbackEvents newEvents = new JsonCallbackEvents(){
+            @Override
             public void onError(PerunError error) {
                 session.getUiElements().setLogErrorText("Deleting application failed.");
                 events.onError(error);
             };
-
+            @Override
             public void onFinished(JavaScriptObject jso) {
                 session.getUiElements().setLogSuccessText("Application deleted.");
                 events.onFinished(jso);
             };
-
+            @Override
             public void onLoadingStart() {
                 events.onLoadingStart();
             };
@@ -305,7 +273,41 @@ public class HandleApplication {
         JsonPostClient jspc = new JsonPostClient(newEvents);
         jspc.sendData(JSON_URL_DELETE, prepareJSONObject());
 
+    }
 
+
+    private boolean testApplication() {
+
+        boolean result = true;
+        String errorMsg = "";
+
+        if(appId == 0){
+            errorMsg += "Application ID can't be 0.";
+            result = false;
+        }
+
+        if(errorMsg.length()>0){
+            UiElements.generateAlert("Parameter error", errorMsg);
+        }
+
+        return result;
+
+    }
+
+    /**
+     * Prepares a JSON object.
+     * @return JSONObject - the whole query
+     */
+    private JSONObject prepareJSONObject() {
+        // query
+        JSONObject query = new JSONObject();
+        query.put("id", new JSONNumber(appId));
+
+        if (reason != null) {
+            query.put("reason", new JSONString(reason));
+        }
+
+        return query;
     }
 
 }
