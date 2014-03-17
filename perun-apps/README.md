d80d3661ef7ac69341471c33a39a2e719aff57e9
Pavel Zlámal <256627@mail.muni.cz>
2014-03-17 14:41:32 +0100

    APPS: added mailinglist-manager mini app
    - Initial commit of perun-apps project with mailing-list manager app.
    - Added README.md with some info about apps.
    
    Common files for all miniapps will come later after some cleanup.

---
+++ perun-apps/README.md
@@ -0,0 +1,10 @@
+Perun mini applications
+====
+
+Mini applications are small one-purpose JS web apps. They are meant for end-users (members of VOs) to configure their settings in user-friendly way.
+Apps can be easily styled, so user can identify application directly with each VO without even knowing about Perun system.
+
+Apps structure
+====
+
+Common files used by all mini-apps are stored in /bootstrap /css /img and /js folders. If your app require own specific files, please put them as sub-folders of your miniapp. You can base you own mini-apps on example apps used for purpose of MetaCentrum VO (folder /metacentrum).
---
+++ perun-apps/metacentrum/js/mailinglist-manager.js
@@ -0,0 +1,72 @@
+// Empty initial perunSession object
+var voShortName = 'meta';
+
+$(document).ready(function(){
+
+     var serviceName = "mailman_meta";
+
+     // Get VO id
+     var vo = {};
+     callPerunSync("vosManager", "getVoByShortName", vo, { shortName : voShortName });
+     // Get member
+     var member = {};
+     callPerunSync("membersManager", "getMemberByUser", member, { vo : vo.id, user : user.id });
+     // Get service
+     var service = {};
+     callPerunSync("servicesManager", "getServiceByName", service, { name : serviceName });
+
+     // Fill basic info about the user
+     $("#user-firstName").text(user.firstName);
+     $("#user-lastName").text(user.lastName);
+     $("#user-organization").text(userAttributes.organization);
+     $("#user-preferredMail").text(userAttributes.preferredMail);
+     $("#user-phone").text(userAttributes.phone);
+     $("#user-preferredLanguage").text(userAttributes.preferredLanguage);
+     $("#user-login").text(userAttributes['login-namespace:einfra']);
+
+      $("#mailinglists-table").html(Configuration.LOADER_IMAGE);
+
+      var resourcesAll = {};
+      callPerunSync("resourcesManager", "getAssignedResources", resourcesAll, { member : member.id , service: service.id });
+
+      // Filter only mailingList resources
+      var resources = [];
+      for (resourceId in resourcesAll) {
+        //if (resourcesAll[resourceId].description.substring(0, 'mailing'.length) === 'mailing') {
+          var resource = resourcesAll[resourceId];
+          // Is member unsubscibed?
+          var attr = {};
+          callPerunSync("attributesManager", "getAttribute", attr, { member : member.id, resource : resource.id, attributeName : 'urn:perun:member_resource:attribute-def:def:optOutMailingList' });
+          if (attr.value == 'true') {
+            resource.actionUrl = '<a href="javascript:subscribe(' + member.id + ',' + resource.id + ')">Subscribe</a>';
+          } else {
+            resource.actionUrl = '<a href="javascript:unsubscribe(' + member.id + ',' + resource.id + ')">Unsubscribe</a>';
+          }
+          resources.push(resource);
+        //}
+      }
+
+      var table = PerunTable.create();
+      table.addColumn("description", "Name");
+      table.addColumn("actionUrl", "Operation");
+      table.add(resources);
+      var tableHtml = table.draw();
+      $("#mailinglists-table").html(tableHtml);    
+
+});
+
+function subscribe(memberId, resourceId) {
+  var attr = {};
+  callPerunSync("attributesManager", "getAttribute", attr, { member : memberId, resource : resourceId, attributeName : 'urn:perun:member_resource:attribute-def:def:optOutMailingList' });
+  attr.value = '';
+  callPerunSyncPost("attributesManager", "setAttribute", attr, { member : memberId, resource : resourceId, attribute : attr });
+  window.location.reload();
+}
+
+function unsubscribe(memberId, resourceId) {
+  var attr = {};
+  callPerunSync("attributesManager", "getAttribute", attr, { member : memberId, resource : resourceId, attributeName : 'urn:perun:member_resource:attribute-def:def:optOutMailingList' });
+  attr.value = 'true';
+  callPerunSyncPost("attributesManager", "setAttribute", attr, { member : memberId, resource : resourceId, attribute : attr })
+  window.location.reload();
+}
