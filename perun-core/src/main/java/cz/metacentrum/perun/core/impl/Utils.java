85dc77f5c949c203d42b7acd390b160989afbee8
Michal Vocu <michal.vocu@gmail.com>
2014-05-21 14:11:04 +0200

    avoid leaking open connection

--- perun-core/src/main/java/cz/metacentrum/perun/core/impl/Utils.java
+++ perun-core/src/main/java/cz/metacentrum/perun/core/impl/Utils.java
@@ -5,6 +5,7 @@ import java.math.BigInteger;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Timestamp;
