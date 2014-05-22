4dc0e41e93dcbb7eb7a2a9429b5ce00144089398
Michal Stava <stavamichal@gmail.com>
2014-05-22 10:14:57 +0200

    Add new version of script perun-afs_group.sh
    - fix removing and adding empty strings (when no user left)
    - fix listing users only on @meta

--- perun-services/slave/process-afs_group.sh
+++ perun-services/slave/process-afs_group.sh
@@ -12,40 +12,47 @@ function process {
 
 	create_lock
 
-
 	for GROUP_NAME in `ls $FROM_PERUN_DIR/` ; do
 
-		USERS_FROM_PERUN_FILE="$WORK_DIR/$GROUP_NAME"
+		USERS_FROM_PERUN_FILE="$FROM_PERUN_DIR/$GROUP_NAME"
 
-		pts membership -nameorid "$GROUP_NAME" | tail -n "+2" | sed -e 's/^\s*//' > "$AFS_GROUP_USERS_FILE" 2> "$TMP_ERROR_FILE"
+		pts membership -nameorid "$GROUP_NAME" | tail -n "+2" | sed -e 's/^\s*//' | grep "@meta$" > "$AFS_GROUP_USERS_FILE" 2> "$TMP_ERROR_FILE"
 		if [ -s "$TMP_ERROR_FILE" ]; then
-			MSG="Command failed: pts membership -nameorid $GROUP_NAME | tail -n +2 | sed -e 's/^\s*//' > $AFS_GROUP_USERS_FILE  Reason: `cat $TMP_ERROR_FILE`"
+			MSG="Command failed: pts membership -nameorid $GROUP_NAME | tail -n +2 | sed -e 's/^\s*//' | grep '@meta$' > $AFS_GROUP_USERS_FILE Reason: `cat $TMP_ERROR_FILE`"
 			echo "$MSG" >&2
-			logger -t "${NAME}" -p daemon.error "${SERVICE}: ${MSG}" &>/dev/null
-			ERROR=1
-			continue
+      logger -t "${NAME}" -p daemon.error "${SERVICE}: ${MSG}" &>/dev/null
+      ERROR=1
+      continue
 		fi
 
-		pts removeuser -user `grep -v -F -f "$USERS_FROM_PERUN_FILE" "$AFS_GROUP_USERS_FILE" ` -group "$GROUP_NAME"
-		if [ $? -ne 0 ]; then
-			MSG="Command failed: pts removeuser -user \`grep -v -F -f $USERS_FROM_PERUN_FILE $AFS_GROUP_USERS_FILE \` -group $GROUP_NAME"
-			echo "$MSG" >&2
-			logger -t "${NAME}" -p daemon.error "${SERVICE}: ${MSG}" &>/dev/null
-			ERROR=1
-			continue
+		USERS=`grep -v -F -f "$USERS_FROM_PERUN_FILE" "$AFS_GROUP_USERS_FILE"`
+		#if there are no users for removing, skip this part
+		if [ -n "$USERS" ]; then
+			pts removeuser -user $USERS -group $GROUP_NAME
+			if [ $? -ne 0 ]; then
+				MSG="Command failed: pts removeuser -user \`grep -v -F -f $USERS_FROM_PERUN_FILE $AFS_GROUP_USERS_FILE \` -group $GROUP_NAME"
+			  echo "$MSG" >&2
+			  logger -t "${NAME}" -p daemon.error "${SERVICE}: ${MSG}" &>/dev/null
+			  ERROR=1
+			  continue
+				fi
 		fi
 
-		pts adduser -user `grep -v -F -f "$AFS_GROUP_USERS_FILE" "$USERS_FROM_PERUN_FILE" ` -group "$GROUP_NAME"
-		if [ $? -ne 0 ]; then
-			MSG="Command failed: pts adduser -user \`grep -v -F -f $AFS_GROUP_USERS_FILE $USERS_FROM_PERUN_FILE \` -group $GROUP_NAME"
-			echo "$MSG" >&2
-			logger -t "${NAME}" -p daemon.error "${SERVICE}: ${MSG}" &>/dev/null
-			ERROR=1
-			continue
+		USERS=`grep -v -F -f "$AFS_GROUP_USERS_FILE" "$USERS_FROM_PERUN_FILE"`
+		#if there are no users for adding, skip this part
+		if [ -n "$USERS" ]; then
+			pts adduser -user $USERS -group $GROUP_NAME
+			if [ $? -ne 0 ]; then
+				MSG="Command failed: pts adduser -user \`grep -v -F -f $AFS_GROUP_USERS_FILE $USERS_FROM_PERUN_FILE \` -group $GROUP_NAME"
+			  echo "$MSG" >&2
+			  logger -t "${NAME}" -p daemon.error "${SERVICE}: ${MSG}" &>/dev/null
+			  ERROR=1
+			  continue
+				fi
 		fi
 
-	done
+done
 
-	[ $ERROR -eq 0 ] || log_msg E_FINISHED_WITH_ERRORS
+  [ $ERROR -eq 0 ] || log_msg E_FINISHED_WITH_ERRORS
 
 }
