bbf7aefa3ac178ffebdf62e1732b4108dc55b15a
Zora Sebestianova <zora@ics.muni.cz>
2014-08-22 11:38:42 +0200

    Tool copyForm allows to copy registrafion forms from VO to VO, from Group to Group, from VO to Group, from Group to VO

---
+++ perun-rpc/src/main/perl/Perun/RegistrarAgent.pm
@@ -0,0 +1,26 @@
+package Perun::RegistrarAgent;
+
+use strict;
+use warnings;
+
+use Perun::Common;
+
+my $manager = 'registrarManager';
+
+use fields qw(_agent _manager);
+
+sub new
+{
+	my $self = fields::new(shift);
+	$self->{_agent} = shift;
+	$self->{_manager} = $manager;
+
+	return $self;
+}
+
+sub copyForm
+{
+	return Perun::Common::callManagerMethod('copyForm', 'null', @_);
+}
+
+1;
