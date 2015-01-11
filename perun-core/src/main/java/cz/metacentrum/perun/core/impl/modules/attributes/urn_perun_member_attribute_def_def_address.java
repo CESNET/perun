fb1ed5a6a0c880ad7f7a3139e831fd305b8287bf
Simona Kruppova <simona.kruppova@gmail.com>
2015-01-11 13:09:04 +0100

    Attribute modules created
    - Attribute modules for phone, organization, address and workplace were created.
    - These modules set value of another attribute (their user variant), if the value in the user variant is null.

---
+++ perun-core/src/main/java/cz/metacentrum/perun/core/impl/modules/attributes/urn_perun_member_attribute_def_def_address.java
@@ -0,0 +1,56 @@
+package cz.metacentrum.perun.core.impl.modules.attributes;
+
+import cz.metacentrum.perun.core.api.Attribute;
+import cz.metacentrum.perun.core.api.AttributeDefinition;
+import cz.metacentrum.perun.core.api.AttributesManager;
+import cz.metacentrum.perun.core.api.Member;
+import cz.metacentrum.perun.core.api.User;
+import cz.metacentrum.perun.core.api.exceptions.AttributeNotExistsException;
+import cz.metacentrum.perun.core.api.exceptions.ConsistencyErrorException;
+import cz.metacentrum.perun.core.api.exceptions.InternalErrorException;
+import cz.metacentrum.perun.core.api.exceptions.WrongAttributeAssignmentException;
+import cz.metacentrum.perun.core.api.exceptions.WrongAttributeValueException;
+import cz.metacentrum.perun.core.api.exceptions.WrongReferenceAttributeValueException;
+import cz.metacentrum.perun.core.impl.PerunSessionImpl;
+import cz.metacentrum.perun.core.implApi.modules.attributes.MemberAttributesModuleAbstract;
+import cz.metacentrum.perun.core.implApi.modules.attributes.MemberAttributesModuleImplApi;
+
+/**
+ * @author Simona Kruppova
+ */
+public class urn_perun_member_attribute_def_def_address extends MemberAttributesModuleAbstract implements MemberAttributesModuleImplApi {
+
+	private static final String A_U_address = AttributesManager.NS_USER_ATTR_DEF + ":address";
+
+	@Override
+	public void changedAttributeHook(PerunSessionImpl session, Member member, Attribute attribute) throws InternalErrorException, WrongReferenceAttributeValueException {
+		User user = session.getPerunBl().getUsersManagerBl().getUserByMember(session, member);
+
+		if(attribute.getValue() != null) {
+			Attribute userAddress = null;
+			try {
+				userAddress = session.getPerunBl().getAttributesManagerBl().getAttribute(session, user, A_U_address);
+				if(userAddress.getValue() == null) {
+					userAddress.setValue(attribute.getValue());
+					session.getPerunBl().getAttributesManagerBl().setAttribute(session, user, userAddress);
+				}
+			} catch (WrongAttributeAssignmentException ex) {
+				throw new InternalErrorException(ex);
+			} catch (AttributeNotExistsException ex) {
+				throw new ConsistencyErrorException(ex);
+			} catch (WrongAttributeValueException ex) {
+				throw new WrongReferenceAttributeValueException(attribute, userAddress, "Mismatch in checking of member address and user address (different checking rules)", ex);
+			}
+		}
+	}
+
+	public AttributeDefinition getAttributeDefinition() {
+		AttributeDefinition attr = new AttributeDefinition();
+		attr.setNamespace(AttributesManager.NS_MEMBER_ATTR_DEF);
+		attr.setFriendlyName("address");
+		attr.setDisplayName("Address (for VO)");
+		attr.setType(String.class.getName());
+		attr.setDescription("Member's address in organization (can be different from user's address).");
+		return attr;
+	}
+}
---
+++ perun-core/src/main/java/cz/metacentrum/perun/core/impl/modules/attributes/urn_perun_member_attribute_def_def_organization.java
@@ -0,0 +1,56 @@
+package cz.metacentrum.perun.core.impl.modules.attributes;
+
+import cz.metacentrum.perun.core.api.Attribute;
+import cz.metacentrum.perun.core.api.AttributeDefinition;
+import cz.metacentrum.perun.core.api.AttributesManager;
+import cz.metacentrum.perun.core.api.Member;
+import cz.metacentrum.perun.core.api.User;
+import cz.metacentrum.perun.core.api.exceptions.AttributeNotExistsException;
+import cz.metacentrum.perun.core.api.exceptions.ConsistencyErrorException;
+import cz.metacentrum.perun.core.api.exceptions.InternalErrorException;
+import cz.metacentrum.perun.core.api.exceptions.WrongAttributeAssignmentException;
+import cz.metacentrum.perun.core.api.exceptions.WrongAttributeValueException;
+import cz.metacentrum.perun.core.api.exceptions.WrongReferenceAttributeValueException;
+import cz.metacentrum.perun.core.impl.PerunSessionImpl;
+import cz.metacentrum.perun.core.implApi.modules.attributes.MemberAttributesModuleAbstract;
+import cz.metacentrum.perun.core.implApi.modules.attributes.MemberAttributesModuleImplApi;
+
+/**
+ * @author Simona Kruppova
+ */
+public class urn_perun_member_attribute_def_def_organization extends MemberAttributesModuleAbstract implements MemberAttributesModuleImplApi {
+
+	private static final String A_U_organization = AttributesManager.NS_USER_ATTR_DEF + ":organization";
+
+	@Override
+	public void changedAttributeHook(PerunSessionImpl session, Member member, Attribute attribute) throws InternalErrorException, WrongReferenceAttributeValueException {
+		User user = session.getPerunBl().getUsersManagerBl().getUserByMember(session, member);
+
+		if(attribute.getValue() != null) {
+			Attribute userOrganization = null;
+			try {
+				userOrganization = session.getPerunBl().getAttributesManagerBl().getAttribute(session, user, A_U_organization);
+				if(userOrganization.getValue() == null) {
+					userOrganization.setValue(attribute.getValue());
+					session.getPerunBl().getAttributesManagerBl().setAttribute(session, user, userOrganization);
+				}
+			} catch (WrongAttributeAssignmentException ex) {
+				throw new InternalErrorException(ex);
+			} catch (AttributeNotExistsException ex) {
+				throw new ConsistencyErrorException(ex);
+			} catch (WrongAttributeValueException ex) {
+				throw new WrongReferenceAttributeValueException(attribute, userOrganization, "Mismatch in checking of member organization and user organization (different checking rules)", ex);
+			}
+		}
+	}
+
+	public AttributeDefinition getAttributeDefinition() {
+		AttributeDefinition attr = new AttributeDefinition();
+		attr.setNamespace(AttributesManager.NS_MEMBER_ATTR_DEF);
+		attr.setFriendlyName("organization");
+		attr.setDisplayName("Organization (for VO)");
+		attr.setType(String.class.getName());
+		attr.setDescription("Organization, from which user comes from.");
+		return attr;
+	}
+}
