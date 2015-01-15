47a31185fc57dfd0d0abeda7fa8670f3602ca3a0
Laliska <sona.mastrakova@gmail.com>
2015-01-15 01:56:54 +0000

    sshkeys_root and k5login_root - the way of creating destination file and setting file permissions has been changed
    if destination file doesn't exist, permissions are set on the file in working directory and then propagated by diff_mv on destination file
    
    if destination file exists, any changes are propagated by diff_mv from file in working directory to destination file
    
    all the changes are described in changelog

--- perun-services/slave/process-sshkeys_root.sh
+++ perun-services/slave/process-sshkeys_root.sh
@@ -9,6 +9,8 @@ function process {
 	### Status codes
 	I_CHANGED=(0 "${DST_FILE} updated")
 	I_NOT_CHANGED=(0 "${DST_FILE} has not changed")
+	E_CHOWN=(50 'Cannot chown on ${FROM_PERUN}')
+	E_CHMOD=(51 'Cannot chmod on ${FROM_PERUN}')
 
 	FROM_PERUN="${WORK_DIR}/sshkeys_root"
 
@@ -17,12 +19,16 @@ function process {
 
 	create_lock
 
+	# Destination file doesn't exist
+	if [ ! -f ${DST_FILE} ]; then
+		catch_error E_CHOWN chown root.root $FROM_PERUN
+		catch_error E_CHMOD chmod 0644 $FROM_PERUN
+	fi
+
 	# Create diff between old.perun and .new
 	diff_mv "${FROM_PERUN}" "${DST_FILE}"
 
 	if [ $? -eq 0 ]; then
-		chown root.root $DST_FILE
-		chmod 0644 $DST_FILE
 		log_msg I_CHANGED
 	else
 		log_msg I_NOT_CHANGED
