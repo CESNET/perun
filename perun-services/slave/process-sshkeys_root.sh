94a0706a611eb4b7de10579f416f08695baf40a0
Slavek Licehammer <slavek@ics.muni.cz>
2015-01-30 12:01:11 +0100

    slave sshkeys_root do not force concrete perun key
    sskeys_root do not add one concrete perun key to authorized keys.
    Instead it keeps all keys containing perun command in authorized keys.

--- perun-services/slave/process-sshkeys_root.sh
+++ perun-services/slave/process-sshkeys_root.sh
@@ -13,11 +13,10 @@ function process {
 	FROM_PERUN="${WORK_DIR}/sshkeys_root"
 
 	# Add perun ssh keys
-	echo -e "\nfrom=\"perun.ics.muni.cz\",command=\"/opt/perun/bin/perun\" ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC26+QiDtZ3bnLiLllySgsImSPUX0/sFBmo//3PmqOsuJIBdWB5BLU5Ws+pTRxefqC8SHfI92ZQoGXe7aJniTXxbRPa0FZJ3fskAHwpbiJfstGVZ1hddBcHIvial3v5Rd++zRiKslDVTkXLlb+b1pTnjyTVbD/6kGILgnUz7RKY5DnXADVnmTdPliQCabhE41AhkWdcuWpHBNwvxONKoZJJpbuouDbcviX4lJu9TF9Ij62rZjcoNzg5/JiIKTcMVi8L04FTjyCMxKRzlo00IjSuapFnXQNNZUL5u/mfPA/HpyIkSAOiPXLhWy9UuBNo7xdrCmfTh1qUvzbuWXJZN3d9 perunv3@perun.ics.muni.cz" >> $FROM_PERUN
+	grep "command=\"/opt/perun/bin/perun\" " "$DST_FILE" >> "$FROM_PERUN"
 
 	create_lock
 
-	# Create diff between old.perun and .new
 	diff_mv "${FROM_PERUN}" "${DST_FILE}"
 
 	if [ $? -eq 0 ]; then
