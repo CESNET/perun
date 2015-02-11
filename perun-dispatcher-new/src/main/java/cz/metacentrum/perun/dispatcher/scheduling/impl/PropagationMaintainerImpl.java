847b10a1a682cd15b7063f74505a389abac4db2c
Michal Vocu <michal.vocu@gmail.com>
2015-02-11 11:39:10 +0100

    fix compilation

--- perun-dispatcher-new/src/main/java/cz/metacentrum/perun/dispatcher/scheduling/impl/PropagationMaintainerImpl.java
+++ perun-dispatcher-new/src/main/java/cz/metacentrum/perun/dispatcher/scheduling/impl/PropagationMaintainerImpl.java
@@ -507,12 +507,8 @@ public class PropagationMaintainerImpl implements PropagationMaintainer {
 					log.error("Could not resolve destination from destination list");
 				}
 				if(completedTask.getDestinations() != null && 
-<<<<<<< HEAD
 				   !completedTask.getDestinations().isEmpty()) {
-=======
-				   !completedTask.getDestinations().empty()) {
->>>>>>> clean destination list on audit events for existing tasks, do not stick to failed destinations
-				        completedTask.setDestinations(destinationList);
+					completedTask.setDestinations(destinationList);
 				}
 			}
 			schedulingPool.setTaskStatus(completedTask, TaskStatus.ERROR);
