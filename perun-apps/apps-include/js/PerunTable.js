340ecdc5a62530cb74e4d4b2cc293c1aaae403bf
ondrej <ondrejvelisek@gmail.com>
2015-03-15 23:00:30 +0100

    apps: Add extend membership column

--- perun-apps/apps-include/js/PerunTable.js
+++ perun-apps/apps-include/js/PerunTable.js
@@ -111,8 +111,7 @@ function PerunTable() {
                         html += this.values[row][column.name].draw();
                         break;
                     case "date":
-                        var date = this.values[row][column.name].split("-");
-                        html += date[2]+"."+date[1]+"."+date[0];
+                        html += this.values[row][column.name].toLocaleDateString();
                         break;
                     default :
                         if (this.values.length == 0) {
@@ -155,17 +154,26 @@ function TableButton(id, name, title, type, action) {
     this.title = title;
     this.type = type;
     this.action = action;
+    this.disabled = false;
 
     this.html = function () {
         var onclick = "";
+        var disabled = "";
         if (this.action) {
-            onclick = 'onclick="' + this.action.fnc.name + '(' + this.action.params + ')"';
+            onclick = ' onclick="' + this.action.fnc.name + '(' + this.action.params + ')" ';
         }
-        var html = '<button ' + onclick + ' id="' + this.name + "-" + this.id + '" class="btn btn-' + this.type + '">' + this.title + '</button>';
+        if (this.disabled) {
+            disabled = ' disabled ';
+        }
+        var html = '<button ' + onclick + disabled + ' id="' + this.name + "-" + this.id + '" class="btn btn-' + this.type + '">' + this.title + '</button>';
         return html;
     };
 
     this.getAction = function () {
         return this.action;
     };
+
+    this.setDisabled = function(disabled) {
+        this.disabled = disabled;
+    }
 }
--- perun-apps/apps/user-profile/js/Projects.js
+++ perun-apps/apps/user-profile/js/Projects.js
@@ -14,22 +14,27 @@ $(document).ready(function() {
 function loadProjects(user) {
     var loadImage = new LoadImage($("#projects-table"), "auto");
     callPerun("membersManager", "getMembersByUser", {user: user.id}, function(members) {
+        members.push({id: members[0].id, voId: 1121});
         var projectsNum = members.length;
         var projectsCurrent = 0;
         var projects = [];
         for (var i in members) {
             var loadVos = callPerun("vosManager", "getVoById", {id: members[i].voId});
-            var loadAttrs = callPerun("attributesManager", "getAttribute",
+            var loadExpiration = callPerun("attributesManager", "getAttribute",
                 {member: members[i].id, attributeName: "urn:perun:member:attribute-def:def:membershipExpiration"});
-            $.when(loadVos, loadAttrs).done(success(members[i]));
+            var loadRules = callPerun("attributesManager", "getAttribute",
+                {vo: members[i].voId, attributeName: "urn:perun:vo:attribute-def:def:membershipExpirationRules"});
+            //var loadExtend = callPerun("membersManager", "canExtendMembership", {member: members[i].id} )
+            $.when(loadVos, loadExpiration, loadRules).done(success(members[i]));
         }
         function success(member) {
-            return function (vo, expiration) {
+            return function (vo, expiration, rules, extend) {
                 vo = vo[0];
-                expiration = expiration[0];
-                var project = {name: vo.name, expiration: expiration.value};
+                var expirationDate = new Date(expiration[0].value);
+                var rules = rules[0].value;
+                var stateBtn = stateButton(expirationDate, rules, member);
+                var project = {name: vo.name, expiration: expirationDate, state: stateBtn};
                 projects.push(project);
-                console.log({member:member, vo:vo, expiration:expiration});
                 projectsCurrent++;
                 if (projectsCurrent == projectsNum) {
                     loadImage.hide();
@@ -40,6 +45,29 @@ function loadProjects(user) {
     });
 }
 
+function stateButton(expiration, rules, member) {
+    if (expiration.getTime() < (new Date()).getTime()) {
+        action = new TableButton(member.id, "expired", "extend", "danger", {fnc: extend, params: [member.id]});
+        return action;
+    }
+    if (rules) {
+        var gracePeriod = rules.gracePeriod;
+        if (false) {
+            action = new TableButton(member.id, "gracePeriod", "extend", "warning", {fnc: extend, params: [member.id]});
+            return action;
+        }
+    }
+
+    action = new TableButton(member.id, "ok", "<i class='glyphicon glyphicon-ok'></i>", "success", {fnc: extend, params: [member.id]});
+    action.setDisabled(true);
+    return action;
+}
+
+
+
+function extend(member) {
+    alert("extend member " + member + ": unsupported yet");
+}
 
 function fillProjects(projects) {
     if (!projects) {
@@ -48,8 +76,9 @@ function fillProjects(projects) {
     }
     var projectTable = new PerunTable();
     projectTable.addColumn({type:"number", title:"#"});
-    projectTable.addColumn({type:"text", title:"Projects", name:"name"});
+    projectTable.addColumn({type:"text", title:"Virtual Organizations", name:"name"});
     projectTable.addColumn({type:"date", title:"Expiration", name:"expiration"});
+    projectTable.addColumn({type: "button2", title: "Extend", button: "state"});
     projectTable.setValues(projects);
     var tableHtml = projectTable.draw();
     $("#projects-table").html(tableHtml);
