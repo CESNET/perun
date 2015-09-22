558c23633658203236b12f76240e792ecc0a9ae0
Pavel Zlámal <zlamal@cesnet.cz>
2015-09-22 13:52:44 +0200

    GUI: Allow logins to start with underscore

--- perun-web-gui/src/main/java/cz/metacentrum/perun/webgui/client/resources/Utils.java
+++ perun-web-gui/src/main/java/cz/metacentrum/perun/webgui/client/resources/Utils.java
@@ -22,7 +22,7 @@ public class Utils {
 	public static final String GROUP_SHORT_NAME_MATCHER = "^[- a-zA-Z.0-9_]+$";
 	public static final String VO_SHORT_NAME_MATCHER = "^[-a-zA-Z0-9_.]+$";
 	public static final String ATTRIBUTE_FRIENDLY_NAME_MATCHER = "^[-a-zA-Z0-9.]+([:][-a-zA-Z0-9.]+)?$";
-	public static final String LOGIN_VALUE_MATCHER = "^[a-zA-Z0-9][-A-z0-9_.@/]*$";
+	public static final String LOGIN_VALUE_MATCHER = "^[a-zA-Z0-9_][-A-z0-9_.@/]*$";
 
 	/**
 	 * Return stripped string with ellipsis of custom length
