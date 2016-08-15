131d8db2ea4bc9367a125b070c98178a1b5c0490
balcirakpeter <peter.balcirak@gmail.com>
2016-08-15 12:57:56 +0200

    - Checkup for attribute module urn_perun_vo_attribute_def_def_fromEmail - extended to check also emails with header - New attribute module urn_perun_group_attribute_def_def_fromEmail added - which will checks emails same way as module above

--- perun-core/src/main/java/cz/metacentrum/perun/core/impl/modules/attributes/urn_perun_group_attribute_def_def_fromEmail.java
+++ perun-core/src/main/java/cz/metacentrum/perun/core/impl/modules/attributes/urn_perun_group_attribute_def_def_fromEmail.java
@@ -3,32 +3,32 @@ package cz.metacentrum.perun.core.impl.modules.attributes;
 import cz.metacentrum.perun.core.api.Attribute;
 import cz.metacentrum.perun.core.api.AttributeDefinition;
 import cz.metacentrum.perun.core.api.AttributesManager;
-import cz.metacentrum.perun.core.api.Vo;
+import cz.metacentrum.perun.core.api.Group;
 import cz.metacentrum.perun.core.api.exceptions.InternalErrorException;
 import cz.metacentrum.perun.core.api.exceptions.WrongAttributeAssignmentException;
 import cz.metacentrum.perun.core.api.exceptions.WrongAttributeValueException;
 import cz.metacentrum.perun.core.api.exceptions.WrongReferenceAttributeValueException;
 import cz.metacentrum.perun.core.impl.PerunSessionImpl;
-import cz.metacentrum.perun.core.implApi.modules.attributes.VoAttributesModuleAbstract;
-import cz.metacentrum.perun.core.implApi.modules.attributes.VoAttributesModuleImplApi;
+import cz.metacentrum.perun.core.implApi.modules.attributes.GroupAttributesModuleAbstract;
+import cz.metacentrum.perun.core.implApi.modules.attributes.GroupAttributesModuleImplApi;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
- * @author Jiří Mauritz
- * @edited Peter Balcirak <peter.balcirak@gmail.com>
+ * @author  Peter Balcirak <peter.balcirak@gmail.com>
+ * @date 15.08.2016
  */
-public class urn_perun_vo_attribute_def_def_fromEmail extends VoAttributesModuleAbstract implements VoAttributesModuleImplApi {
+public class urn_perun_group_attribute_def_def_fromEmail  extends GroupAttributesModuleAbstract implements GroupAttributesModuleImplApi {
 
 	private static final Pattern pattern = Pattern.compile("^\".+\" <.+>$");
 
 	@Override
-	public void checkAttributeValue(PerunSessionImpl sess, Vo vo, Attribute attribute) throws InternalErrorException, WrongAttributeValueException, WrongReferenceAttributeValueException, WrongAttributeAssignmentException {
+	public void checkAttributeValue(PerunSessionImpl sess, Group group, Attribute attribute) throws InternalErrorException, WrongAttributeValueException, WrongReferenceAttributeValueException, WrongAttributeAssignmentException {
 		String fromEmail = null;
 
 		// null attribute
-		if (attribute.getValue() == null) throw new WrongAttributeValueException(attribute, "Vo fromEmail cannot be null.");
+		if (attribute.getValue() == null) throw new WrongAttributeValueException(attribute, "Group fromEmail cannot be null.");
 
 		// wrong type of the attribute
 		if (!(attribute.getValue() instanceof String)) throw new WrongAttributeValueException(attribute, "Wrong type of the attribute. Expected: String");
@@ -39,14 +39,14 @@ public class urn_perun_vo_attribute_def_def_fromEmail extends VoAttributesModule
 
 			Matcher match = pattern.matcher(fromEmail);
 
-			if (!match.matches()) {
-				throw new WrongAttributeValueException(attribute, "Vo : " + vo.getName() + " has fromEmail " + fromEmail + " which is not valid. It has to be in form \"header\" <correct email> or just correct email.");
+    		if (!match.matches()) {
+				throw new WrongAttributeValueException(attribute, "Group : " + group.getName() + " has fromEmail " + fromEmail + " which is not valid. It has to be in form \"header\" <correct email> or just correct email.");
 			}else{
 
 				String[] emailParts = fromEmail.split("[<>]+");
 
 				if (!(sess.getPerunBl().getModulesUtilsBl().isNameOfEmailValid(sess, emailParts[1]))){
-					throw new WrongAttributeValueException(attribute, "Vo : " + vo.getName() +" has email in <> " + emailParts[1] +" which is not valid.");
+					throw new WrongAttributeValueException(attribute, "Group : " + group.getName() +" has email in <> " + emailParts[1] +" which is not valid.");
 				}
 			}
 		}
@@ -55,7 +55,7 @@ public class urn_perun_vo_attribute_def_def_fromEmail extends VoAttributesModule
 	@Override
 	public AttributeDefinition getAttributeDefinition() {
 		AttributeDefinition attr = new AttributeDefinition();
-		attr.setNamespace(AttributesManager.NS_VO_ATTR_DEF);
+		attr.setNamespace(AttributesManager.NS_GROUP_ATTR_DEF);
 		attr.setFriendlyName("fromEmail");
 		attr.setDisplayName("\"From\" email address");
 		attr.setType(String.class.getName());
