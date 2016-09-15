e15979b455d6aab2e288674aa29c1313c350b07b
Michal Stava <stavamichal@gmail.com>
2016-09-15 11:29:47 +0200

    Some changes for module optionalLogin-namespace:mu
     - change display name and description
     - rework getAttributeValue method to copy value to the correct
       attribute

--- perun-core/src/main/java/cz/metacentrum/perun/core/impl/modules/attributes/urn_perun_user_attribute_def_virt_optionalLogin_namespace_mu.java
+++ perun-core/src/main/java/cz/metacentrum/perun/core/impl/modules/attributes/urn_perun_user_attribute_def_virt_optionalLogin_namespace_mu.java
@@ -5,35 +5,42 @@ import cz.metacentrum.perun.core.api.AttributeDefinition;
 import cz.metacentrum.perun.core.api.AttributesManager;
 import cz.metacentrum.perun.core.api.User;
 import cz.metacentrum.perun.core.api.exceptions.AttributeNotExistsException;
-import cz.metacentrum.perun.core.api.exceptions.ConsistencyErrorException;
 import cz.metacentrum.perun.core.api.exceptions.InternalErrorException;
 import cz.metacentrum.perun.core.api.exceptions.WrongAttributeAssignmentException;
 import cz.metacentrum.perun.core.impl.PerunSessionImpl;
+import cz.metacentrum.perun.core.impl.Utils;
 import cz.metacentrum.perun.core.implApi.modules.attributes.UserVirtualAttributesModuleAbstract;
 
 /**
  * Attribute value depends on login-namespace:mu attribute
  *
- * @author Simona Kruppova
+ * @author Simona Kruppova, Michal Stava
  */
 public class urn_perun_user_attribute_def_virt_optionalLogin_namespace_mu extends UserVirtualAttributesModuleAbstract {
 
 	@Override
 	public Attribute getAttributeValue(PerunSessionImpl sess, User user, AttributeDefinition attributeDefinition) throws InternalErrorException {
+		Attribute attribute = new Attribute(attributeDefinition);
+
 		try {
-			return sess.getPerunBl().getAttributesManagerBl().getAttribute(sess, user, "urn:perun:user:attribute-def:def:login-namespace:mu");
-		} catch (WrongAttributeAssignmentException | AttributeNotExistsException e) {
-			throw new ConsistencyErrorException(e);
+			Attribute loginInMU = sess.getPerunBl().getAttributesManagerBl().getAttribute(sess, user, AttributesManager.NS_USER_ATTR_DEF + ":login-namespace:mu");
+			attribute = Utils.copyAttributeToVirtualAttributeWithValue(loginInMU, attribute);
+		} catch (AttributeNotExistsException ex) {
+			//That means that egi-ui attribute not exists at all, return empty attribute
+			return attribute;
+		} catch (WrongAttributeAssignmentException ex) {
+			throw new InternalErrorException(ex);
 		}
+		return attribute;
 	}
 
 	public AttributeDefinition getAttributeDefinition() {
 		AttributeDefinition attr = new AttributeDefinition();
 		attr.setNamespace(AttributesManager.NS_USER_ATTR_VIRT);
 		attr.setFriendlyName("optionalLogin-namespace:mu");
-		attr.setDisplayName("Optional MU login");
+		attr.setDisplayName("MU login (if available)");
 		attr.setType(String.class.getName());
-		attr.setDescription("Optional Masaryk university login");
+		attr.setDescription("Masaryk University login (if available)");
 		return attr;
 	}
 }
