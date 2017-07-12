f3d3cf49b3e4e9a6768153162cfc3db328607b81
Martin Kuba <makub@ics.muni.cz>
2017-07-12 14:53:16 +0200

    implemented eduPersonORCID virtual attribute

---
+++ perun-core/src/main/java/cz/metacentrum/perun/core/impl/modules/attributes/urn_perun_user_attribute_def_virt_eduPersonORCID.java
@@ -0,0 +1,38 @@
+package cz.metacentrum.perun.core.impl.modules.attributes;
+
+import cz.metacentrum.perun.core.implApi.modules.attributes.UserVirtualAttributeCollectedFromUserExtSource;
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * ORCIDs collected from UserExtSources attributes.
+ *
+ * @author Martin Kuba makub@ics.muni.cz
+ */
+@SuppressWarnings("unused")
+public class urn_perun_user_attribute_def_virt_eduPersonORCID extends UserVirtualAttributeCollectedFromUserExtSource {
+
+	@Override
+	public String getSourceAttributeFriendlyName() {
+		return "eppn";
+	}
+
+	@Override
+	public String getDestinationAttributeFriendlyName() {
+		return "eduPersonORCID";
+	}
+
+	@Override
+	public String getDestinationAttributeDescription() {
+		return "All ORCIDs of a user";
+	}
+
+	@Override
+	public String modifyValue(String value) {
+		if(value.endsWith("@orcid")) {
+			return "http://orcid.org/"+StringUtils.substringBefore(value,"@");
+		} else {
+			return null;
+		}
+
+	}
+}
--- perun-core/src/main/java/cz/metacentrum/perun/core/impl/modules/attributes/urn_perun_user_attribute_def_virt_eduPersonScopedAffiliations.java
+++ perun-core/src/main/java/cz/metacentrum/perun/core/impl/modules/attributes/urn_perun_user_attribute_def_virt_eduPersonScopedAffiliations.java
@@ -1,68 +1,23 @@
 package cz.metacentrum.perun.core.impl.modules.attributes;
 
-import cz.metacentrum.perun.core.api.*;
-import cz.metacentrum.perun.core.api.exceptions.AttributeNotExistsException;
-import cz.metacentrum.perun.core.api.exceptions.InternalErrorException;
-import cz.metacentrum.perun.core.api.exceptions.WrongAttributeAssignmentException;
-import cz.metacentrum.perun.core.bl.AttributesManagerBl;
-import cz.metacentrum.perun.core.impl.PerunSessionImpl;
-import cz.metacentrum.perun.core.implApi.modules.attributes.UserVirtualAttributesModuleAbstract;
-import cz.metacentrum.perun.core.implApi.modules.attributes.UserVirtualAttributesModuleImplApi;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import cz.metacentrum.perun.core.implApi.modules.attributes.UserVirtualAttributeCollectedFromUserExtSource;
 
 /**
  * All affiliations collected from UserExtSources attributes.
- * Afilliation is a multi-valued attribute, but Apache joins multiple values using ';',
- * thus we must split it up here.
  *
  * @author Martin Kuba makub@ics.muni.cz
  */
-public class urn_perun_user_attribute_def_virt_eduPersonScopedAffiliations extends UserVirtualAttributesModuleAbstract implements UserVirtualAttributesModuleImplApi {
-
-	private final static Logger log = LoggerFactory.getLogger(urn_perun_user_attribute_def_virt_eduPersonScopedAffiliations.class);
+@SuppressWarnings("unused")
+public class urn_perun_user_attribute_def_virt_eduPersonScopedAffiliations extends UserVirtualAttributeCollectedFromUserExtSource {
 
 	@Override
-	public Attribute getAttributeValue(PerunSessionImpl sess, User user, AttributeDefinition attributeDefinition) throws InternalErrorException {
-
-		Attribute attribute = new Attribute(attributeDefinition);
-		List<String> values = new ArrayList<>();
-
-		List<UserExtSource> userExtSources = sess.getPerunBl().getUsersManagerBl().getUserExtSources(sess, user);
-		AttributesManagerBl am = sess.getPerunBl().getAttributesManagerBl();
-		for (UserExtSource userExtSource : userExtSources) {
-			try {
-				Attribute a = am.getAttribute(sess, userExtSource, "urn:perun:ues:attribute-def:def:affiliation");
-				Object value = a.getValue();
-				if(value!=null && value instanceof String) {
-					String affiliation = (String) value;
-					if(affiliation.contains(";")) {
-						//multiple values
-						values.addAll(Arrays.asList(affiliation.split(";")));
-					} else {
-						//just one value
-						values.add(affiliation);
-					}
-				}
-			} catch (WrongAttributeAssignmentException | AttributeNotExistsException e) {
-				log.error("cannot read affiliation from userExtSource "+userExtSource.getId()+" of user "+user.getId(),e);
-			}
-		}
-		attribute.setValue(values);
-		return attribute;
+	public String getSourceAttributeFriendlyName() {
+		return "affiliation";
 	}
 
-	public AttributeDefinition getAttributeDefinition() {
-		AttributeDefinition attr = new AttributeDefinition();
-		attr.setNamespace(AttributesManager.NS_USER_ATTR_VIRT);
-		attr.setFriendlyName("eduPersonScopedAffiliations");
-		attr.setDisplayName("eduPersonScopedAffiliations");
-		attr.setType(ArrayList.class.getName());
-		attr.setDescription("All affiliations of a user");
-		return attr;
+	@Override
+	public String getDestinationAttributeFriendlyName() {
+		return "eduPersonScopedAffiliations";
 	}
+
 }
