0094e12f1111ad971d6a1ded1189ac1cb150d258
Pavel Zlámal <zlamal@cesnet.cz>
2017-08-02 08:30:16 +0200

    Added attribute modules for west-life proxy IdP
    - Added persistent and persistent-shadow login-namespace
      modules for west-life.

---
+++ perun-core/src/main/java/cz/metacentrum/perun/core/impl/modules/attributes/urn_perun_user_attribute_def_def_login_namespace_westlife_persistent_shadow.java
@@ -0,0 +1,97 @@
+package cz.metacentrum.perun.core.impl.modules.attributes;
+
+import cz.metacentrum.perun.core.api.Attribute;
+import cz.metacentrum.perun.core.api.AttributeDefinition;
+import cz.metacentrum.perun.core.api.AttributesManager;
+import cz.metacentrum.perun.core.api.ExtSource;
+import cz.metacentrum.perun.core.api.User;
+import cz.metacentrum.perun.core.api.UserExtSource;
+import cz.metacentrum.perun.core.api.exceptions.ExtSourceNotExistsException;
+import cz.metacentrum.perun.core.api.exceptions.InternalErrorException;
+import cz.metacentrum.perun.core.api.exceptions.UserExtSourceExistsException;
+import cz.metacentrum.perun.core.api.exceptions.WrongAttributeAssignmentException;
+import cz.metacentrum.perun.core.api.exceptions.WrongReferenceAttributeValueException;
+import cz.metacentrum.perun.core.impl.PerunSessionImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Class for checking logins uniqueness in the namespace and filling westlife-persistent id.
+ * It is only storage! Use module login westlife_persistent for access the value.
+ *
+ * @author Pavel Zlámal <zlamal@cesnet.cz>
+ */
+public class urn_perun_user_attribute_def_def_login_namespace_westlife_persistent_shadow extends urn_perun_user_attribute_def_def_login_namespace {
+
+	private final static Logger log = LoggerFactory.getLogger(urn_perun_user_attribute_def_def_login_namespace_westlife_persistent_shadow.class);
+	private final static String extSourceNameWestlife = "https://auth.west-life.eu/proxy/saml2/idp/metadata.php";
+	private final static String domainNameWestlife = "@west-life.eu";
+	private final static String attrNameWestlife = "login-namespace:westlife-persistent-shadow";
+
+	/**
+	 * Filling implemented for login:namespace:westlife-persistent attribute
+	 * Format is: "[hash]@west-life.eu" where [hash] represents sha1hash counted from user's id and perun instance id a login-namespace name
+	 *
+	 * @param perunSession PerunSession
+	 * @param user User to fill attribute for
+	 * @param attribute Attribute to fill value to
+	 * @return Filled attribute
+	 * @throws InternalErrorException
+	 * @throws WrongAttributeAssignmentException
+	 */
+	@Override
+	public Attribute fillAttribute(PerunSessionImpl perunSession, User user, AttributeDefinition attribute) throws InternalErrorException, WrongAttributeAssignmentException {
+
+		Attribute filledAttribute = new Attribute(attribute);
+
+		if (attribute.getFriendlyName().equals(attrNameWestlife)) {
+			filledAttribute.setValue(sha1HashCount(user, domainNameWestlife).toString() + domainNameWestlife);
+			return filledAttribute;
+		} else {
+			// without value
+			return filledAttribute;
+		}
+	}
+
+	/**
+	 * ChangedAttributeHook() sets UserExtSource with following properties:
+	 *  - extSourceType is IdP
+	 *  - extSourceName is https://auth.west-life.eu/proxy/saml2/idp/metadata.php
+	 *  - user's extSource login is the same as his westlife-persistent attribute
+	 *
+	 * @param session PerunSession
+	 * @param user User to set UserExtSource for
+	 * @param attribute Attribute containing westlifeID
+	 * @throws InternalErrorException
+	 * @throws WrongReferenceAttributeValueException
+	 */
+	@Override
+	public void changedAttributeHook(PerunSessionImpl session, User user, Attribute attribute) throws InternalErrorException, WrongReferenceAttributeValueException {
+		try {
+			String userNamespace = attribute.getFriendlyNameParameter();
+
+			if(userNamespace.equals("westlife-persistent-shadow") && attribute.getValue() != null){
+				ExtSource extSource = session.getPerunBl().getExtSourcesManagerBl().getExtSourceByName(session, extSourceNameWestlife);
+				UserExtSource userExtSource = new UserExtSource(extSource, 0, attribute.getValue().toString());
+
+				session.getPerunBl().getUsersManagerBl().addUserExtSource(session, user, userExtSource);
+			}
+		} catch (UserExtSourceExistsException ex) {
+			log.warn("Westlife IdP external source already exists for the user.", ex);
+		} catch (ExtSourceNotExistsException ex) {
+			throw new InternalErrorException("IdP external source for Westlife doesn't exist.", ex);
+		}
+	}
+
+	public AttributeDefinition getAttributeDefinition() {
+		AttributeDefinition attr = new AttributeDefinition();
+		attr.setNamespace(AttributesManager.NS_USER_ATTR_DEF);
+		attr.setFriendlyName("login-namespace:westlife-persistent-shadow");
+		attr.setDisplayName("WEST-LIFE login");
+		attr.setType(String.class.getName());
+		attr.setDescription("Login to West-life. Do not use it directly! " +
+				"Use \"user:virt:login-namespace:westlife-persistent\" attribute instead.");
+		return attr;
+	}
+
+}
