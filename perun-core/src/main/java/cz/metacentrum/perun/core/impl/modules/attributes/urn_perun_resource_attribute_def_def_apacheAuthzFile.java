4e672f614a7cb62c7a6706bfab163095d2a40abe
Vladimír Mečko <vladimir.mecko@gmail.com>
2017-08-14 16:57:38 +0200

    Created attribute module for apacheAuthzFile attribute
    Created attribute module which checks if attribute is non-empty
    and if it contains unix-like file path.

---
+++ perun-core/src/main/java/cz/metacentrum/perun/core/impl/modules/attributes/urn_perun_resource_attribute_def_def_apacheAuthzFile.java
@@ -0,0 +1,51 @@
+package cz.metacentrum.perun.core.impl.modules.attributes;
+
+import cz.metacentrum.perun.core.api.Attribute;
+import cz.metacentrum.perun.core.api.AttributeDefinition;
+import cz.metacentrum.perun.core.api.AttributesManager;
+import cz.metacentrum.perun.core.api.Resource;
+import cz.metacentrum.perun.core.api.exceptions.InternalErrorException;
+import cz.metacentrum.perun.core.api.exceptions.WrongAttributeAssignmentException;
+import cz.metacentrum.perun.core.api.exceptions.WrongAttributeValueException;
+import cz.metacentrum.perun.core.api.exceptions.WrongReferenceAttributeValueException;
+import cz.metacentrum.perun.core.impl.PerunSessionImpl;
+import cz.metacentrum.perun.core.implApi.modules.attributes.ResourceAttributesModuleAbstract;
+import cz.metacentrum.perun.core.implApi.modules.attributes.ResourceAttributesModuleImplApi;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Attribute module for apacheAuthzFile attribute. Module checks that
+ * attribute is not empty and it also contains unix-like file path.
+ *
+ * @author Vladimir Mecko vladimir.mecko@gmail.com
+ */
+public class urn_perun_resource_attribute_def_def_apacheAuthzFile extends ResourceAttributesModuleAbstract implements ResourceAttributesModuleImplApi {
+	private Pattern pattern = Pattern.compile("^(/[-_a-zA-Z0-9]+)+$");
+
+	@Override
+	public void checkAttributeValue(PerunSessionImpl perunSession, Resource resource, Attribute attribute) throws InternalErrorException, WrongAttributeValueException, WrongReferenceAttributeValueException, WrongAttributeAssignmentException {
+		if (attribute.getValue() == null) {
+			throw new WrongAttributeValueException(attribute, resource, null, "Attribute value can not be null.");
+		}
+
+		//checks for valid unix file path in attribute based on pattern
+		Matcher match = pattern.matcher((String) attribute.getValue());
+
+		if(!match.matches()) {
+			throw new WrongAttributeValueException(attribute, resource, null, "Wrong file path format in attribute (should be like '/dir1/dir2').");
+		}
+	}
+
+	@Override
+	public AttributeDefinition getAttributeDefinition() {
+		AttributeDefinition attr = new AttributeDefinition();
+		attr.setNamespace(AttributesManager.NS_RESOURCE_ATTR_DEF);
+		attr.setFriendlyName("apacheAuthzFile");
+		attr.setDisplayName("Apache authz file");
+		attr.setType(String.class.getName());
+		attr.setDescription("File containing authz entries for Apache.");
+		return attr;
+	}
+}
