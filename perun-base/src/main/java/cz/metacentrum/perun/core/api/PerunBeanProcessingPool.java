ec1e07ac286ee842e1f14555f29a90d6fa182d44
Michal Stava <stavamichal@gmail.com>
2017-08-23 14:59:32 +0200

    New concept of group synchronization using thread pool:
     - was created a new class PerunBeanProcessingPool as concurent thread-safe
       structure to putting new waiting jobs (defined as any perunBean we
       want to work with) and taking these jobs by threads to be processed
       (removing by these threads after processing is finished). Only uniqueu
       job can be added (unique between all waiting and running jobs). This
       class use semaphore to let threads wait until list of waiting queues
       is not empty. You can also get set of running and list of waiting jobs
       at moment of calling. This class is then used as thread pool of groups
       to be synchronized.
     - number of active concurent threads is limited by new property
       perun.group.maxConcurentGroupsToSynchronize with default value=10,
       this property can be override in perun.properties file for every
       instance separate
     - method synchronizeGroups respect new concept and firstly interrupts
       all threads after timeout, then remove all interrupted threads from
       list of active threads, and create a new threads to the maximum count
       of threads defined by constant from property file. New groups for
       synchronization are adding by this method to the processing pool
       without skipping order. This method also log all known information
       about changes in list of active threads and also log information
       about running and waiting synchronizations.
     - method forceGroupSynchronization is also using new processing pool
       with skipping order (add to the pool of waiting jobs as first)
     - newly threads are still running except interupting or timeouting, from
       start it always set everything to default and take another group (or
       wait if no such is waiting) from the thread pool
     - there is still process synchronizeGroups running by cron tool every 5
       minutes but behavior of this process is now little different. It
       remove all interrupted or timeouted threads from threadpool and then
       it creates a new threads and starts them (maximum defined by property
       menationed above). After restarting threads it also add new groups to
       the queue (groups where is already time to run defined by interval)

---
+++ perun-base/src/main/java/cz/metacentrum/perun/core/api/PerunBeanProcessingPool.java
@@ -0,0 +1,197 @@
+package cz.metacentrum.perun.core.api;
+
+import cz.metacentrum.perun.core.api.exceptions.InternalErrorException;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * Processing pool for processing PerunBeans as jobs.
+ *
+ * This implementation is thread-safe.
+ *
+ * You can put new waiting jobs in it and take waiting jobs to set them as running,
+ * and then remove them after work with them is finished.
+ * Jobs in processing pool are unique and can't be added if they are already
+ * in list of waiting jobs or in set of running jobs.
+ * New waiting job can skip order and be putted as first in the list of waiting jobs.
+ *
+ * Note: this class doesn't run any task scheduler, it just provides structures and functionality to manipulate
+ * with these structures used for processing job from the pool of waiting jobs.
+ *
+ * @author Michal Stava <stavamichal@gmail.com>
+ */
+public class PerunBeanProcessingPool<T extends PerunBean> {
+
+	//List of waiting jobs (they should be process as soon as possible), there is FIFO order
+	private final LinkedList<T> waitingJobs = new LinkedList<>();
+	//Set of already running jobs (we need to track them until they are finished)
+	private final HashSet<T> runningJobs = new HashSet<>();
+	//Semaphore which takes care about emptiness of list of waiting jobs (threads will wait for another job)
+	//Counter in this semaphore counts number of waiting jobs in the pool (0 means no jobs are waiting to be processed)
+	private final Semaphore notEmptyPoolSemaphore = new Semaphore(0, true);
+	//Access lock to create concurrent access by any operation to any structure of this processing pool
+	private final Lock jobsAccessLock = new ReentrantLock(true);
+
+	/**
+	 * Put new unique job to the list of waiting jobs.
+	 *
+	 * If job is already running, return false because there would be no change.
+	 * If job is already waiting, check if asFirst is true. If yes, move it (remove and add) to
+	 * the first place and return true. If not, return false.
+	 * If there is no such job, add it and return true.
+	 *
+	 * Uniqueness of the job in pool is mandatory and it is no difference between waiting and running status of job in
+	 * this case.
+	 *
+	 * @param job perunBean object which defines job
+	 * @param asFirst true if job will skip order and will be placed to the list as first (LIFO)
+	 *
+	 * @return true if unique job was added or job was added to the beginning of the queue (asFirst is true), false if job exists and
+	 * was not added to the beginning of the queue (asFirst is false) or it is already running
+	 *
+	 * @throws InternalErrorException if job in parameter is null
+	 */
+	public boolean putJobIfAbsent(T job, boolean asFirst) throws InternalErrorException {
+		if(job == null) throw new InternalErrorException("Can't put null job to list of waiting jobs.");
+
+		try {
+			//get lock for any operation with structures of jobs
+			jobsAccessLock.lock();
+
+			//put only jobs which are not running yet
+			if (runningJobs.contains(job)) return false;
+
+			//If the job is already in waiting queue, we don't have to plan it again unless we want to move it
+			// to the begging of the queue which is indicated by asFirst variable.
+			if (waitingJobs.contains(job)) {
+				if (!asFirst) {
+					return false;
+				} else {
+					//remove it from the waiting queue before putting it back as first waiting job
+					waitingJobs.remove(job);
+				}
+			}
+
+			//add it as first or at last defined by parameter of method
+			if (asFirst) {
+				waitingJobs.addFirst(job);
+			} else {
+				waitingJobs.addLast(job);
+			}
+
+			//increase number of waiting jobs by one
+			notEmptyPoolSemaphore.release();
+
+		} finally {
+			//unlock access lock
+			jobsAccessLock.unlock();
+		}
+
+		//return true (new waiting job in list of waiting jobs)
+		return true;
+	}
+
+	/**
+	 * Take job from list of waiting jobs and add it to the list of running jobs.
+	 *
+	 * @return first waiting job from the list of waiting jobs
+	 *
+	 * @throws InterruptedException if acquiring of permit on semaphore was interrupted
+	 */
+	public T takeJob() throws InterruptedException {
+		//I can take only if there is not empty list of waiting jobs
+		notEmptyPoolSemaphore.acquire();
+
+		T job;
+		try {
+			//If there is at least one job in the list of waiting jobs, get access to manipulate with it
+			jobsAccessLock.lock();
+
+			//We should always get not null job, because semaphore pool was not empty
+			job = waitingJobs.pollFirst();
+
+			runningJobs.add(job);
+		} finally {
+			//unlock access lock
+			jobsAccessLock.unlock();
+
+		}
+
+		return job;
+	}
+
+	/**
+	 * Remove jobs from set of running jobs. It means that we are done with this job.
+	 *
+	 * @param job perunBean object which defines job
+	 *
+	 * @return true if removing was successful, false if not (structure not contained the job)
+	 */
+	public boolean removeJob(T job) {
+		//Can't remove null job
+		if (job == null) return false;
+
+		boolean removedSuccessfully;
+		try {
+			//get lock for any operation with structures of jobs
+			jobsAccessLock.lock();
+
+			//try to remove running job (because it should be done)
+			removedSuccessfully = runningJobs.remove(job);
+		} finally {
+			//in any case unlock access lock
+			jobsAccessLock.unlock();
+		}
+
+		return removedSuccessfully;
+	}
+
+	/**
+	 * Get set of running jobs at this moment.
+	 *
+	 * @return set of running jobs at this moment
+	 */
+	public Set<T> getRunningJobs() {
+		Set<T> runningJobs;
+		try {
+			//get lock for any operation with structures of jobs
+			jobsAccessLock.lock();
+
+			runningJobs = Collections.unmodifiableSet(this.runningJobs);
+
+		} finally {
+			//in any case unlock access lock
+			jobsAccessLock.unlock();
+		}
+
+		return runningJobs;
+	}
+
+	/**
+	 * Get ordered list of waiting jobs at this moment.
+	 *
+	 * @return ordered list of waiting jobs at this moment
+	 */
+	public List<T> getWaitingJobs() {
+		List<T> waitingJobs;
+		try {
+			//get lock for any operation with structures of jobs
+			jobsAccessLock.lock();
+
+			waitingJobs = Collections.unmodifiableList(this.waitingJobs);
+		} finally {
+			//in any case unlock access lock
+			jobsAccessLock.unlock();
+		}
+
+		return waitingJobs;
+	}
+}
