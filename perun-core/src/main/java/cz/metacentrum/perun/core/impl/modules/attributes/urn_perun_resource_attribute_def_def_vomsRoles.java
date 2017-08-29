5f42a9cdc071efb34238b3bd67e953a468c3254c
Vojtěch Sassmann <vojtech.sassmann@gmail.com>
2017-08-29 09:41:47 +0200

    Added resource and group vomsRoles attributes.
    Added vomsRoles attributes for group and resource.
    Also allowed null value in urn_perun_group_attribute_def_def_vomsGroupName.

---
+++ perun-core/src/main/java/cz/metacentrum/perun/core/impl/modules/attributes/urn_perun_resource_attribute_def_def_vomsRoles.java
@@ -0,0 +1,58 @@
+package cz.metacentrum.perun.core.impl.modules.attributes;
+
+import cz.metacentrum.perun.core.api.Attribute;
+import cz.metacentrum.perun.core.api.AttributeDefinition;
+import cz.metacentrum.perun.core.api.AttributesManager;
+import cz.metacentrum.perun.core.api.Resource;
+import cz.metacentrum.perun.core.api.exceptions.InternalErrorException;
+import cz.metacentrum.perun.core.api.exceptions.WrongAttributeAssignmentException;
+import cz.metacentrum.perun.core.api.exceptions.WrongAttributeValueException;
+import cz.metacentrum.perun.core.api.exceptions.WrongReferenceAttributeValueException;
+import cz.metacentrum.perun.core.impl.PerunSessionImpl;
+import cz.metacentrum.perun.core.implApi.modules.attributes.ResourceVirtualAttributesModuleAbstract;
+import cz.metacentrum.perun.core.implApi.modules.attributes.ResourceVirtualAttributesModuleImplApi;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Resource vomsRoles attribute.
+ *
+ * @author Vojtech Sassmann &lt;vojtech.sassmann@gmail.com&gt;
+ */
+public class urn_perun_resource_attribute_def_def_vomsRoles extends ResourceVirtualAttributesModuleAbstract implements ResourceVirtualAttributesModuleImplApi {
+
+	private final Pattern pattern = Pattern.compile("^[^<>&]*$");
+
+	@Override
+	@SuppressWarnings("unchecked")
+	public void checkAttributeValue(PerunSessionImpl perunSession, Resource resource, Attribute attribute) throws InternalErrorException, WrongAttributeValueException, WrongReferenceAttributeValueException, WrongAttributeAssignmentException {
+		if(attribute.getValue() == null) {
+			return;
+		}
+		try {
+			List<String> vomRoles = (List<String>) attribute.getValue();
+			for (String vomRole : vomRoles) {
+				Matcher matcher = pattern.matcher(vomRole);
+				if(!matcher.matches()) {
+					throw new WrongAttributeValueException(attribute, "Bad resource vomsRoles value. It should not contain '<>&' characters.");
+				}
+			}
+		} catch (ClassCastException e) {
+			throw new WrongAttributeValueException(attribute, "Value should be a list of Strings.");
+		}
+	}
+
+	@Override
+	public AttributeDefinition getAttributeDefinition() {
+		AttributeDefinition attr = new AttributeDefinition();
+		attr.setFriendlyName("vomsRoles");
+		attr.setDisplayName("Voms roles");
+		attr.setDescription("Default roles of all people assigned to this resource.");
+		attr.setType(ArrayList.class.getName());
+		attr.setNamespace(AttributesManager.NS_RESOURCE_ATTR_DEF);
+		return attr;
+	}
+}
