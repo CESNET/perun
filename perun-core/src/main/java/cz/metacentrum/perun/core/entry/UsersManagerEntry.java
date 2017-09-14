e5c0af6ae9055ab0ecd1751bb3b0c75d35c44664
Michal Stava <stavamichal@gmail.com>
2017-09-14 13:53:56 +0200

    Add to role REGISTRAR rights to update users in Perun
     - we want to add rights for role REGISTRAR to have rights for methods
       updateUsers and updateNameTitles in Perun

--- perun-core/src/main/java/cz/metacentrum/perun/core/entry/UsersManagerEntry.java
+++ perun-core/src/main/java/cz/metacentrum/perun/core/entry/UsersManagerEntry.java
@@ -340,7 +340,7 @@ public class UsersManagerEntry implements UsersManager {
 		Utils.checkPerunSession(sess);
 
 		// Authorization
-		if(!AuthzResolver.isAuthorized(sess, Role.PERUNADMIN)) {
+		if(!AuthzResolver.isAuthorized(sess, Role.REGISTRAR)) {
 			throw new PrivilegeException(sess, "updateUser");
 		}
 
@@ -354,7 +354,8 @@ public class UsersManagerEntry implements UsersManager {
 		Utils.checkPerunSession(sess);
 
 		// Authorization
-		if(!AuthzResolver.isAuthorized(sess, Role.SELF, user)) {
+		if(!AuthzResolver.isAuthorized(sess, Role.SELF, user) &&
+				!AuthzResolver.isAuthorized(sess, Role.REGISTRAR)) {
 			throw new PrivilegeException(sess, "updateNameTitles");
 		}
 
