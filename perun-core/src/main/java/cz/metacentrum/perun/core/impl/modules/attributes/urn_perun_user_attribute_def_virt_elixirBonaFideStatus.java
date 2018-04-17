5b7a47bdbe062f03a75e7c97ca6b01794395f13d
Pavel Zlámal <zlamal@cesnet.cz>
2018-04-17 08:32:25 +0200

    Updated resolving value of elixirBonaFideStatus
    - We now consider user a "researcher" also when he has publications
      reported in Perun with thanks to owner "ELIXIR".
      So now attribute module check changes of user:def:publications
      attribute and when getting value its used as a last resort after
      previous processing.

--- perun-core/src/main/java/cz/metacentrum/perun/core/impl/modules/attributes/urn_perun_user_attribute_def_virt_elixirBonaFideStatus.java
+++ perun-core/src/main/java/cz/metacentrum/perun/core/impl/modules/attributes/urn_perun_user_attribute_def_virt_elixirBonaFideStatus.java
@@ -1,6 +1,5 @@
 package cz.metacentrum.perun.core.impl.modules.attributes;
 
-
 import cz.metacentrum.perun.core.api.Attribute;
 import cz.metacentrum.perun.core.api.AttributeDefinition;
 import cz.metacentrum.perun.core.api.AttributesManager;
@@ -16,6 +15,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -27,8 +27,10 @@ import java.util.regex.Pattern;
  * The decision depends on attribute 'elixirBonaFideStatusREMS', if it
  * is not empty then the user is a researcher. If it is empty, then this module
  * searches in user's affiliations. If any of those affiliations starts
- * with 'faculty@' then the user is a researcher as well. Otherwise,
- * null value is set.
+ * with 'faculty@' then the user is a researcher as well.
+ * If none of above succeed, module looks into user:def:publications and if its value (map)
+ * contains key "ELIXIR" and value of such key is > 0 then is researcher.
+ * Otherwise, null value is set.
  *
  * @author Vojtech Sassmann &lt;vojtech.sassmann@gmail.com&gt;
  */
@@ -36,8 +38,10 @@ public class urn_perun_user_attribute_def_virt_elixirBonaFideStatus extends User
 
 	private final static Logger log = LoggerFactory.getLogger(urn_perun_user_attribute_def_virt_elixirBonaFideStatus.class);
 
-	private final Pattern userSetPattern = Pattern.compile("Attribute:\\[(.*)friendlyName=<elixirBonaFideStatusREMS>(.*)] set for User:\\[(.*)]");
-	private final Pattern userRemovePattern = Pattern.compile("AttributeDefinition:\\[(.*)friendlyName=<elixirBonaFideStatusREMS>(.*)] removed for User:\\[(.*)]");
+	private final Pattern userRemsSetPattern = Pattern.compile("Attribute:\\[(.*)friendlyName=<elixirBonaFideStatusREMS>(.*)] set for User:\\[(.*)]");
+	private final Pattern userRemsRemovePattern = Pattern.compile("AttributeDefinition:\\[(.*)friendlyName=<elixirBonaFideStatusREMS>(.*)] removed for User:\\[(.*)]");
+	private final Pattern userPublicationsSetPattern = Pattern.compile("Attribute:\\[(.*)friendlyName=<publications>(.*)] set for User:\\[(.*)]");
+	private final Pattern userPublicationsRemovePattern = Pattern.compile("AttributeDefinition:\\[(.*)friendlyName=<publications>(.*)] removed for User:\\[(.*)]");
 	private final Pattern userAllAttrsRemovedPattern = Pattern.compile("All attributes removed for User:\\[(.*)]");
 	private final Pattern uesAllAttrsRemovedPattern = Pattern.compile("All attributes removed for UserExtSource:\\[(.*)]");
 	private final Pattern uesSetAffiliationAttributePattern = Pattern.compile("Attribute:\\[(.*)friendlyName=<affiliation>(.*)] set for UserExtSource:\\[(.*)]");
@@ -48,11 +52,13 @@ public class urn_perun_user_attribute_def_virt_elixirBonaFideStatus extends User
 
 	private static final String USER_REMS_ATTR_NAME = "elixirBonaFideStatusREMS";
 	private static final String USER_AFFILIATIONS_ATTR_NAME = "eduPersonScopedAffiliations";
+	private static final String USER_PUBLICATIONS_ATTR_NAME = "publications";
 
 	@Override
 	public Attribute getAttributeValue(PerunSessionImpl sess, User user, AttributeDefinition attributeDefinition) throws InternalErrorException {
 		Attribute attribute = new Attribute(attributeDefinition);
 
+		// get value from 'elixirBonaFideStatusREMS'
 		try {
 			Attribute statusAttr = sess.getPerunBl().getAttributesManagerBl().getAttribute(sess, user, AttributesManager.NS_USER_ATTR_DEF + ":" + USER_REMS_ATTR_NAME);
 			String statusAttrValue = (String) statusAttr.getValue();
@@ -63,6 +69,7 @@ public class urn_perun_user_attribute_def_virt_elixirBonaFideStatus extends User
 			log.error("Cannot read {} from user {}", USER_REMS_ATTR_NAME, user, e);
 		}
 
+		// fallback on users affiliations
 		if (attribute.getValue() == null) {
 			try {
 				Attribute userAttribute = sess.getPerunBl().getAttributesManagerBl().getAttribute(sess, user, AttributesManager.NS_USER_ATTR_VIRT + ":" + USER_AFFILIATIONS_ATTR_NAME);
@@ -80,6 +87,32 @@ public class urn_perun_user_attribute_def_virt_elixirBonaFideStatus extends User
 			}
 		}
 
+		// fallback on reported publications with thanks to "ELIXIR".
+		if (attribute.getValue() == null) {
+			try {
+				Attribute userAttribute = null;
+				userAttribute = sess.getPerunBl().getAttributesManagerBl().getAttribute(sess, user, AttributesManager.NS_USER_ATTR_DEF + ":" + USER_PUBLICATIONS_ATTR_NAME);
+
+				if (userAttribute.getValue() != null) {
+					LinkedHashMap<String,String> publications = (LinkedHashMap<String,String>) userAttribute.getValue();
+					if (publications.containsKey("ELIXIR")) {
+						String value = publications.get("ELIXIR");
+						try {
+							int count = Integer.parseInt(value);
+							if (count > 0) {
+								attribute.setValue(URL);
+							}
+						} catch (NumberFormatException ex) {
+							log.error("Attribute user:def:publications has wrong value for key ELIXIR", ex);
+						}
+					}
+				}
+			} catch (WrongAttributeAssignmentException | AttributeNotExistsException e) {
+				log.error("Cannot read {} from user {}", USER_PUBLICATIONS_ATTR_NAME, user, e);
+			}
+
+		}
+
 		return attribute;
 	}
 
@@ -88,8 +121,10 @@ public class urn_perun_user_attribute_def_virt_elixirBonaFideStatus extends User
 		List<String> resolvingMessages = new ArrayList<>();
 		if (message == null) return resolvingMessages;
 
-		Matcher userSetMatcher = userSetPattern.matcher(message);
-		Matcher userRemoveMatcher = userRemovePattern.matcher(message);
+		Matcher userRemsSetMatcher = userRemsSetPattern.matcher(message);
+		Matcher userRemsRemoveMatcher = userRemsRemovePattern.matcher(message);
+		Matcher userPublicationsSetMatcher = userPublicationsSetPattern.matcher(message);
+		Matcher userPublicationsRemoveMatcher = userPublicationsRemovePattern.matcher(message);
 		Matcher userAllAttrsRemovedMatcher = userAllAttrsRemovedPattern.matcher(message);
 		Matcher uesAllAttrsRemovedMatcher = uesAllAttrsRemovedPattern.matcher(message);
 		Matcher uesSetAffiliationAttributeMatcher = uesSetAffiliationAttributePattern.matcher(message);
@@ -99,7 +134,8 @@ public class urn_perun_user_attribute_def_virt_elixirBonaFideStatus extends User
 		Attribute attribute;
 
 		if (uesSetAffiliationAttributeMatcher.find() ||	uesRemoveAffiliationAttributeMatcher.find() ||	uesAllAttrsRemovedMatcher.find() ||
-				userSetMatcher.find() || userRemoveMatcher.find() || userAllAttrsRemovedMatcher.find()) {
+				userRemsSetMatcher.find() || userRemsRemoveMatcher.find() || userAllAttrsRemovedMatcher.find() || userPublicationsSetMatcher.find() ||
+				userPublicationsRemoveMatcher.find()) {
 
 			user = sess.getPerunBl().getModulesUtilsBl().getUserFromMessage(sess, message);
 			if (user != null) {
