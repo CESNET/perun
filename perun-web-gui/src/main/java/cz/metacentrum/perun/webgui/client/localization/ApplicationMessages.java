7d7f0d524f54a21e8771a24364b0c2d459e76f4b
Pavel Zlámal <zlamal@cesnet.cz>
2018-04-25 13:25:22 +0200

    GUI: Removal of no-longer used apps for registration and pwd-reset
     - We user new WUI apps for registration and password reset.
       Removed unused and no longer maintained code.

--- perun-web-gui/src/main/java/cz/metacentrum/perun/webgui/client/localization/ApplicationMessages.java
+++ perun-web-gui/src/main/java/cz/metacentrum/perun/webgui/client/localization/ApplicationMessages.java
@@ -12,9 +12,6 @@ public interface ApplicationMessages extends Messages {
 
 	public static final ApplicationMessages INSTANCE = GWT.create(ApplicationMessages.class);
 
-	@DefaultMessage("Applications")
-	String applicationForm();
-
 	@DefaultMessage("Missing value")
 	String missingValue();
 
@@ -39,71 +36,14 @@ public interface ApplicationMessages extends Messages {
 	@DefaultMessage("Input text is too long")
 	String inputTextIsTooLong();
 
-	@DefaultMessage("Application for {0}")
-	String applicationFormForVo(String voName);
-
-	@DefaultMessage("Application for {0}")
-	String applicationFormForGroup(String groupName);
-
-	@DefaultMessage("Membership extension for {0}")
-	String membershipExtensionForVo(String voName);
-
-	@DefaultMessage("Membership extension for {0}")
-	String membershipExtensionForGroup(String groupName);
-
-	@DefaultMessage("Your application should by automatically approved within a few minutes. Check your e-mail for further information.")
-	String autoApprovalApplicationText();
-
-	@DefaultMessage("Please wait until your application is either approved or rejected by VO administrator. Check your e-mail for further information.")
-	String nonAutoApprovalApplicationText();
-
-	@DefaultMessage("Application for {0} has been successfully created.")
-	String applicationSuccessfullySent(String voName);
-
-	@DefaultMessage("You have provided unverified e-mail, please check your mailbox for validation link.")
-	String ifEmailProvidedCheckInbox();
-
-	@DefaultMessage("Extension application for VO {0} has been successfully sent.")
-	String membershipExtensionSuccessfullySent(String voName);
-
 	@DefaultMessage("Error occurred when processing your application.")
 	String errorWhileCreatingApplication();
 
 	@DefaultMessage("Your application was not saved! <p> Login/password reservation failed in external system. <br /> Please contact support to resolve this issue before new application submission.")
 	String errorWhileCreatingApplicationMessage();
 
-	@DefaultMessage("VO administrator has been notified about all errors.")
-	String voAdministratorWasNotified();
-
-	@DefaultMessage("{0}: my applications")
-	String applicationsForUser(String user);
-
-	@DefaultMessage("Filter by virtual organization:")
-	String filterByVo();
-
-	@DefaultMessage("{0} application for {1}")
-	String applicationDetail(String type, String vo);
-
-	@DefaultMessage("{0} application for group {1} in VO {2}")
-	String applicationDetailGroup(String type, String group, String vo);
-
-	@DefaultMessage("Application submitted at <strong>{0}</strong> is in state <strong>{1}</strong>")
-	String applicationDetailMessage(String date, String state);
-
-	@DefaultMessage("Back")
-	String backButton();
-
-	@DefaultMessage("Back to My applications")
-	String backButtonMessage();
-
 	@DefaultMessage("My applications")
 	String applications();
-
-	@DefaultMessage("Similar user(s) found")
-	String similarUsersFound();
-
-	@DefaultMessage("Similar user(s) are already registered in system. If it`s you, prove your identity by logging-in using one of the registered identities.")
-	String similarUsersFoundIsItYou();
 
 	@DefaultMessage("Person")
 	String name();
@@ -116,36 +56,6 @@ public interface ApplicationMessages extends Messages {
 
 	@DefaultMessage("Prove identity by")
 	String identities();
-
-	@DefaultMessage("No suitable identity found.")
-	String noIdentsFound();
-
-	@DefaultMessage("Yes")
-	String yes();
-
-	@DefaultMessage("No, continue")
-	String noContinue();
-
-	@DefaultMessage("Please, contact user support {2}, which will help you connect your new identity ({0} / {1}) to the existing one.")
-	String forAddingNewIdentityContactSupport(String identity, String extSourceName, String support);
-
-	@DefaultMessage("E-mail verification")
-	String emailValidationMenuItem();
-
-	@DefaultMessage("Your e-mail address has been verified !")
-	String emailValidationSuccess();
-
-	@DefaultMessage("Your e-mail address has NOT been verified !<p>Wrong verification code !")
-	String emailValidationFail();
-
-	@DefaultMessage("Change language to {0}")
-	String changeLanguageToCzech(String lang);
-
-	@DefaultMessage("Change language to English")
-	String changeLanguageToEnglish();
-
-	@DefaultMessage("<p>Changing language will reload whole application.</p><p><strong>All unsaved changes will be lost.</strong></p><p>Do you want to proceed ?</p>")
-	String changeLanguageText();
 
 	@DefaultMessage("<h2>You have already sent your initial application to this VO / group.</h2><h2>Please wait until your application is either approved or rejected by VO / group administrator.</h2>")
 	String duplicateRegistrationAttemptExceptionText();
@@ -165,26 +75,8 @@ public interface ApplicationMessages extends Messages {
 	@DefaultMessage("<h2>You are already member of VO.</h2>")
 	String alreadyVoMember();
 
-	@DefaultMessage("You are already member of {0}.")
-	String groupMembershipCantBeExtended(String groupName);
-
 	@DefaultMessage("Processing...")
 	String processing();
-
-	@DefaultMessage("Contact: ")
-	String supportContact();
-
-	@DefaultMessage("<h2>In order to continue to the registration page, please use CAPTCHA below.</h2>")
-	String captchaDescription();
-
-	@DefaultMessage("Answer is incorrect. Please try again.")
-	String captchaErrorMessage();
-
-	@DefaultMessage("Wrong CAPTCHA answer")
-	String captchaErrorHeader();
-
-	@DefaultMessage("Continue")
-	String captchaSendButton();
 
 	@DefaultMessage("<h2>Your IDP does not provide Level of Assurance but VO requires it.</h2><h2>Try to log-in with different IDP.</h2>")
 	String noUserLoa();
@@ -197,12 +89,6 @@ public interface ApplicationMessages extends Messages {
 
 	@DefaultMessage("<h2>Your membership cannot be extended right now.</h2><h2>There is fixed period before membership expiration, when membership can be extended.</h2>")
 	String outsideExtensionPeriod();
-
-	@DefaultMessage("Join identity")
-	String joinIdentity();
-
-	@DefaultMessage("No, thanks")
-	String notJoinIdentity();
 
 	@DefaultMessage("Created date")
 	String createdDate();
@@ -218,14 +104,5 @@ public interface ApplicationMessages extends Messages {
 
 	@DefaultMessage("Group")
 	String group();
-
-	@DefaultMessage("Click on application to see more details.")
-	String clickOnApplicationToSee();
-
-	@DefaultMessage("In order to apply for group membership, you must be VO member first.")
-	String mustBeVoMemberFirst();
-
-	@DefaultMessage("Logout")
-	String logout();
 
 }
