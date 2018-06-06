af99ff201a6abb3071e2200f94c03988c16234e5
Slavek Licehammer <slavek@ics.muni.cz>
2018-06-06 17:04:41 +0200

    New attributes for eduTEAMS login

---
+++ perun-core/src/main/java/cz/metacentrum/perun/core/impl/modules/attributes/urn_perun_user_attribute_def_virt_login_namespace_eduteams_persistent.java
@@ -0,0 +1,71 @@
+package cz.metacentrum.perun.core.impl.modules.attributes;
+
+import cz.metacentrum.perun.core.api.Attribute;
+import cz.metacentrum.perun.core.api.AttributeDefinition;
+import cz.metacentrum.perun.core.api.AttributesManager;
+import cz.metacentrum.perun.core.api.User;
+import cz.metacentrum.perun.core.api.exceptions.AttributeNotExistsException;
+import cz.metacentrum.perun.core.api.exceptions.InternalErrorException;
+import cz.metacentrum.perun.core.api.exceptions.WrongAttributeAssignmentException;
+import cz.metacentrum.perun.core.api.exceptions.WrongAttributeValueException;
+import cz.metacentrum.perun.core.api.exceptions.WrongReferenceAttributeValueException;
+import cz.metacentrum.perun.core.impl.PerunSessionImpl;
+import cz.metacentrum.perun.core.implApi.modules.attributes.UserVirtualAttributesModuleAbstract;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Class for access def:eduteams-persistent-shadow attribute. It generates value if you call it for the first time.
+ *
+ */
+public class urn_perun_user_attribute_def_virt_login_namespace_eduteams_persistent extends UserVirtualAttributesModuleAbstract {
+
+	public static final String SHADOW = "urn:perun:user:attribute-def:def:login-namespace:eduteams-persistent-shadow";
+
+	@Override
+	public Attribute getAttributeValue(PerunSessionImpl sess, User user, AttributeDefinition attributeDefinition) throws InternalErrorException {
+		Attribute eduteamsPersistent = new Attribute(attributeDefinition);
+
+		try {
+			Attribute eduteamsPersistentShadow = sess.getPerunBl().getAttributesManagerBl().getAttribute(sess, user, SHADOW);
+
+			if (eduteamsPersistentShadow.getValue() == null) {
+
+				eduteamsPersistentShadow = sess.getPerunBl().getAttributesManagerBl().fillAttribute(sess, user, eduteamsPersistentShadow);
+
+				if (eduteamsPersistentShadow.getValue() == null) {
+					throw new InternalErrorException("Eduteams id couldn't be set automatically");
+				}
+				sess.getPerunBl().getAttributesManagerBl().setAttribute(sess, user, eduteamsPersistentShadow);
+			}
+
+			eduteamsPersistent.setValue(eduteamsPersistentShadow.getValue());
+			return eduteamsPersistent;
+
+		} catch (WrongAttributeAssignmentException e) {
+			throw new InternalErrorException(e);
+		} catch (AttributeNotExistsException e) {
+			throw new InternalErrorException(e);
+		} catch (WrongReferenceAttributeValueException e) {
+			throw new InternalErrorException(e);
+		} catch (WrongAttributeValueException e) {
+			throw new InternalErrorException(e);
+		}
+	}
+
+	@Override
+	public List<String> getStrongDependencies() {
+		return Collections.singletonList(SHADOW);
+	}
+
+	public AttributeDefinition getAttributeDefinition() {
+		AttributeDefinition attr = new AttributeDefinition();
+		attr.setNamespace(AttributesManager.NS_USER_ATTR_VIRT);
+		attr.setFriendlyName("login-namespace:eduteams-persistent");
+		attr.setDisplayName("eduTEAMS login");
+		attr.setType(String.class.getName());
+		attr.setDescription("Login to eduTEAMS. It is set automatically with first call.");
+		return attr;
+	}
+}
