f7733dfc27115a23d6942bef48124d008c4f4bcb
Pavel Zlámal <zlamal@cesnet.cz>
2018-06-15 08:45:49 +0200

    Reimplemented RpcCallerImpl so we can properly support CSRF
    - Previous implementation failed to set request Header.
    - Added possibility to set request timeout (used from LDAPc).

--- perun-rpc-lib/src/main/java/cz/metacentrum/perun/rpclib/api/RpcCaller.java
+++ perun-rpc-lib/src/main/java/cz/metacentrum/perun/rpclib/api/RpcCaller.java
@@ -3,10 +3,34 @@ package cz.metacentrum.perun.rpclib.api;
 import java.util.Map;
 
 import cz.metacentrum.perun.core.api.exceptions.PerunException;
-import cz.metacentrum.perun.rpclib.api.Deserializer;
 
+/**
+ * Interface for Java RPC Client
+ *
+ * @author Michal Šťava
+ * @author Pavel Zlámal
+ */
 public interface RpcCaller {
+
+	/**
+	 * Call RPC API method without params
+	 *
+	 * @param managerName called manager
+	 * @param methodName called method
+	 * @return JsonDeserializer with response
+	 * @throws PerunException If anything fails
+	 */
 	public Deserializer call(String managerName, String methodName) throws PerunException;
 
+	/**
+	 * Call RPC API method with params
+	 *
+	 * @param managerName called manager
+	 * @param methodName called method
+	 * @param params method params passed with the call
+	 * @return JsonDeserializer with response
+	 * @throws PerunException If anything fails
+	 */
 	public Deserializer call(String managerName, String methodName, Map<String, Object> params) throws PerunException;
+
 }
