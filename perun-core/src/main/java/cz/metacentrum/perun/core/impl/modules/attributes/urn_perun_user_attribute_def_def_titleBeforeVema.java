9002e2734bfd5bee21a77c8028eb271b8cd42092
Pavel Zlámal <zlamal@cesnet.cz>
2018-07-18 09:08:44 +0200

    Merge pull request #1909 from zlamalp/vsup
    CORE: Added modules for title before name from KOS and VEMA systems

---
+++ perun-core/src/main/java/cz/metacentrum/perun/core/impl/modules/attributes/urn_perun_user_attribute_def_def_titleBeforeVema.java
@@ -0,0 +1,53 @@
+package cz.metacentrum.perun.core.impl.modules.attributes;
+
+import cz.metacentrum.perun.core.api.Attribute;
+import cz.metacentrum.perun.core.api.AttributeDefinition;
+import cz.metacentrum.perun.core.api.AttributesManager;
+import cz.metacentrum.perun.core.api.User;
+import cz.metacentrum.perun.core.api.exceptions.ConsistencyErrorException;
+import cz.metacentrum.perun.core.api.exceptions.InternalErrorException;
+import cz.metacentrum.perun.core.api.exceptions.UserNotExistsException;
+import cz.metacentrum.perun.core.api.exceptions.WrongReferenceAttributeValueException;
+import cz.metacentrum.perun.core.impl.PerunSessionImpl;
+import cz.metacentrum.perun.core.implApi.modules.attributes.UserAttributesModuleAbstract;
+import cz.metacentrum.perun.core.implApi.modules.attributes.UserAttributesModuleImplApi;
+
+/**
+ * Update title before name on User if value in attribute is changed.
+ *
+ * @author Pavel Zlámal <zlamal@cesnet.cz>
+ */
+public class urn_perun_user_attribute_def_def_titleBeforeVema extends UserAttributesModuleAbstract implements UserAttributesModuleImplApi {
+
+	/**
+	 * When title before name from VEMA changes, update User.
+	 *
+	 * @param session
+	 * @param user
+	 * @param attribute
+	 * @throws InternalErrorException
+	 * @throws WrongReferenceAttributeValueException
+	 */
+	@Override
+	public void changedAttributeHook(PerunSessionImpl session, User user, Attribute attribute) throws InternalErrorException, WrongReferenceAttributeValueException {
+
+		user.setTitleBefore((String)attribute.getValue());
+		try {
+			session.getPerunBl().getUsersManagerBl().updateNameTitles(session, user);
+		} catch (UserNotExistsException e) {
+			throw new ConsistencyErrorException("User we set attributes for doesn't exists!", e);
+		}
+
+	}
+
+	public AttributeDefinition getAttributeDefinition() {
+		AttributeDefinition attr = new AttributeDefinition();
+		attr.setNamespace(AttributesManager.NS_USER_ATTR_DEF);
+		attr.setFriendlyName("titleBeforeVema");
+		attr.setDisplayName("Title before (VEMA)");
+		attr.setType(Integer.class.getName());
+		attr.setDescription("Title before name from VEMA.");
+		return attr;
+	}
+
+}
