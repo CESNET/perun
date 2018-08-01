c5237abb54a936a91f24b4743bea83ca574ad5c2
Pavel Zlámal <zlamal@cesnet.cz>
2018-08-01 10:38:49 +0200

    REGISTRAR: Extended content length of form items
    - Extended length to unlimited for content of HTML, Radiobutton,
      Checkbox, Selectionbox, Combobox items. For the HTML is all content,
      for the rest it expand count of possible options.
    - There is no change in a code, only DB change is necessary.

--- perun-db/oracle.sql
+++ perun-db/oracle.sql
@@ -1,4 +1,4 @@
--- database version 3.1.47 (don't forget to update insert statement at the end of file)
+-- database version 3.1.48 (don't forget to update insert statement at the end of file)
 
 create user perunv3 identified by password;
 grant create session to perunv3;
@@ -615,8 +615,8 @@ create table application_form_item_apptypes (
 create table application_form_item_texts (
 	item_id integer not null,     --identifier of form item (application_form_items.id)
 	locale nvarchar2(128) not null, --language for application
-	label nvarchar2(4000),          --label of item on app. form
-	options nvarchar2(4000),        --options for items with menu
+	label clob,          --label of item on app. form
+	options clob,        --options for items with menu
 	help nvarchar2(4000),           --text of help
 	error_message nvarchar2(4000),  --text of error message
 	created_by_uid integer,
@@ -1771,7 +1771,7 @@ CREATE INDEX ufauv_idx ON user_facility_attr_u_values (user_id, facility_id, att
 CREATE INDEX vauv_idx ON vo_attr_u_values (vo_id, attr_id) ;
 
 -- set initial Perun DB version
-insert into configurations values ('DATABASE VERSION','3.1.47');
+insert into configurations values ('DATABASE VERSION','3.1.48');
 
 -- insert membership types
 insert into membership_types (id, membership_type, description) values (1, 'DIRECT', 'Member is directly added into group');
