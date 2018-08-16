98cd6d87b2b8b1355e54e77b37954b956c2a9f26
Michal Stava <stavamichal@gmail.com>
2018-08-16 13:16:58 +0200

    New method getAttributes for facility by list of attr names
     - add this new method to all layers (entry, business and impl)
     - add some basic tests
     - add this method also to RPC

--- perun-core/src/main/java/cz/metacentrum/perun/core/implApi/AttributesManagerImplApi.java
+++ perun-core/src/main/java/cz/metacentrum/perun/core/implApi/AttributesManagerImplApi.java
@@ -58,6 +58,18 @@ public interface AttributesManagerImplApi {
 	List<Attribute> getAttributes(PerunSession sess, Facility facility) throws InternalErrorException;
 
 	/**
+	 * Get all attributes associated with the facility which have name in list attrNames (empty too).
+	 * Virtual attribute too.
+	 *
+	 * @param sess      perun session
+	 * @param facility    to get the attributes from
+	 * @param attrNames list of attributes' names
+	 * @return list of attributes
+	 * @throws InternalErrorException   if an exception raise in concrete implementation, the exception is wrapped in InternalErrorException
+	 */
+	List<Attribute> getAttributes(PerunSession sess, Facility facility, List<String> attrNames) throws InternalErrorException;
+
+	/**
 	 * Get all virtual attributes associated with the facility.
 	 *
 	 * @param sess perun session
