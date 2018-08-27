9ce3fed380616be887ca8c417dbde46e98280d52
Pavel Zlámal <zlamal@cesnet.cz>
2018-08-27 13:01:16 +0200

    GUI: Allow searching members and resource by attribute searcher

--- perun-web-gui/src/main/java/cz/metacentrum/perun/webgui/tabs/perunadmintabs/SearcherTabItem.java
+++ perun-web-gui/src/main/java/cz/metacentrum/perun/webgui/tabs/perunadmintabs/SearcherTabItem.java
@@ -1,7 +1,10 @@
 package cz.metacentrum.perun.webgui.tabs.perunadmintabs;
 
 import com.google.gwt.cell.client.FieldUpdater;
+import com.google.gwt.core.client.JavaScriptObject;
 import com.google.gwt.dom.client.Style;
+import com.google.gwt.event.dom.client.ChangeEvent;
+import com.google.gwt.event.dom.client.ChangeHandler;
 import com.google.gwt.event.logical.shared.SelectionEvent;
 import com.google.gwt.event.logical.shared.SelectionHandler;
 import com.google.gwt.resources.client.ImageResource;
@@ -12,18 +15,33 @@ import cz.metacentrum.perun.webgui.client.UiElements;
 import cz.metacentrum.perun.webgui.client.mainmenu.MainMenu;
 import cz.metacentrum.perun.webgui.client.resources.PerunEntity;
 import cz.metacentrum.perun.webgui.client.resources.SmallIcons;
+import cz.metacentrum.perun.webgui.client.resources.TableSorter;
+import cz.metacentrum.perun.webgui.json.JsonCallbackEvents;
+import cz.metacentrum.perun.webgui.json.JsonUtils;
 import cz.metacentrum.perun.webgui.json.searcher.GetFacilities;
+import cz.metacentrum.perun.webgui.json.searcher.GetMembers;
+import cz.metacentrum.perun.webgui.json.searcher.GetResources;
 import cz.metacentrum.perun.webgui.json.searcher.GetUsers;
+import cz.metacentrum.perun.webgui.json.vosManager.GetVos;
 import cz.metacentrum.perun.webgui.model.Facility;
+import cz.metacentrum.perun.webgui.model.Member;
+import cz.metacentrum.perun.webgui.model.PerunError;
+import cz.metacentrum.perun.webgui.model.Resource;
 import cz.metacentrum.perun.webgui.model.User;
+import cz.metacentrum.perun.webgui.model.VirtualOrganization;
 import cz.metacentrum.perun.webgui.tabs.PerunAdminTabs;
 import cz.metacentrum.perun.webgui.tabs.TabItem;
 import cz.metacentrum.perun.webgui.tabs.TabItemWithUrl;
 import cz.metacentrum.perun.webgui.tabs.UrlMapper;
 import cz.metacentrum.perun.webgui.tabs.facilitiestabs.FacilityDetailTabItem;
+import cz.metacentrum.perun.webgui.tabs.memberstabs.MemberDetailTabItem;
+import cz.metacentrum.perun.webgui.tabs.resourcestabs.ResourceDetailTabItem;
 import cz.metacentrum.perun.webgui.tabs.userstabs.UserDetailTabItem;
+import cz.metacentrum.perun.webgui.widgets.ListBoxWithObjects;
 import cz.metacentrum.perun.webgui.widgets.PerunSearchParametersWidget;
+import cz.metacentrum.perun.webgui.widgets.TabMenu;
 
+import java.util.ArrayList;
 import java.util.Map;
 
 /**
@@ -78,11 +96,15 @@ public class SearcherTabItem implements TabItem, TabItemWithUrl {
 
 		final SimplePanel sp0 = new SimplePanel(); // users
 		final SimplePanel sp1 = new SimplePanel(); // facilities
+		final SimplePanel sp2 = new SimplePanel(); // members
+		final SimplePanel sp3 = new SimplePanel(); // resources
 
 		session.getUiElements().resizeSmallTabPanel(tabPanel, 100, this);
 
 		tabPanel.add(sp0, "Users");
+		tabPanel.add(sp2, "Members");
 		tabPanel.add(sp1, "Facilities");
+		tabPanel.add(sp3, "Resources");
 
 		sp0.setWidget(loadUsersTab());
 
@@ -96,8 +118,16 @@ public class SearcherTabItem implements TabItem, TabItemWithUrl {
 						sp0.setWidget(loadUsersTab());
 					}
 				} else if (1 == event.getSelectedItem()) {
+					if (sp2.getWidget() == null) {
+						sp2.setWidget(loadMembersTab());
+					}
+				} else if (2 == event.getSelectedItem()) {
 					if (sp1.getWidget() == null) {
 						sp1.setWidget(loadFacilitiesTab());
+					}
+				} else if (3 == event.getSelectedItem()) {
+					if (sp3.getWidget() == null) {
+						sp3.setWidget(loadResourcesTab());
 					}
 				}
 			}
@@ -206,6 +236,132 @@ public class SearcherTabItem implements TabItem, TabItemWithUrl {
 
 	}
 
+	private Widget loadResourcesTab() {
+
+		// request
+		final GetResources request = new GetResources();
+
+		// MAIN TAB PANEL
+		VerticalPanel firstTabPanel = new VerticalPanel();
+		firstTabPanel.setSize("100%", "100%");
+
+		PerunSearchParametersWidget params = new PerunSearchParametersWidget(PerunEntity.RESOURCE, new PerunSearchParametersWidget.SearchEvent() {
+
+			public void search(Map<String, String> map) {
+
+				request.clearParameters();
+
+				for(Map.Entry<String, String> entry : map.entrySet())
+				{
+					request.addSearchParameter(entry.getKey(), entry.getValue());
+				}
+
+				request.search();
+			}
+		});
+
+		firstTabPanel.add(params);
+
+		// get the table
+		final CellTable<Resource> table = request.getEmptyTable(new FieldUpdater<Resource, String>() {
+			public void update(int index, Resource object, String value) {
+				// opens the tab
+				session.getTabManager().addTab(new ResourceDetailTabItem(object.getId(), object.getFacilityId()));
+			}
+		});
+
+		// add a class to the table and wrap it into scroll panel
+		table.addStyleName("perun-table");
+
+		ScrollPanel sp = new ScrollPanel(table);
+		sp.addStyleName("perun-tableScrollPanel");
+		firstTabPanel.add(sp);
+		session.getUiElements().resizePerunTable(sp, 350, this);
+		return firstTabPanel;
+
+	}
+
+	private Widget loadMembersTab() {
+
+		// request
+		final GetMembers request = new GetMembers(0);
+
+		// MAIN TAB PANEL
+		VerticalPanel firstTabPanel = new VerticalPanel();
+		firstTabPanel.setSize("100%", "100%");
+
+		PerunSearchParametersWidget params = new PerunSearchParametersWidget(PerunEntity.MEMBER, new PerunSearchParametersWidget.SearchEvent() {
+
+			public void search(Map<String, String> map) {
+
+				request.clearParameters();
+
+				for(Map.Entry<String, String> entry : map.entrySet())
+				{
+					request.addSearchParameter(entry.getKey(), entry.getValue());
+				}
+
+				request.search();
+			}
+		});
+
+		final ListBoxWithObjects<VirtualOrganization> vos = new ListBoxWithObjects<VirtualOrganization>();
+
+		vos.addChangeHandler(new ChangeHandler() {
+			@Override
+			public void onChange(ChangeEvent changeEvent) {
+				request.setVoId(vos.getSelectedObject().getId());
+			}
+		});
+
+		// initial fill listbox and trigger groups loading
+		GetVos vosCall = new GetVos(new JsonCallbackEvents(){
+			public void onLoadingStart(){
+				vos.clear();
+				vos.addItem("Loading...");
+			}
+			public void onFinished(JavaScriptObject jso) {
+				vos.clear();
+				ArrayList<VirtualOrganization> returnedVos = JsonUtils.jsoAsList(jso);
+				returnedVos = new TableSorter<VirtualOrganization>().sortByName(returnedVos);
+				if (returnedVos == null || returnedVos.isEmpty()){
+					vos.addItem("No VO available");
+					return;
+				}
+				vos.addAllItems(returnedVos);
+				request.setVoId(vos.getSelectedObject().getId());
+			}
+			public void onError(PerunError error){
+				vos.clear();
+				vos.addItem("Error while loading");
+			};
+		});
+		vosCall.retrieveData();
+
+		TabMenu menu = new TabMenu();
+		menu.addWidget(new HTML("<strong>Selected VO:</strong>"));
+		menu.addWidget(vos);
+		firstTabPanel.add(menu);
+		firstTabPanel.add(params);
+
+		// get the table
+		final CellTable<Member> table = request.getEmptyTable(new FieldUpdater<Member, String>() {
+			public void update(int index, Member object, String value) {
+				// opens the tab
+				session.getTabManager().addTab(new MemberDetailTabItem(object.getId(), object.getSourceGroupId()));
+			}
+		});
+
+		// add a class to the table and wrap it into scroll panel
+		table.addStyleName("perun-table");
+
+		ScrollPanel sp = new ScrollPanel(table);
+		sp.addStyleName("perun-tableScrollPanel");
+		firstTabPanel.add(sp);
+		session.getUiElements().resizePerunTable(sp, 350, this);
+		return firstTabPanel;
+
+	}
 
 	/**
 	 * Returns ID of last selected subtab in this page
