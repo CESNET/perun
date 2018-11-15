2beffb835d4add36332e28dc404b88e6814d5c65
Pavel Zlámal <zlamal@cesnet.cz>
2018-11-15 10:03:35 +0100

    Merge pull request #2044 from licehammer/eduTEAMS-attrs
    New attributes for eduTEAMS login

---
+++ perun-core/src/main/java/cz/metacentrum/perun/core/impl/modules/attributes/urn_perun_user_attribute_def_def_login_namespace_eduteams_persistent_shadow.java
@@ -0,0 +1,93 @@
+package cz.metacentrum.perun.core.impl.modules.attributes;
+
+import cz.metacentrum.perun.core.api.AttributesManager;
+import cz.metacentrum.perun.core.api.exceptions.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import cz.metacentrum.perun.core.api.Attribute;
+import cz.metacentrum.perun.core.api.AttributeDefinition;
+import cz.metacentrum.perun.core.api.ExtSource;
+import cz.metacentrum.perun.core.api.User;
+import cz.metacentrum.perun.core.api.UserExtSource;
+import cz.metacentrum.perun.core.impl.PerunSessionImpl;
+
+/**
+ * Class for checking logins uniqueness in the namespace and filling eduteams-persistent id.
+ * It is only storage! Use module login eduteams_persistent for access the value.
+ *
+ */
+public class urn_perun_user_attribute_def_def_login_namespace_eduteams_persistent_shadow extends urn_perun_user_attribute_def_def_login_namespace {
+
+	private final static Logger log = LoggerFactory.getLogger(urn_perun_user_attribute_def_def_login_namespace_eduteams_persistent_shadow.class);
+	private final static String extSourceNameEduteams = "https://proxy.eduteams.org/proxy";
+	private final static String domainNameEduteams = "@eduteams.org";
+	private final static String attrNameEduteams = "login-namespace:eduteams-persistent-shadow";
+
+	/**
+	 * Filling implemented for login:namespace:eduteams-persistent attribute
+	 * Format is: "[hash]@eduteams-europe.org" where [hash] represents sha1hash counted from user's id and perun instance id a login-namespace name
+	 *
+	 * @param perunSession PerunSession
+	 * @param user User to fill attribute for
+	 * @param attribute Attribute to fill value to
+	 * @return Filled attribute
+	 * @throws InternalErrorException
+	 * @throws WrongAttributeAssignmentException
+	 */
+	@Override
+	public Attribute fillAttribute(PerunSessionImpl perunSession, User user, AttributeDefinition attribute) throws InternalErrorException, WrongAttributeAssignmentException {
+
+		Attribute filledAttribute = new Attribute(attribute);
+
+		if (attribute.getFriendlyName().equals(attrNameEduteams)) {
+			filledAttribute.setValue(sha1HashCount(user, domainNameEduteams).toString() + domainNameEduteams);
+			return filledAttribute;
+		} else {
+			// without value
+			return filledAttribute;
+		}
+	}
+
+	/**
+	 * ChangedAttributeHook() sets UserExtSource with following properties:
+	 *  - extSourceType is IdP
+	 *  - extSourceName is https://engine.eduteams-idp.ics.muni.cz/authentication/idp/metadata
+	 *  - user's extSource login is the same as his eduteams-persistent attribute
+	 *
+	 * @param session PerunSession
+	 * @param user User to set UserExtSource for
+	 * @param attribute Attribute containing eduteamsID
+	 * @throws cz.metacentrum.perun.core.api.exceptions.InternalErrorException
+	 * @throws cz.metacentrum.perun.core.api.exceptions.WrongReferenceAttributeValueException
+	 */
+	@Override
+	public void changedAttributeHook(PerunSessionImpl session, User user, Attribute attribute) throws InternalErrorException, WrongReferenceAttributeValueException {
+		try {
+			String userNamespace = attribute.getFriendlyNameParameter();
+
+			if(userNamespace.equals("eduteams-persistent-shadow") && attribute.getValue() != null){
+				ExtSource extSource = session.getPerunBl().getExtSourcesManagerBl().getExtSourceByName(session, extSourceNameEduteams);
+				UserExtSource userExtSource = new UserExtSource(extSource, 0, attribute.getValue().toString());
+
+				session.getPerunBl().getUsersManagerBl().addUserExtSource(session, user, userExtSource);
+			}
+		} catch (UserExtSourceExistsException ex) {
+			log.warn("Eduteams IdP external source already exists for the user.", ex);
+		} catch (ExtSourceNotExistsException ex) {
+			throw new InternalErrorException("IdP external source for eduteams doesn't exist.", ex);
+		}
+	}
+
+	public AttributeDefinition getAttributeDefinition() {
+		AttributeDefinition attr = new AttributeDefinition();
+		attr.setNamespace(AttributesManager.NS_USER_ATTR_DEF);
+		attr.setFriendlyName("login-namespace:eduteams-persistent-shadow");
+		attr.setDisplayName("eduTEAMS login");
+		attr.setType(String.class.getName());
+		attr.setDescription("Login to eduTEAMS. Do not use it directly! " +
+				"Use \"user:virt:login-namespace:eduteams-persistent\" attribute instead.");
+		return attr;
+	}
+
+}
---
+++ perun-core/src/main/java/cz/metacentrum/perun/core/impl/modules/attributes/urn_perun_user_attribute_def_def_login_namespace_fenix_nickname.java
@@ -0,0 +1,144 @@
+package cz.metacentrum.perun.core.impl.modules.attributes;
+
+import cz.metacentrum.perun.core.api.Attribute;
+import cz.metacentrum.perun.core.api.AttributeDefinition;
+import cz.metacentrum.perun.core.api.AttributesManager;
+import cz.metacentrum.perun.core.api.ExtSource;
+import cz.metacentrum.perun.core.api.User;
+import cz.metacentrum.perun.core.api.UserExtSource;
+import cz.metacentrum.perun.core.api.exceptions.ExtSourceNotExistsException;
+import cz.metacentrum.perun.core.api.exceptions.InternalErrorException;
+import cz.metacentrum.perun.core.api.exceptions.UserExtSourceAlreadyRemovedException;
+import cz.metacentrum.perun.core.api.exceptions.UserExtSourceExistsException;
+import cz.metacentrum.perun.core.api.exceptions.WrongAttributeAssignmentException;
+import cz.metacentrum.perun.core.api.exceptions.WrongAttributeValueException;
+import cz.metacentrum.perun.core.api.exceptions.WrongReferenceAttributeValueException;
+import cz.metacentrum.perun.core.blImpl.ModulesUtilsBlImpl;
+import cz.metacentrum.perun.core.impl.PerunSessionImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+/**
+ * Class for checking logins uniqueness in the namespace and filling it
+ *
+ */
+public class urn_perun_user_attribute_def_def_login_namespace_fenix_nickname extends urn_perun_user_attribute_def_def_login_namespace {
+
+	private final static Logger log = LoggerFactory.getLogger(urn_perun_user_attribute_def_def_login_namespace_fenix_nickname.class);
+
+	/**
+	 * Checks if the user's login is unique in the namespace organization.
+	 * Check if maximum length is 20 chars
+	 *
+	 * @param sess PerunSession
+	 * @param user User to check attribute for
+	 * @param attribute Attribute to check value to
+	 * @throws cz.metacentrum.perun.core.api.exceptions.InternalErrorException
+	 * @throws cz.metacentrum.perun.core.api.exceptions.WrongAttributeValueException
+	 * @throws cz.metacentrum.perun.core.api.exceptions.WrongAttributeAssignmentException
+	 */
+	@Override
+	public void checkAttributeValue(PerunSessionImpl sess, User user, Attribute attribute) throws InternalErrorException, WrongAttributeValueException, WrongAttributeAssignmentException {
+
+		// check uniqueness
+		super.checkAttributeValue(sess, user, attribute);
+
+		// plus check, that login is max 20 chars.
+		if (attribute.getValue() != null) {
+			if (((String)attribute.getValue()).length() > 20) throw new WrongAttributeValueException(attribute, user, "Login '" + attribute.getValue() + "' exceeds 20 chars limit.");
+		}
+
+	}
+
+	/**
+	 * Filling implemented for:
+	 * - namespaces configured in /etc/perun/perun.properties as property: "perun.loginNamespace.generated"
+	 *
+	 * Resulting format/rules:
+	 * - "firstName.lastName[number]" where number is opt and start with 1 when same login is already present.
+	 * - Only first part of "firstName" and last part of "lastName" is taken.
+	 * - All accented chars are unaccented and all non (a-z,A-Z) chars are removed from name and value is lowered.
+	 *
+	 * @param perunSession PerunSession
+	 * @param user User to fill attribute for
+	 * @param attribute Attribute to fill value to
+	 * @return Filled attribute
+	 * @throws InternalErrorException
+	 * @throws WrongAttributeAssignmentException
+	 */
+	@Override
+	public Attribute fillAttribute(PerunSessionImpl perunSession, User user, AttributeDefinition attribute) throws InternalErrorException, WrongAttributeAssignmentException {
+
+		Attribute filledAttribute = new Attribute(attribute);
+
+		if (generatedNamespaces.contains(attribute.getFriendlyNameParameter())) {
+
+			ModulesUtilsBlImpl.LoginGenerator generator = new ModulesUtilsBlImpl.LoginGenerator();
+			String login = generator.generateLogin(user, new ModulesUtilsBlImpl.LoginGenerator.LoginGeneratorFunction() {
+				@Override
+				public String generateLogin(String firstName, String lastName) {
+
+					// unable to fill login for users without name or with partial name
+					if (firstName == null || firstName.isEmpty() || lastName == null || lastName.isEmpty()) {
+						return "fenix-user";
+					}
+
+					String login = firstName+ "." + lastName;
+					if (login.length()>20) {
+						login = login.substring(0, 20);
+					}
+					return login;
+				}
+			});
+
+			if (login == null) return filledAttribute;
+
+			// fill value
+			int iterator = 0;
+			while (iterator >= 0) {
+				if (iterator > 0) {
+					int iteratorLength = String.valueOf(iterator).length();
+					if (login.length() + iteratorLength > 20) {
+						// if login+iterator > 20 => crop login & reset iterator
+						login = login.substring(0, login.length()-1);
+						iterator = 0;
+						filledAttribute.setValue(login);
+					} else {
+						filledAttribute.setValue(login + iterator);
+					}
+
+				} else {
+					filledAttribute.setValue(login);
+				}
+				try {
+					checkAttributeValue(perunSession, user, filledAttribute);
+					return filledAttribute;
+				} catch (WrongAttributeValueException ex) {
+					// continue in a WHILE cycle
+					iterator++;
+				}
+			}
+
+			return filledAttribute;
+
+		} else {
+			// without value
+			return filledAttribute;
+		}
+	}
+
+	public AttributeDefinition getAttributeDefinition() {
+		AttributeDefinition attr = new AttributeDefinition();
+		attr.setNamespace(AttributesManager.NS_USER_ATTR_DEF);
+		attr.setFriendlyName("login-namespace:fenix-nickname");
+		attr.setDisplayName("Login in namespace: fenix");
+		attr.setType(String.class.getName());
+		attr.setDescription("Logname in namespace 'fenix'.");
+		return attr;
+	}
+
+
+
+}
