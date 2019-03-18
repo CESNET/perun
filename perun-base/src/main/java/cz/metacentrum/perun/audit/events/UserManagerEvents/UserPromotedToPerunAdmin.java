58ec486e37d363c503beea93b1f2e1bc2547282f
Michal Stava <stavamichal@gmail.com>
2019-03-18 12:26:12 +0100

    Add audit event to method makeUserPerunAdmin
     - we want to watch situation, when somebody has been promoted to
     PerunAdmin and notify administrators of such instance of Perun about
     this behavior

---
+++ perun-base/src/main/java/cz/metacentrum/perun/audit/events/UserManagerEvents/UserPromotedToPerunAdmin.java
@@ -0,0 +1,33 @@
+package cz.metacentrum.perun.audit.events.UserManagerEvents;
+
+import cz.metacentrum.perun.audit.events.AuditEvent;
+import cz.metacentrum.perun.core.api.User;
+
+public class UserPromotedToPerunAdmin extends AuditEvent {
+
+	private User user;
+	private String message;
+
+	@SuppressWarnings("unused") // used by jackson mapper
+	public UserPromotedToPerunAdmin() {
+	}
+
+	public UserPromotedToPerunAdmin(User user) {
+		this.user = user;
+		this.message = formatMessage("%s was promoted to PERUNADMIN.", user);
+	}
+
+	@Override
+	public String getMessage() {
+		return message;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	@Override
+	public String toString() {
+		return message;
+	}
+}
