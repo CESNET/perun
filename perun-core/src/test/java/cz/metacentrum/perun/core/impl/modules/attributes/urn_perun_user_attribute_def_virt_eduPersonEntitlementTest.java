547a2f148d9a594a63dc88f053e007deac569637
Dominik František Bučík <bucik@ics.muni.cz>
2019-06-03 12:45:21 +0200

    Modified user:virt:eduPersonEntitlement
    - Removed collecting value from urn:perun:user:attribute-def:virt:groupNames
    - Modified tests according to change

--- perun-core/src/test/java/cz/metacentrum/perun/core/impl/modules/attributes/urn_perun_user_attribute_def_virt_eduPersonEntitlementTest.java
+++ perun-core/src/test/java/cz/metacentrum/perun/core/impl/modules/attributes/urn_perun_user_attribute_def_virt_eduPersonEntitlementTest.java
@@ -1,15 +1,7 @@
 package cz.metacentrum.perun.core.impl.modules.attributes;
 
 
-import cz.metacentrum.perun.audit.events.AttributesManagerEvents.AllAttributesRemovedForUser;
-import cz.metacentrum.perun.audit.events.AttributesManagerEvents.AllAttributesRemovedForUserExtSource;
-import cz.metacentrum.perun.audit.events.AttributesManagerEvents.AttributeRemovedForUes;
-import cz.metacentrum.perun.audit.events.AttributesManagerEvents.AttributeRemovedForUser;
-import cz.metacentrum.perun.audit.events.AttributesManagerEvents.AttributeSetForUes;
-import cz.metacentrum.perun.audit.events.AttributesManagerEvents.AttributeSetForUser;
-import cz.metacentrum.perun.audit.events.AuditEvent;
 import cz.metacentrum.perun.core.api.Attribute;
-import cz.metacentrum.perun.core.api.AttributesManager;
 import cz.metacentrum.perun.core.api.ExtSource;
 import cz.metacentrum.perun.core.api.User;
 import cz.metacentrum.perun.core.api.UserExtSource;
@@ -17,7 +9,6 @@ import cz.metacentrum.perun.core.impl.PerunSessionImpl;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -42,12 +33,9 @@ public class urn_perun_user_attribute_def_virt_eduPersonEntitlementTest {
 	private UserExtSource ues2;
 	private Attribute uesAtt1;
 	private Attribute uesAtt2;
-	private Attribute groupNamesAttr;
 	private final String VALUE1 = "11entitlement11@somewhere.edu";
 	private final String VALUE2 = "22entitlement22@somewhere.edu";
 	private final String VALUE3 = "33entitlement33@somewhere.edu";
-	private final String VALUE4 = "44entitlement44@somewhere.edu";
-	private final String VALUE5 = "55entitlement55@somewhere.edu";
 
 	@Before
 	public void setVariables() {
@@ -66,12 +54,6 @@ public class urn_perun_user_attribute_def_virt_eduPersonEntitlementTest {
 		uesAtt2 = new Attribute();
 		uesAtt1.setValue(VALUE1);
 		uesAtt2.setValue(VALUE2+";"+VALUE3);
-
-		groupNamesAttr = new Attribute();
-		List<String> arrListValue = new ArrayList<>();
-		arrListValue.add(VALUE4);
-		arrListValue.add(VALUE5);
-		groupNamesAttr.setValue(arrListValue);
 	}
 
 	@Test
@@ -80,7 +62,6 @@ public class urn_perun_user_attribute_def_virt_eduPersonEntitlementTest {
 		PerunSessionImpl session = mock(PerunSessionImpl.class, RETURNS_DEEP_STUBS);
 
 		String primarySourceAttributeName = classInstance.getSourceAttributeName();
-		String secondarySourceAttrName = classInstance.getSecondarySourceAttributeName();
 
 		// USER_EXT_SOURCE
 		when(session.getPerunBl().getUsersManagerBl().getUserExtSources(session, user)).thenReturn(
@@ -93,11 +74,6 @@ public class urn_perun_user_attribute_def_virt_eduPersonEntitlementTest {
 				uesAtt2
 		);
 
-		// ENTITLEMENTS FROM GROUP_NAMES
-		when(session.getPerunBl().getAttributesManagerBl().getAttribute(session, user, secondarySourceAttrName)).thenReturn(
-				groupNamesAttr
-		);
-
 		Attribute receivedAttr = classInstance.getAttributeValue(session, user, classInstance.getAttributeDefinition());
 		assertTrue(receivedAttr.getValue() instanceof List);
 		assertEquals("destination attribute name wrong",classInstance.getDestinationAttributeFriendlyName(),receivedAttr.getFriendlyName());
@@ -105,99 +81,9 @@ public class urn_perun_user_attribute_def_virt_eduPersonEntitlementTest {
 		@SuppressWarnings("unchecked")
 		List<String> actual = (List<String>) receivedAttr.getValue();
 		Collections.sort(actual);
-		List<String> expected = Arrays.asList(VALUE1, VALUE2, VALUE3, VALUE4, VALUE5);
-		Collections.sort(expected);
-		assertEquals("collected values are incorrect", expected, actual);
-	}
-
-	@Test
-	public void getAttributeValueOnlyGroupNames() throws Exception {
-		urn_perun_user_attribute_def_virt_eduPersonEntitlement classInstance = new urn_perun_user_attribute_def_virt_eduPersonEntitlement();
-		PerunSessionImpl session = mock(PerunSessionImpl.class, RETURNS_DEEP_STUBS);
-
-		String secondarySourceAttrName = classInstance.getSecondarySourceAttributeName();
-
-		when(session.getPerunBl().getAttributesManagerBl().getAttribute(session, user, secondarySourceAttrName)).thenReturn(
-				groupNamesAttr
-		);
-
-		Attribute receivedAttr = classInstance.getAttributeValue(session, user, classInstance.getAttributeDefinition());
-		assertTrue(receivedAttr.getValue() instanceof List);
-		assertEquals("destination attribute name wrong", classInstance.getDestinationAttributeFriendlyName(), receivedAttr.getFriendlyName());
-
-		@SuppressWarnings("unchecked")
-		List<String> actual = (List<String>) receivedAttr.getValue();
-		Collections.sort(actual);
-		List<String> expected = Arrays.asList(VALUE4, VALUE5);
-		Collections.sort(expected);
-		assertEquals("collected values are incorrect", expected, actual);
-	}
-
-	@Test
-	public void getAttributeValueOnlyFromUserExtSources() throws Exception {
-		urn_perun_user_attribute_def_virt_eduPersonEntitlement classInstance = new urn_perun_user_attribute_def_virt_eduPersonEntitlement();
-		PerunSessionImpl session = mock(PerunSessionImpl.class, RETURNS_DEEP_STUBS);
-
-		when(session.getPerunBl().getUsersManagerBl().getUserExtSources(session, user)).thenReturn(
-				Arrays.asList(ues1, ues2)
-		);
-
-		String attributeName = classInstance.getSourceAttributeName();
-		when(session.getPerunBl().getAttributesManagerBl().getAttribute(session, ues1, attributeName)).thenReturn(
-				uesAtt1
-		);
-		when(session.getPerunBl().getAttributesManagerBl().getAttribute(session, ues2, attributeName)).thenReturn(
-				uesAtt2
-		);
-
-		Attribute receivedAttr = classInstance.getAttributeValue(session, user, classInstance.getAttributeDefinition());
-		assertTrue(receivedAttr.getValue() instanceof List);
-		assertEquals("destination attribute name wrong", classInstance.getDestinationAttributeFriendlyName(), receivedAttr.getFriendlyName());
-
-		@SuppressWarnings("unchecked")
-		List<String> actual = (List<String>) receivedAttr.getValue();
-		Collections.sort(actual);
 		List<String> expected = Arrays.asList(VALUE1, VALUE2, VALUE3);
 		Collections.sort(expected);
 		assertEquals("collected values are incorrect", expected, actual);
-	}
-
-	@Test
-	public void resolveAttributeValueChangeTest() throws Exception {
-		when(session.getPerunBl().getUsersManagerBl().getUserById(session, 1)).thenReturn(user);
-
-		AuditEvent event = new AllAttributesRemovedForUserExtSource(ues1);
-		List<AuditEvent> auditEvents = classInstance.resolveVirtualAttributeValueChange(session, event);
-		assertEquals(AttributeSetForUser.class, auditEvents.get(0).getClass());
-
-		Attribute uesAttribute = new Attribute();
-		uesAttribute.setFriendlyName(classInstance.getSourceAttributeFriendlyName());
-
-		event = new AttributeSetForUes(uesAttribute, ues1);
-		auditEvents = classInstance.resolveVirtualAttributeValueChange(session, event);
-		assertEquals(AttributeSetForUser.class, auditEvents.get(0).getClass());
-
-		event = new AttributeRemovedForUes(uesAttribute, ues1);
-		auditEvents = classInstance.resolveVirtualAttributeValueChange(session, event);
-		assertEquals(AttributeSetForUser.class, auditEvents.get(0).getClass());
-
-		event = new AllAttributesRemovedForUser(user);
-		auditEvents = classInstance.resolveVirtualAttributeValueChange(session, event);
-		assertEquals(AttributeSetForUser.class, auditEvents.get(0).getClass());
-
-		Attribute attribute = new Attribute();
-		attribute.setFriendlyName(classInstance.getSecondarySourceAttributeFriendlyName());
-
-		event = new AttributeSetForUser(attribute, user);
-		auditEvents = classInstance.resolveVirtualAttributeValueChange(session, event);
-		assertEquals(AttributeSetForUser.class, auditEvents.get(0).getClass());
-
-		// change behavior to empty resulting entitlements
-		when(session.getPerunBl().getAttributesManagerBl().getAttribute(session, user, AttributesManager.NS_USER_ATTR_VIRT + ":" + "eduPersonEntitlement")).thenReturn(new Attribute());
-
-		event = new AttributeSetForUser(attribute, user);
-		auditEvents = classInstance.resolveVirtualAttributeValueChange(session, event);
-		assertEquals(AttributeRemovedForUser.class, auditEvents.get(0).getClass());
 	}
 
 }
