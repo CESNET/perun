72b19b4391fa03d952d0f3f90fdb258be73fa6b0
Pavel Zlámal <zlamal@cesnet.cz>
2019-08-16 08:49:11 +0200

    CORE: Optimized value retrieval for group:virt:denbiProjectMembers
    - We previously retrieved attributes one by one. Now we get attributes
      in once callback to DB per user.
    - Technically order of properties in JSON objects might vary, while
      previously it was in exactly same order. But order shouldn't matter
      in JSON.

--- perun-core/src/main/java/cz/metacentrum/perun/core/impl/modules/attributes/urn_perun_group_attribute_def_virt_denbiProjectMembers.java
+++ perun-core/src/main/java/cz/metacentrum/perun/core/impl/modules/attributes/urn_perun_group_attribute_def_virt_denbiProjectMembers.java
@@ -5,9 +5,7 @@ import cz.metacentrum.perun.core.api.AttributeDefinition;
 import cz.metacentrum.perun.core.api.AttributesManager;
 import cz.metacentrum.perun.core.api.Group;
 import cz.metacentrum.perun.core.api.User;
-import cz.metacentrum.perun.core.api.exceptions.AttributeNotExistsException;
 import cz.metacentrum.perun.core.api.exceptions.InternalErrorException;
-import cz.metacentrum.perun.core.api.exceptions.WrongAttributeAssignmentException;
 import cz.metacentrum.perun.core.impl.PerunSessionImpl;
 import cz.metacentrum.perun.core.implApi.modules.attributes.GroupVirtualAttributesModuleAbstract;
 import cz.metacentrum.perun.core.implApi.modules.attributes.GroupVirtualAttributesModuleImplApi;
@@ -15,52 +13,44 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-
-
 /**
- * Module for virtual group attribute.
+ * Module returns group users in JSON structure with properties: "id", "login-namespace:elixir-persistent", "login-namespace:elixir" and "preferredMail"
+ * retrieved from respective user attributes.
  *
- * Return login-namespace:userId, elixir-persistent, preferredMail and login for all user in group as JSON.
  * @author Pavel Vyskocil <vyskocilpavel@muni.cz>
  */
 public class urn_perun_group_attribute_def_virt_denbiProjectMembers extends GroupVirtualAttributesModuleAbstract implements GroupVirtualAttributesModuleImplApi  {
 
-	public static final String USER_ID = "urn:perun:user:attribute-def:core:id";
-	public static final String ELIXIR_PERSISTENT = "urn:perun:user:attribute-def:virt:login-namespace:elixir-persistent";
-	public static final String PREFERRED_MAIL = "urn:perun:user:attribute-def:def:preferredMail";
-	public static final String ELIXIR_LOGIN = "urn:perun:user:attribute-def:def:login-namespace:elixir";
+	private static final String ELIXIR_PERSISTENT = "urn:perun:user:attribute-def:virt:login-namespace:elixir-persistent";
+	private static final String PREFERRED_MAIL = "urn:perun:user:attribute-def:def:preferredMail";
+	private static final String ELIXIR_LOGIN = "urn:perun:user:attribute-def:def:login-namespace:elixir";
 
 	@Override
 	public Attribute getAttributeValue(PerunSessionImpl perunSession, Group group, AttributeDefinition attribute) throws InternalErrorException {
+
 		List<User> users = perunSession.getPerunBl().getGroupsManagerBl().getGroupUsers(perunSession, group);
 		JSONArray jsonMembers = new JSONArray();
-		for (User user: users) {
+		for (User user : users) {
 			JSONObject jsonUser = new JSONObject();
-			try{
-				Attribute userId = perunSession.getPerunBl().getAttributesManagerBl().getAttribute(perunSession, user, USER_ID);
-				jsonUser.put(userId.getFriendlyName(), userId.getValue());
-
-				Attribute elixirPersistent = perunSession.getPerunBl().getAttributesManagerBl().getAttribute(perunSession, user, ELIXIR_PERSISTENT);
-				jsonUser.put(elixirPersistent.getFriendlyName(), elixirPersistent.getValue());
-
-				Attribute preferredMail = perunSession.getPerunBl().getAttributesManagerBl().getAttribute(perunSession, user, PREFERRED_MAIL);
-				jsonUser.put(preferredMail.getFriendlyName(), preferredMail.getValue());
-
-				Attribute elixirLogin = perunSession.getPerunBl().getAttributesManagerBl().getAttribute(perunSession, user, ELIXIR_LOGIN);
-				jsonUser.put(elixirLogin.getFriendlyName(), elixirLogin.getValue());
-
+			try {
+				List<Attribute> attributes = perunSession.getPerunBl().getAttributesManagerBl().getAttributes(perunSession, user, Arrays.asList(ELIXIR_PERSISTENT, ELIXIR_LOGIN, PREFERRED_MAIL));
+				jsonUser.put("id", user.getId());
+				for (Attribute attr : attributes) {
+					jsonUser.put(attr.getFriendlyName(), attr.getValue());
+				}
 				jsonMembers.put(jsonUser);
-
-			} catch (JSONException | AttributeNotExistsException | WrongAttributeAssignmentException e){
+			} catch (JSONException e){
 				throw new InternalErrorException(e);
 			}
 		}
+
 		Attribute members = new Attribute(attribute);
 		members.setValue(jsonMembers.toString());
 		return members;
+
 	}
 
 	//IMPORTANT - this is very performance demanding operation, we will skip it
