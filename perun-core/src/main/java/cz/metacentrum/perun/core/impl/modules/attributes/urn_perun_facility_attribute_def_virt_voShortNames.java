0f01517086b219c316aea87b7b9c14ea7b5f9283
Michal Stava <stavamichal@gmail.com>
2019-08-21 13:55:41 +0200

    Merge pull request #2371 from zlamalp/voShortNames
    CORE: Updated facility:virt:voShortNames module

--- perun-core/src/main/java/cz/metacentrum/perun/core/impl/modules/attributes/urn_perun_facility_attribute_def_virt_voShortNames.java
+++ perun-core/src/main/java/cz/metacentrum/perun/core/impl/modules/attributes/urn_perun_facility_attribute_def_virt_voShortNames.java
@@ -1,11 +1,19 @@
 package cz.metacentrum.perun.core.impl.modules.attributes;
 
+import cz.metacentrum.perun.audit.events.AttributesManagerEvents.AttributeRemovedForFacility;
+import cz.metacentrum.perun.audit.events.AttributesManagerEvents.AttributeSetForFacility;
+import cz.metacentrum.perun.audit.events.AuditEvent;
+import cz.metacentrum.perun.audit.events.ResourceManagerEvents.ResourceCreated;
+import cz.metacentrum.perun.audit.events.ResourceManagerEvents.ResourceDeleted;
 import cz.metacentrum.perun.core.api.Attribute;
 import cz.metacentrum.perun.core.api.AttributeDefinition;
-import cz.metacentrum.perun.core.api.AttributesManager;
 import cz.metacentrum.perun.core.api.Facility;
 import cz.metacentrum.perun.core.api.RichResource;
+import cz.metacentrum.perun.core.api.exceptions.AttributeNotExistsException;
+import cz.metacentrum.perun.core.api.exceptions.ConsistencyErrorException;
+import cz.metacentrum.perun.core.api.exceptions.FacilityNotExistsException;
 import cz.metacentrum.perun.core.api.exceptions.InternalErrorException;
+import cz.metacentrum.perun.core.api.exceptions.WrongAttributeAssignmentException;
 import cz.metacentrum.perun.core.impl.PerunSessionImpl;
 import cz.metacentrum.perun.core.implApi.modules.attributes.FacilityVirtualAttributesModuleAbstract;
 import cz.metacentrum.perun.core.implApi.modules.attributes.FacilityVirtualAttributesModuleImplApi;
@@ -14,6 +22,8 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+
+import static cz.metacentrum.perun.core.api.AttributesManager.NS_FACILITY_ATTR_VIRT;
 
 /**
  * List of VO short names, which have resources on this facility.
@@ -43,12 +53,44 @@ public class urn_perun_facility_attribute_def_virt_voShortNames extends Facility
 	@Override
 	public AttributeDefinition getAttributeDefinition() {
 		AttributeDefinition attr = new AttributeDefinition();
-		attr.setNamespace(AttributesManager.NS_FACILITY_ATTR_VIRT);
+		attr.setNamespace(NS_FACILITY_ATTR_VIRT);
 		attr.setFriendlyName("voShortNames");
 		attr.setDisplayName("VO shortNames");
 		attr.setType(ArrayList.class.getName());
 		attr.setDescription("List of VOs short names, which have resources on this facility.");
 		return attr;
+	}
+
+	@Override
+	public List<AuditEvent> resolveVirtualAttributeValueChange(PerunSessionImpl sess, AuditEvent message) throws InternalErrorException, AttributeNotExistsException, WrongAttributeAssignmentException {
+		List<AuditEvent> resolvingMessages = new ArrayList<>();
+		if (message == null) return resolvingMessages;
+
+		if (message instanceof ResourceCreated) {
+			try {
+				Facility facility = sess.getPerunBl().getFacilitiesManagerBl().getFacilityById(sess, ((ResourceCreated) message).getResource().getFacilityId());
+				resolvingMessages.addAll(resolveEvent(sess, facility));
+			} catch (FacilityNotExistsException e) {
+				throw new ConsistencyErrorException("Facility for created Resource doesn't exists when resolving messages.", e);
+			}
+		} else if (message instanceof ResourceDeleted) {
+			resolvingMessages.addAll(resolveEvent(sess, ((ResourceDeleted) message).getFacility()));
+		}
+		return resolvingMessages;
+	}
+
+	private List<AuditEvent> resolveEvent(PerunSessionImpl sess, Facility facility) throws InternalErrorException, AttributeNotExistsException, WrongAttributeAssignmentException {
+		List<AuditEvent> resolvingMessages = new ArrayList<>();
+
+		Attribute attribute = sess.getPerunBl().getAttributesManagerBl().getAttribute(sess, facility, NS_FACILITY_ATTR_VIRT+":voShortNames");
+		if (attribute.valueAsList() == null || attribute.valueAsList().isEmpty()){
+			AttributeDefinition attributeDefinition = new AttributeDefinition(attribute);
+			resolvingMessages.add(new AttributeRemovedForFacility(attributeDefinition, facility));
+		} else {
+			resolvingMessages.add(new AttributeSetForFacility(attribute, facility));
+		}
+
+		return resolvingMessages;
 	}
 
 }
