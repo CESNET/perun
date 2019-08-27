df806776eb313fec6efa1dee808716e065ee8c5b
Pavel Zlámal <zlamal@cesnet.cz>
2019-08-27 14:58:34 +0200

    CORE, GUI, CLI: Support new windows destinations
    - Added constants and checks for new windows type destinations.
    - They are expected to contains hostname or user@hostname values,
      hence we treat them as standard host like destinations when they
      are added.
    - Added support for their setting from GUI.
    - Added support in CLI (documentation, whitespace/typo fixes).

--- perun-core/src/main/java/cz/metacentrum/perun/core/entry/ServicesManagerEntry.java
+++ perun-core/src/main/java/cz/metacentrum/perun/core/entry/ServicesManagerEntry.java
@@ -456,7 +456,9 @@ public class ServicesManagerEntry implements ServicesManager {
 		List<Facility> facilitiesByDestination = new ArrayList<>();
 		if(destination.getType().equals(Destination.DESTINATIONHOSTTYPE) ||
 				destination.getType().equals(Destination.DESTINATIONUSERHOSTTYPE) ||
-				destination.getType().equals(Destination.DESTINATIONUSERHOSTPORTTYPE)) {
+				destination.getType().equals(Destination.DESTINATIONUSERHOSTPORTTYPE) ||
+				destination.getType().equals(Destination.DESTINATIONWINDOWS) ||
+				destination.getType().equals(Destination.DESTINATIONWINDOWSPROXY)) {
 			facilitiesByHostname = getPerunBl().getFacilitiesManagerBl().getFacilitiesByHostName(perunSession, destination.getHostNameFromDestination());
 			if(facilitiesByHostname.isEmpty()) facilitiesByDestination = getPerunBl().getFacilitiesManagerBl().getFacilitiesByDestination(perunSession, destination.getHostNameFromDestination());
 
@@ -509,7 +511,9 @@ public class ServicesManagerEntry implements ServicesManager {
 		List<Facility> facilitiesByDestination = new ArrayList<>();
 		if(destination.getType().equals(Destination.DESTINATIONHOSTTYPE) ||
 				destination.getType().equals(Destination.DESTINATIONUSERHOSTTYPE) ||
-				destination.getType().equals(Destination.DESTINATIONUSERHOSTPORTTYPE)) {
+				destination.getType().equals(Destination.DESTINATIONUSERHOSTPORTTYPE) ||
+				destination.getType().equals(Destination.DESTINATIONWINDOWS) ||
+				destination.getType().equals(Destination.DESTINATIONWINDOWSPROXY)) {
 			facilitiesByHostname = getPerunBl().getFacilitiesManagerBl().getFacilitiesByHostName(sess, destination.getHostNameFromDestination());
 			if(facilitiesByHostname.isEmpty()) facilitiesByDestination = getPerunBl().getFacilitiesManagerBl().getFacilitiesByDestination(sess, destination.getHostNameFromDestination());
 
