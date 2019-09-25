8a2aa56a360c2f9bb95cae5b7c81710878907945
Pavel Zlámal <zlamal@cesnet.cz>
2019-09-25 11:51:32 +0200

    Bumped development version to 3.9.0-SNAPSHOT

--- perun-cli/Perun/Agent.pm
+++ perun-cli/Perun/Agent.pm
@@ -1,6 +1,6 @@
 package Perun::Agent;
 my $agentVersionMajor = '3';
-my $agentVersionMinor = '8';
+my $agentVersionMinor = '9';
 my $agentVersionPatch = '0';
 my $agentVersion = $agentVersionMajor . "." . $agentVersionMinor . "." . $agentVersionPatch;
 
