b7bd527f2c842f19df0e650c2c5e6f42963f9c5e
Pavel Zlámal <zlamal@cesnet.cz>
2019-09-25 11:00:40 +0200

    Merge pull request #2410 from stavamichal/orcidWillBeReadFromDiffAttribute
    Change in source attribute for attribute with collected orcid

--- perun-core/src/main/java/cz/metacentrum/perun/core/impl/modules/attributes/urn_perun_user_attribute_def_virt_eduPersonORCID.java
+++ perun-core/src/main/java/cz/metacentrum/perun/core/impl/modules/attributes/urn_perun_user_attribute_def_virt_eduPersonORCID.java
@@ -1,10 +1,7 @@
 package cz.metacentrum.perun.core.impl.modules.attributes;
 
-import cz.metacentrum.perun.core.api.PerunSession;
-import cz.metacentrum.perun.core.api.UserExtSource;
 import cz.metacentrum.perun.core.implApi.modules.attributes.SkipValueCheckDuringDependencyCheck;
 import cz.metacentrum.perun.core.implApi.modules.attributes.UserVirtualAttributeCollectedFromUserExtSource;
-import org.apache.commons.lang3.StringUtils;
 
 /**
  * ORCIDs collected from UserExtSources attributes.
@@ -17,7 +14,7 @@ public class urn_perun_user_attribute_def_virt_eduPersonORCID extends UserVirtua
 
 	@Override
 	public String getSourceAttributeFriendlyName() {
-		return "eppn";
+		return "eduPersonOrcid";
 	}
 
 	@Override
@@ -28,15 +25,5 @@ public class urn_perun_user_attribute_def_virt_eduPersonORCID extends UserVirtua
 	@Override
 	public String getDestinationAttributeDescription() {
 		return "All ORCIDs of a user";
-	}
-
-	@Override
-	public String modifyValue(PerunSession session, ModifyValueContext ctx, UserExtSource ues, String value) {
-		if(value.endsWith("@orcid")) {
-			return "http://orcid.org/"+StringUtils.substringBefore(value,"@");
-		} else {
-			return null;
-		}
-
 	}
 }
