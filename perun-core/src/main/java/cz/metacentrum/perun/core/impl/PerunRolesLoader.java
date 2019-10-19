04825f58b9430ed964e268c6d750d9d643029f18
Peter Balcirak <peter.balcirak@gmail.com>
2019-10-19 10:01:53 +0200

    Load and get perun policies
    - PerunRolesLoader was extended to also support loading perun policies
      from the configuration file.
    - New class PerunPoliciesContainer was created which will store the
      policies.
    - AuthzResolverImpl will load the policies to the PerunPoliciesContainer
      during the initialize() method. It has now also method
      getPolicy(String policyName) which will fetch the given policy from the
      container.

--- perun-core/src/main/java/cz/metacentrum/perun/core/impl/PerunRolesLoader.java
+++ perun-core/src/main/java/cz/metacentrum/perun/core/impl/PerunRolesLoader.java
@@ -14,7 +14,10 @@ import org.springframework.core.io.Resource;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 /**
  * The purpose of the PerunRolesLoader is to load perun roles and policies from the perun-roles.yml configuration file.
@@ -31,17 +34,10 @@ public class PerunRolesLoader {
 	public void loadPerunRoles(JdbcPerunTemplate jdbc) {
 		if (BeansUtils.isPerunReadOnly()) log.debug("Loading authzresolver manager init in readOnly version.");
 
-		List<String> roles;
-		ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+		JsonNode rootNode = loadConfigurationFile();
 
-		try (InputStream is = configurationPath.getInputStream()) {
-			JsonNode rolesNode = mapper.readTree(is).get("perun_roles");
-			roles = new ObjectMapper().convertValue(rolesNode, new TypeReference<List<String>>() {});
-		} catch (FileNotFoundException e) {
-			throw new InternalErrorException("Configuration file not found for perun roles. It should be in: " + configurationPath, e);
-		} catch (IOException e) {
-			throw new InternalErrorException("IO exception was thrown during the processing of the file: " + configurationPath, e);
-		}
+		JsonNode rolesNode = rootNode.get("perun_roles");
+		List<String> roles = new ObjectMapper().convertValue(rolesNode, new TypeReference<List<String>>() {});
 
 		// Check if all roles defined in class Role exists in the DB
 		for (String role : roles) {
@@ -59,6 +55,35 @@ public class PerunRolesLoader {
 				throw new InternalErrorException(e);
 			}
 		}
+	}
+
+	public Map<String, JsonNode> loadPerunPolicies() {
+		Map<String, JsonNode> policies = new HashMap<>();
+		JsonNode rootNode = loadConfigurationFile();
+		JsonNode policiesNode = rootNode.get("perun_policies");
+
+		Iterator<String> policyNames = policiesNode.fieldNames();
+		while(policyNames.hasNext()) {
+			String policyname = policyNames.next();
+			policies.put(policyname, policiesNode.get(policyname));
+		}
+
+		return policies;
+	}
+
+	private JsonNode loadConfigurationFile() {
+		ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+
+		JsonNode rootNode;
+		try (InputStream is = configurationPath.getInputStream()) {
+			rootNode = mapper.readTree(is);
+		} catch (FileNotFoundException e) {
+			throw new InternalErrorException("Configuration file not found for perun roles. It should be in: " + configurationPath, e);
+		} catch (IOException e) {
+			throw new InternalErrorException("IO exception was thrown during the processing of the file: " + configurationPath, e);
+		}
+
+		return rootNode;
 	}
 
 	public void setConfigurationPath(Resource configurationPath) {
