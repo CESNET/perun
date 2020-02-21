72c5ed8ee3b615e363935cb21ba4997f4aab862b
Pavel Zlámal <zlamal@cesnet.cz>
2020-02-21 10:55:13 +0100

    Added user virt attribute modules for eIDAS and europeanStudentId
    - We will collect all values from ues:def:eIDASPersonIdentifier
      of user into user:virt:eIDASPersonIdentifiers and it will be
      pushed to the LDAP.
      Same goes for ues:def:europeanStudentID which will be collected
      in user:virt:europeanStudentIDs and also pushed to the LDAP.

---
+++ perun-core/src/main/java/cz/metacentrum/perun/core/impl/modules/attributes/urn_perun_user_attribute_def_virt_europeanStudentIDs.java
@@ -0,0 +1,35 @@
+package cz.metacentrum.perun.core.impl.modules.attributes;
+
+import cz.metacentrum.perun.core.implApi.modules.attributes.SkipValueCheckDuringDependencyCheck;
+import cz.metacentrum.perun.core.implApi.modules.attributes.UserVirtualAttributeCollectedFromUserExtSource;
+
+/**
+ * All europeanStudentIDs collected from UserExtSources attributes europeanStudentID.
+ *
+ * @author Pavel Zlamal <zlamal@cesnet.cz>
+ */
+@SuppressWarnings("unused")
+@SkipValueCheckDuringDependencyCheck
+public class urn_perun_user_attribute_def_virt_europeanStudentIDs extends UserVirtualAttributeCollectedFromUserExtSource {
+
+	@Override
+	public String getSourceAttributeFriendlyName() {
+		return "europeanStudentID";
+	}
+
+	@Override
+	public String getDestinationAttributeFriendlyName() {
+		return "europeanStudentIDs";
+	}
+
+	@Override
+	public String getDestinationAttributeDisplayName() {
+		return "europeanStudentIDs";
+	}
+
+	@Override
+	public String getDestinationAttributeDescription() {
+		return "All european student IDs of a user.";
+	}
+
+}
