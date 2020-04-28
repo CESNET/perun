69cb862dbd7ecea96428b690ca96ce683d530af6
Pavel Zlámal <zlamal@cesnet.cz>
2020-04-28 11:39:42 +0200

    CORE: Custom implementation of alt password manager in einfra
    - Only create/delete param is passed to the command directly
    - We now pass all other necessary properties as ENV variables,
      so the script doesn't have to ask perun for additional data.
    - We do not set empty env variables.

--- perun-core/src/main/java/cz/metacentrum/perun/core/impl/modules/pwdmgr/EinfraPasswordManagerModule.java
+++ perun-core/src/main/java/cz/metacentrum/perun/core/impl/modules/pwdmgr/EinfraPasswordManagerModule.java
@@ -1,8 +1,21 @@
 package cz.metacentrum.perun.core.impl.modules.pwdmgr;
 
+import cz.metacentrum.perun.core.api.Attribute;
+import cz.metacentrum.perun.core.api.AttributesManager;
 import cz.metacentrum.perun.core.api.PerunSession;
 import cz.metacentrum.perun.core.api.User;
+import cz.metacentrum.perun.core.api.exceptions.AttributeNotExistsException;
 import cz.metacentrum.perun.core.api.exceptions.InternalErrorException;
+import cz.metacentrum.perun.core.api.exceptions.WrongAttributeAssignmentException;
+import cz.metacentrum.perun.core.api.exceptions.rt.EmptyPasswordRuntimeException;
+import cz.metacentrum.perun.core.api.exceptions.rt.LoginNotExistsRuntimeException;
+import cz.metacentrum.perun.core.bl.PerunBl;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.Map;
 
 /**
  * Password manager for EINFRA login-namespace. It performs custom checks
@@ -13,6 +26,8 @@ import cz.metacentrum.perun.core.api.exceptions.InternalErrorException;
  * @author Pavel Zlamal <zlamal@cesnet.cz>
  */
 public class EinfraPasswordManagerModule extends GenericPasswordManagerModule {
+
+	private final static Logger log = LoggerFactory.getLogger(EinfraPasswordManagerModule.class);
 
 	public EinfraPasswordManagerModule() {
 
@@ -53,8 +68,101 @@ public class EinfraPasswordManagerModule extends GenericPasswordManagerModule {
 
 	@Override
 	public void createAlternativePassword(PerunSession sess, User user, String passwordId, String password) {
+		if (StringUtils.isBlank(password)) {
+			throw new EmptyPasswordRuntimeException("Password for " + actualLoginNamespace + ":" + passwordId + " cannot be empty.");
+		}
 		checkPasswordStrength(password);
-		super.createAlternativePassword(sess, user, passwordId, password);
+
+		ProcessBuilder pb = new ProcessBuilder(altPasswordManagerProgram, PASSWORD_CREATE);
+		// pass variables as ENV
+		Map<String,String> env = pb.environment();
+		env.put("PMGR_PASSWORD_ID", passwordId);
+		env.put("PMGR_PASSWORD", password);
+		if (StringUtils.isNotBlank(user.getDisplayName())) env.put("PMGR_CN", user.getDisplayName());
+		if (StringUtils.isNotBlank(user.getFirstName())) env.put("PMGR_GIVEN_NAME", user.getFirstName());
+		if (StringUtils.isNotBlank(user.getLastName())) env.put("PMGR_SN", user.getLastName());
+		String mail = getMail(sess, user);
+		if (StringUtils.isNotBlank(mail)) env.put("PMGR_MAIL", mail);
+		String login = getEinfraLogin(sess, user);
+		if (StringUtils.isNotBlank(login)) env.put("PMGR_LOGIN", login);
+
+		Process process;
+		try {
+			process = pb.start();
+		} catch (IOException e) {
+			throw new InternalErrorException(e);
+		}
+
+		handleAltPwdManagerExit(process, user, actualLoginNamespace, passwordId);
+
+	}
+
+	@Override
+	public void deleteAlternativePassword(PerunSession sess, User user, String passwordId) {
+
+		ProcessBuilder pb = new ProcessBuilder(altPasswordManagerProgram, PASSWORD_DELETE);
+		// pass variables as ENV
+		Map<String,String> env = pb.environment();
+		env.put("PMGR_PASSWORD_ID", passwordId);
+		String login = getEinfraLogin(sess, user);
+		if (StringUtils.isNotBlank(login)) env.put("PMGR_LOGIN", login);
+
+		Process process;
+		try {
+			process = pb.start();
+		} catch (IOException e) {
+			throw new InternalErrorException(e);
+		}
+
+		handleAltPwdManagerExit(process, user, actualLoginNamespace, passwordId);
+
+	}
+
+	/**
+	 * Retrieve "einfra" login of the user, since its necessary for alternative password backend
+	 *
+	 * @param session session
+	 * @param user user to get login for
+	 * @return einfra login
+	 * @throws LoginNotExistsRuntimeException if user doesn't have "einfra" login
+	 */
+	private String getEinfraLogin(PerunSession session, User user) {
+
+		String login = null;
+		try {
+			Attribute attribute = ((PerunBl)session.getPerun()).getAttributesManagerBl().getAttribute(session, user, AttributesManager.NS_USER_ATTR_DEF+":login-namespace:einfra");
+			if (attribute.getValue() == null) {
+				log.warn("{} doesn't have login in namespace 'einfra', so the alternative password can't be set.", user);
+				throw new LoginNotExistsRuntimeException(user+" doesn't have login in namespace 'einfra', so the alternative password can't be set.");
+			}
+			login = attribute.valueAsString();
+		} catch (WrongAttributeAssignmentException | AttributeNotExistsException e) {
+			// shouldn't happen
+			log.error("We couldn't retrieve 'einfra' login for the {} to create/delete alternative password.", user, e);
+			throw new InternalErrorException("We couldn't retrieve 'einfra' login for the "+user+" to create/delete alternative password.");
+		}
+		return login;
+
+	}
+
+	/**
+	 * Retrieve users preferred mail
+	 *
+	 * @param session session
+	 * @param user user to get mail for
+	 * @return users preferred mail
+	 */
+	private String getMail(PerunSession session, User user) {
+
+		try {
+			Attribute attribute = ((PerunBl)session.getPerun()).getAttributesManagerBl().getAttribute(session, user, AttributesManager.NS_USER_ATTR_DEF+":preferredMail");
+			return attribute.valueAsString();
+		} catch (WrongAttributeAssignmentException | AttributeNotExistsException e) {
+			// shouldn't happen
+			log.error("We couldn't retrieve mail for the {} to create/delete alternative password.", user, e);
+			throw new InternalErrorException("We couldn't retrieve mail for the "+user+" to create/delete alternative password.");
+		}
+
 	}
 
 	private void checkLoginFormat(String userLogin) {
