75eca932a5cdad624e434e4a58932a8346fa30ba
Martin Kuba <makub@ics.muni.cz>
2020-05-04 19:19:11 +0200

    removed limit on column ext_sources_attributes.attr_value

--- perun-db/oracle.sql
+++ perun-db/oracle.sql
@@ -1,4 +1,4 @@
--- database version 3.1.58 (don't forget to update insert statement at the end of file)
+-- database version 3.1.59 (don't forget to update insert statement at the end of file)
 
 create user perunv3 identified by password;
 grant create session to perunv3;
@@ -1055,7 +1055,7 @@ create table ext_sources (
 create table ext_sources_attributes (
 	ext_sources_id integer not null,   --identifier of ext. source (ext_sources.id)
 	attr_name nvarchar2(128) not null,   --name of attribute at ext. source
-	attr_value nvarchar2(4000),          --value of attribute
+	attr_value clob,          --value of attribute
 	created_at date default sysdate not null,
 	created_by nvarchar2(1300) default user not null,
 	modified_at date default sysdate not null,
@@ -1754,7 +1754,7 @@ CREATE INDEX ufauv_idx ON user_facility_attr_u_values (user_id, facility_id, att
 CREATE INDEX vauv_idx ON vo_attr_u_values (vo_id, attr_id) ;
 
 -- set initial Perun DB version
-insert into configurations values ('DATABASE VERSION','3.1.58');
+insert into configurations values ('DATABASE VERSION','3.1.59');
 
 -- insert membership types
 insert into membership_types (id, membership_type, description) values (1, 'DIRECT', 'Member is directly added into group');
