0e575f77ffaf9478e60baf8346f123e2092b2e7d
Michal Stava <stavamichal@gmail.com>
2020-05-14 10:20:38 +0200

    Merge pull request #2698 from martin-kuba/cli_java_GetOwnerFacilities
    added command GetOwnerFacilities to java CLI

---
+++ perun-cli-java/src/main/java/cz/metacentrum/perun/cli/commands/GetOwnerFacilities.java
@@ -0,0 +1,43 @@
+package cz.metacentrum.perun.cli.commands;
+
+import cz.metacentrum.perun.cli.PerunCLI;
+import cz.metacentrum.perun.cli.PerunCommand;
+import cz.metacentrum.perun.openapi.model.Facility;
+import cz.metacentrum.perun.openapi.model.Owner;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.springframework.web.client.RestClientException;
+
+import java.util.List;
+
+/**
+ * Lists Facilites owned by an Owner.
+ *
+ * @author Martin Kuba makub@ics.muni.cz
+ */
+@SuppressWarnings("unused")
+public class GetOwnerFacilities extends PerunCommand {
+
+	@Override
+	public String getCommandDescription() {
+		return "lists Facilities owned by an Owner";
+	}
+
+	@Override
+	public void addOptions(Options options) {
+		options.addOption(Option.builder("o").required(true).hasArg(true).longOpt("ownerId").desc("owner id").build());
+	}
+
+	@Override
+	public void executeCommand(PerunCLI.CommandContext ctx) throws RestClientException {
+		int ownerId = Integer.parseInt(ctx.getCommandLine().getOptionValue("o"));
+		Owner owner = ctx.getPerunRPC().getOwnersManager().getOwnerById(ownerId);
+		List<Facility> facilities = ctx.getPerunRPC().getFacilitiesManager().getOwnerFacilities(ownerId);
+		System.out.println("Facilities owned by owner "+owner.getId()+" "+owner.getName()+" "+owner.getContact()+" "+owner.getType());
+		System.out.println("=====================================================================================");
+		for (Facility facility : facilities) {
+			System.out.printf("%5d %40s %-40s%n", facility.getId(), facility.getName(), facility.getDescription());
+		}
+	}
+
+}
