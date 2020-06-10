37a7c18b9ed233f8648da4475e519dd11b10ecf2
Pavel Zlámal <zlamal@cesnet.cz>
2020-06-10 13:51:21 +0200

    Merge pull request #2725 from Vojtech-Sassmann/refactorPersistentShadowModules
    Refactorization of persistent shadow modules

--- perun-core/src/main/java/cz/metacentrum/perun/core/impl/modules/attributes/urn_perun_user_attribute_def_def_login_namespace_fenix_persistent_shadow.java
+++ perun-core/src/main/java/cz/metacentrum/perun/core/impl/modules/attributes/urn_perun_user_attribute_def_def_login_namespace_fenix_persistent_shadow.java
@@ -1,119 +1,48 @@
 package cz.metacentrum.perun.core.impl.modules.attributes;
 
-import cz.metacentrum.perun.core.api.Attribute;
-import cz.metacentrum.perun.core.api.AttributeDefinition;
-import cz.metacentrum.perun.core.api.AttributesManager;
-import cz.metacentrum.perun.core.api.ExtSource;
-import cz.metacentrum.perun.core.api.User;
-import cz.metacentrum.perun.core.api.UserExtSource;
-import cz.metacentrum.perun.core.api.exceptions.ExtSourceNotExistsException;
-import cz.metacentrum.perun.core.api.exceptions.InternalErrorException;
-import cz.metacentrum.perun.core.api.exceptions.UserExtSourceExistsException;
-import cz.metacentrum.perun.core.impl.PerunSessionImpl;
-import cz.metacentrum.perun.core.impl.modules.ModulesConfigLoader;
-import cz.metacentrum.perun.core.impl.modules.ModulesYamlConfigLoader;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import cz.metacentrum.perun.core.implApi.modules.attributes.UserPersistentShadowAttributeWithConfig;
 
 /**
  * Class for checking logins uniqueness in the namespace and filling fenix-persistent id.
  * It is only storage! Use module login fenix_persistent for access the value.
  *
  */
-public class urn_perun_user_attribute_def_def_login_namespace_fenix_persistent_shadow extends urn_perun_user_attribute_def_def_login_namespace {
+public class urn_perun_user_attribute_def_def_login_namespace_fenix_persistent_shadow
+		extends UserPersistentShadowAttributeWithConfig {
 
-	private final static Logger log = LoggerFactory.getLogger(urn_perun_user_attribute_def_def_login_namespace_fenix_persistent_shadow.class);
 	private final static String attrNameFenix = "login-namespace:fenix-persistent-shadow";
 
 	private final static String CONFIG_EXT_SOURCE_NAME_FENIX = "extSourceNameFenix";
 	private final static String CONFIG_DOMAIN_NAME_FENIX = "domainNameFenix";
 
-	private ModulesConfigLoader loader = new ModulesYamlConfigLoader();
-	private String extSourceNameFenix = null;
-	private String domainNameFenix = null;
-
-	public urn_perun_user_attribute_def_def_login_namespace_fenix_persistent_shadow() { }
-
-	public urn_perun_user_attribute_def_def_login_namespace_fenix_persistent_shadow(ModulesConfigLoader loader) {
-		this.loader = loader;
-	}
-
-	/**
-	 * Filling implemented for login:namespace:fenix-persistent attribute
-	 * Format is: "[hash]@fenix-europe.org" where [hash] represents sha1hash counted from user's id and perun instance id a login-namespace name
-	 *
-	 * @param perunSession PerunSession
-	 * @param user User to fill attribute for
-	 * @param attribute Attribute to fill value to
-	 * @return Filled attribute
-	 * @throws InternalErrorException
-	 */
 	@Override
-	public Attribute fillAttribute(PerunSessionImpl perunSession, User user, AttributeDefinition attribute) throws InternalErrorException {
-
-		Attribute filledAttribute = new Attribute(attribute);
-
-		if (attribute.getFriendlyName().equals(attrNameFenix)) {
-			String domain = "@" + getDomainNameFenix();
-			filledAttribute.setValue(sha1HashCount(user, domain).toString() + domain);
-			return filledAttribute;
-		} else {
-			// without value
-			return filledAttribute;
-		}
+	public String getExtSourceConfigName() {
+		return CONFIG_EXT_SOURCE_NAME_FENIX;
 	}
 
-	/**
-	 * ChangedAttributeHook() sets UserExtSource with following properties:
-	 *  - extSourceType is IdP
-	 *  - extSourceName is https://engine.fenix-idp.ics.muni.cz/authentication/idp/metadata
-	 *  - user's extSource login is the same as his fenix-persistent attribute
-	 *
-	 * @param session PerunSession
-	 * @param user User to set UserExtSource for
-	 * @param attribute Attribute containing fenixID
-	 * @throws cz.metacentrum.perun.core.api.exceptions.InternalErrorException
-	 */
 	@Override
-	public void changedAttributeHook(PerunSessionImpl session, User user, Attribute attribute) throws InternalErrorException {
-		try {
-			String userNamespace = attribute.getFriendlyNameParameter();
-
-			if(userNamespace.equals("fenix-persistent-shadow") && attribute.getValue() != null){
-				ExtSource extSource = session.getPerunBl().getExtSourcesManagerBl().getExtSourceByName(session, getExtSourceNameFenix());
-				UserExtSource userExtSource = new UserExtSource(extSource, 0, attribute.getValue().toString());
-
-				session.getPerunBl().getUsersManagerBl().addUserExtSource(session, user, userExtSource);
-			}
-		} catch (UserExtSourceExistsException ex) {
-			log.warn("Fenix IdP external source already exists for the user.", ex);
-		} catch (ExtSourceNotExistsException ex) {
-			throw new InternalErrorException("IdP external source for fenix doesn't exist.", ex);
-		}
+	public String getDomainConfigName() {
+		return CONFIG_DOMAIN_NAME_FENIX;
 	}
 
-	public AttributeDefinition getAttributeDefinition() {
-		AttributeDefinition attr = new AttributeDefinition();
-		attr.setNamespace(AttributesManager.NS_USER_ATTR_DEF);
-		attr.setFriendlyName("login-namespace:fenix-persistent-shadow");
-		attr.setDisplayName("FENIX login");
-		attr.setType(String.class.getName());
-		attr.setDescription("Login for FENIX. Do not use it directly! " +
-				"Use \"user:virt:login-namespace:fenix-persistent\" attribute instead.");
-		return attr;
+	@Override
+	public String getFriendlyName() {
+		return attrNameFenix;
 	}
 
-	public String getExtSourceNameFenix() {
-		if (extSourceNameFenix == null) {
-			extSourceNameFenix = loader.loadString(getClass().getSimpleName(), CONFIG_EXT_SOURCE_NAME_FENIX);
-		}
-		return extSourceNameFenix;
+	@Override
+	public String getDescription() {
+		return "Login for FENIX. Do not use it directly! " +
+			   "Use \"user:virt:login-namespace:fenix-persistent\" attribute instead.";
 	}
 
-	public String getDomainNameFenix() {
-		if (domainNameFenix == null) {
-			domainNameFenix = loader.loadString(getClass().getSimpleName(), CONFIG_DOMAIN_NAME_FENIX);
-		}
-		return domainNameFenix;
+	@Override
+	public String getFriendlyNameParameter() {
+		return "fenix-persistent-shadow";
+	}
+
+	@Override
+	public String getDisplayName() {
+		return "FENIX login";
 	}
 }
--- perun-core/src/main/java/cz/metacentrum/perun/core/impl/modules/attributes/urn_perun_user_attribute_def_def_login_namespace_surf_ram_persistent_shadow.java
+++ perun-core/src/main/java/cz/metacentrum/perun/core/impl/modules/attributes/urn_perun_user_attribute_def_def_login_namespace_surf_ram_persistent_shadow.java
@@ -1,119 +1,48 @@
 package cz.metacentrum.perun.core.impl.modules.attributes;
 
-import cz.metacentrum.perun.core.api.Attribute;
-import cz.metacentrum.perun.core.api.AttributeDefinition;
-import cz.metacentrum.perun.core.api.AttributesManager;
-import cz.metacentrum.perun.core.api.ExtSource;
-import cz.metacentrum.perun.core.api.User;
-import cz.metacentrum.perun.core.api.UserExtSource;
-import cz.metacentrum.perun.core.api.exceptions.ExtSourceNotExistsException;
-import cz.metacentrum.perun.core.api.exceptions.InternalErrorException;
-import cz.metacentrum.perun.core.api.exceptions.UserExtSourceExistsException;
-import cz.metacentrum.perun.core.impl.PerunSessionImpl;
-import cz.metacentrum.perun.core.impl.modules.ModulesConfigLoader;
-import cz.metacentrum.perun.core.impl.modules.ModulesYamlConfigLoader;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import cz.metacentrum.perun.core.implApi.modules.attributes.UserPersistentShadowAttributeWithConfig;
 
 /**
  * Class for checking logins uniqueness in the namespace and filling surf-ram-persistent id.
  * It is only storage! Use module login surf_ram_persistent for access the value.
  *
  */
-public class urn_perun_user_attribute_def_def_login_namespace_surf_ram_persistent_shadow extends urn_perun_user_attribute_def_def_login_namespace {
+public class urn_perun_user_attribute_def_def_login_namespace_surf_ram_persistent_shadow
+		extends UserPersistentShadowAttributeWithConfig {
 
-	private final static Logger log = LoggerFactory.getLogger(urn_perun_user_attribute_def_def_login_namespace_surf_ram_persistent_shadow.class);
 	private final static String attrNameSurfRam = "login-namespace:surf-ram-persistent-shadow";
 
 	private final static String CONFIG_EXT_SOURCE_NAME_SURF_RAM = "extSourceNameSurfRam";
 	private final static String CONFIG_DOMAIN_NAME_SURF_RAM = "domainNameSurfRam";
 
-	private ModulesConfigLoader loader = new ModulesYamlConfigLoader();
-	private String extSourceNameSurfRam = null;
-	private String domainNameSurfRam = null;
-
-	public urn_perun_user_attribute_def_def_login_namespace_surf_ram_persistent_shadow() { }
-
-	public urn_perun_user_attribute_def_def_login_namespace_surf_ram_persistent_shadow(ModulesConfigLoader loader) {
-		this.loader = loader;
-	}
-
-	/**
-	 * Filling implemented for login:namespace:surf-ram-persistent attribute
-	 * Format is: "[hash]@domainNameSurfRam" where [hash] represents sha1hash counted from user's id and perun instance id a login-namespace name
-	 *
-	 * @param perunSession PerunSession
-	 * @param user User to fill attribute for
-	 * @param attribute Attribute to fill value to
-	 * @return Filled attribute
-	 * @throws InternalErrorException
-	 */
 	@Override
-	public Attribute fillAttribute(PerunSessionImpl perunSession, User user, AttributeDefinition attribute) throws InternalErrorException {
-
-		Attribute filledAttribute = new Attribute(attribute);
-
-		if (attribute.getFriendlyName().equals(attrNameSurfRam)) {
-			String domain = "@" + getDomainNameSurfRam();
-			filledAttribute.setValue(sha1HashCount(user, domain).toString() + domain);
-			return filledAttribute;
-		} else {
-			// without value
-			return filledAttribute;
-		}
+	public String getExtSourceConfigName() {
+		return CONFIG_EXT_SOURCE_NAME_SURF_RAM;
 	}
 
-	/**
-	 * ChangedAttributeHook() sets UserExtSource with following properties:
-	 *  - extSourceType is IdP
-	 *  - extSourceName is stored in extSourceNameSurfRam
-	 *  - user's extSource login is the same as his surf-ram-persistent attribute
-	 *
-	 * @param session PerunSession
-	 * @param user User to set UserExtSource for
-	 * @param attribute Attribute containing SURF RAM ID
-	 * @throws cz.metacentrum.perun.core.api.exceptions.InternalErrorException
-	 */
 	@Override
-	public void changedAttributeHook(PerunSessionImpl session, User user, Attribute attribute) throws InternalErrorException {
-		try {
-			String userNamespace = attribute.getFriendlyNameParameter();
-
-			if(userNamespace.equals("surf-ram-persistent-shadow") && attribute.getValue() != null){
-				ExtSource extSource = session.getPerunBl().getExtSourcesManagerBl().getExtSourceByName(session, getExtSourceNameSurfRam());
-				UserExtSource userExtSource = new UserExtSource(extSource, 0, attribute.getValue().toString());
-
-				session.getPerunBl().getUsersManagerBl().addUserExtSource(session, user, userExtSource);
-			}
-		} catch (UserExtSourceExistsException ex) {
-			log.warn("SurfRam IdP external source already exists for the user.", ex);
-		} catch (ExtSourceNotExistsException ex) {
-			throw new InternalErrorException("IdP external source for SURF RAM doesn't exist.", ex);
-		}
+	public String getDomainConfigName() {
+		return CONFIG_DOMAIN_NAME_SURF_RAM;
 	}
 
-	public AttributeDefinition getAttributeDefinition() {
-		AttributeDefinition attr = new AttributeDefinition();
-		attr.setNamespace(AttributesManager.NS_USER_ATTR_DEF);
-		attr.setFriendlyName("login-namespace:surf-ram-persistent-shadow");
-		attr.setDisplayName("SURF RAM login");
-		attr.setType(String.class.getName());
-		attr.setDescription("Login for SURF RAM. Do not use it directly! " +
-				"Use \"user:virt:login-namespace:surf-ram-persistent\" attribute instead.");
-		return attr;
+	@Override
+	public String getFriendlyName() {
+		return attrNameSurfRam;
 	}
 
-	public String getExtSourceNameSurfRam() {
-		if (extSourceNameSurfRam == null) {
-			extSourceNameSurfRam = loader.loadString(getClass().getSimpleName(), CONFIG_EXT_SOURCE_NAME_SURF_RAM);
-		}
-		return extSourceNameSurfRam;
+	@Override
+	public String getDescription() {
+		return "Login for SURF RAM. Do not use it directly! " +
+			   "Use \"user:virt:login-namespace:surf-ram-persistent\" attribute instead.";
 	}
 
-	public String getDomainNameSurfRam() {
-		if (domainNameSurfRam == null) {
-			domainNameSurfRam = loader.loadString(getClass().getSimpleName(), CONFIG_DOMAIN_NAME_SURF_RAM);
-		}
-		return domainNameSurfRam;
+	@Override
+	public String getDisplayName() {
+		return "SURF RAM login";
+	}
+
+	@Override
+	public String getFriendlyNameParameter() {
+		return "surf-ram-persistent-shadow";
 	}
 }
