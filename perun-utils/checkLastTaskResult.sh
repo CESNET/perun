feade71463a01a499991fa1bfa9a0553fe914508
Pavel Zlámal <zlamal@cesnet.cz>
2020-06-16 16:41:47 +0200

    UTILS: Changed tools for NRPE commands to use local definition
    - Do not source perun-engine identity, allow local
      definition of API url and user.
    - Also properly export perl5libs.

--- perun-utils/checkLastTaskResult.sh
+++ perun-utils/checkLastTaskResult.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-# source ENGINE identity and API URL
-. /etc/perun/perun-engine
-# overwrite cookie to ours
+# define connection to Perun
+export PERUN_URL=
+export PERUN_USER=
 export PERUN_COOKIE=/var/lib/nagios/.perun-nagios-cookie.txt
+# export perl libs
+PERL5LIB="/opt/perun-cli/lib/${PERL5LIB+:}${PERL5LIB}";
+export PERL5LIB;
+export PERL5LIB=$PERL5LIB":."
+
 # run tool
 /opt/perun-cli/bin/checkLastTaskResult "$@" 2>&1 || exit 2
