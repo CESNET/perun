2694938479de6e262d98d87fd949d6ecdd4f48bc
Vojtěch Sassmann <vojtech.sassmann@gmail.com>
2020-07-14 13:20:46 +0200

    Attributes cache removed
    * The implementation of the attributes cache was not working very well.
    The Perun was actually slower with the cache enabled. We are not able to
    improve it so it can be removed.
    * Removed all classes related to the attributes cache and all of theirs
    usages.

--- perun-core/src/main/java/cz/metacentrum/perun/core/impl/PerunTransactionManager.java
+++ perun-core/src/main/java/cz/metacentrum/perun/core/impl/PerunTransactionManager.java
@@ -19,40 +19,27 @@ public class PerunTransactionManager extends DataSourceTransactionManager implem
 	private final static Logger log = LoggerFactory.getLogger(PerunLocksUtils.class);
 	private static final long serialVersionUID = 1L;
 	private Auditer auditer;
-	private CacheManager cacheManager;
 
 	@Override
 	protected void doBegin(Object transaction, TransactionDefinition definition) {
 		this.getAuditer().newTopLevelTransaction();
-		if(BeansUtils.getCoreConfig().isCacheEnabled()) {
-			this.getCacheManager().newTopLevelTransaction();
-		}
 		super.doBegin(transaction, definition);
 	}
 
 	@Override
 	protected void doCommit(DefaultTransactionStatus status) {
-		if(BeansUtils.getCoreConfig().isCacheEnabled()) {
-			this.getCacheManager().commit();
-		}
 		super.doCommit(status);
 		this.getAuditer().flush();
 	}
 
 	@Override
 	protected void doRollback(DefaultTransactionStatus status) {
-		if(BeansUtils.getCoreConfig().isCacheEnabled()) {
-			this.getCacheManager().rollback();
-		}
 		super.doRollback(status);
 		this.getAuditer().clean();
 	}
 
 	@Override
 	protected void doCleanupAfterCompletion(Object transaction) {
-		if(BeansUtils.getCoreConfig().isCacheEnabled()) {
-			this.getCacheManager().clean();
-		}
 		super.doCleanupAfterCompletion(transaction);
 
 		List<Lock> locks = (List<Lock>) TransactionSynchronizationManager.getResource(PerunLocksUtils.uniqueKey.get());
@@ -72,11 +59,4 @@ public class PerunTransactionManager extends DataSourceTransactionManager implem
 		this.auditer = auditer;
 	}
 
-	public CacheManager getCacheManager() {
-		return cacheManager;
-	}
-
-	public void setCacheManager(CacheManager cacheManager) {
-		this.cacheManager = cacheManager;
-	}
 }
