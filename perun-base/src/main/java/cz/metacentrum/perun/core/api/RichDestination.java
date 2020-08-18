2c41676986b5581f5e9758a027b5e86ca7511a22
Vojtěch Sassmann <vojtech.sassmann@gmail.com>
2020-08-18 09:52:34 +0200

    Core - RichDestinations, service denial info
    * Added a new field in RichDestination with information about
    destination blocks. If the destination is blocked, the service denial
    field contains a non-null ServiceDenial object. This object has
    information about who and when created this denial.

--- perun-base/src/main/java/cz/metacentrum/perun/core/api/RichDestination.java
+++ perun-base/src/main/java/cz/metacentrum/perun/core/api/RichDestination.java
@@ -11,17 +11,23 @@ import cz.metacentrum.perun.core.api.BeansUtils;
 public class RichDestination extends Destination implements Comparable<PerunBean> {
 	private Service service;
 	private Facility facility;
+	private boolean blocked;
 
 	public RichDestination(){
 	}
 
 	public RichDestination(Destination destination, Facility facility, Service service) {
+		this(destination, facility, service, false);
+	}
+
+	public RichDestination(Destination destination, Facility facility, Service service, boolean blocked) {
 		super(destination.getId(), destination.getDestination(), destination.getType(), destination.getCreatedAt(),
 				destination.getCreatedBy(), destination.getModifiedAt(), destination.getModifiedBy(),
 				destination.getCreatedByUid(), destination.getModifiedByUid());
 		setPropagationType(destination.getPropagationType());
 		this.service = service;
 		this.facility = facility;
+		this.blocked = blocked;
 	}
 
 	/*public RichDestination(User user, Member member, List<UserExtSource> userExtSources, List<Attribute> userAttributes, List<Attribute> memberAttributes) {
@@ -46,6 +52,13 @@ public class RichDestination extends Destination implements Comparable<PerunBean
 		this.service = service;
 	}
 
+	public boolean isBlocked() {
+		return blocked;
+	}
+
+	public void setBlocked(boolean blocked) {
+		this.blocked = blocked;
+	}
 
 	@Override
 	public int hashCode() {
@@ -95,6 +108,7 @@ public class RichDestination extends Destination implements Comparable<PerunBean
 			", type=<").append(super.getType() == null ? "\\0" : BeansUtils.createEscaping(super.getType())).append(">").append(
 			", facility=<").append(getFacility() == null ? "\\0" : getFacility().serializeToString()).append(">").append(
 			", service=<").append(getService() == null ? "\\0" : getService().serializeToString()).append(">").append(
+			", blocked=<").append(isBlocked()).append(">").append(
 			']').toString();
 	}
 
@@ -108,6 +122,7 @@ public class RichDestination extends Destination implements Comparable<PerunBean
 			).append("', type='").append(super.getType()
 			).append("', facility='").append(getFacility()
 			).append("', service='").append(getService()
+			).append("', blocked='").append(isBlocked()
 			).append("']").toString();
 	}
 }
---
+++ perun-core/src/test/java/cz/metacentrum/perun/core/impl/ServicesManagerImplIntegrationTest.java
@@ -0,0 +1,138 @@
+package cz.metacentrum.perun.core.impl;
+
+import cz.metacentrum.perun.core.AbstractPerunIntegrationTest;
+import cz.metacentrum.perun.core.api.Destination;
+import cz.metacentrum.perun.core.api.Facility;
+import cz.metacentrum.perun.core.api.RichDestination;
+import cz.metacentrum.perun.core.api.Service;
+import cz.metacentrum.perun.core.implApi.ServicesManagerImplApi;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author Vojtech Sassmann <vojtech.sassmann@gmail.com>
+ */
+public class ServicesManagerImplIntegrationTest extends AbstractPerunIntegrationTest {
+
+	private static final String CLASS_NAME = "ServicesManagerImpl.";
+	private ServicesManagerImplApi servicesManagerImpl;
+
+	private Facility facility;
+	private Service service;
+	private Destination destination;
+
+	@Before
+	public void setUp() throws Exception {
+		servicesManagerImpl = (ServicesManagerImplApi) ReflectionTestUtils.getField(
+				perun.getServicesManagerBl(), "servicesManagerImpl"
+		);
+
+		setUpEntities();
+	}
+
+	@Test
+	public void getAllRichDestinationsCorrectDestinationDenial() throws Exception {
+		System.out.println(CLASS_NAME + "getAllRichDestinationsCorrectDestinationDenial");
+
+		servicesManagerImpl.blockServiceOnDestination(sess, service.getId(), destination.getId());
+
+		List<RichDestination> richDestinations = servicesManagerImpl.getAllRichDestinations(sess, facility);
+
+		hasCorrectRichDestinationWithServiceDenial(richDestinations);
+	}
+
+	@Test
+	public void getAllRichDestinationsCorrectFacilityDenial() throws Exception {
+		System.out.println(CLASS_NAME + "getAllRichDestinationsCorrectFacilityDenial");
+
+		servicesManagerImpl.blockServiceOnFacility(sess, service.getId(), facility.getId());
+
+		List<RichDestination> richDestinations = servicesManagerImpl.getAllRichDestinations(sess, facility);
+
+		hasCorrectRichDestinationWithoutServiceDenial(richDestinations);
+	}
+
+	@Test
+	public void getAllRichDestinationsForServiceCorrectDestinationDenial() throws Exception {
+		System.out.println(CLASS_NAME + "getAllRichDestinationsForServiceCorrectDestinationDenial");
+
+		servicesManagerImpl.blockServiceOnDestination(sess, service.getId(), destination.getId());
+
+		List<RichDestination> richDestinations = servicesManagerImpl.getAllRichDestinations(sess, service);
+
+		hasCorrectRichDestinationWithServiceDenial(richDestinations);
+	}
+
+	@Test
+	public void getRichDestinationsCorrectDestinationDenial() throws Exception {
+		System.out.println(CLASS_NAME + "getRichDestinationsCorrectDestinationDenial");
+
+		servicesManagerImpl.blockServiceOnDestination(sess, service.getId(), destination.getId());
+
+		List<RichDestination> richDestinations = servicesManagerImpl.getRichDestinations(sess, facility, service);
+
+		hasCorrectRichDestinationWithServiceDenial(richDestinations);
+	}
+
+	@Test
+	public void Foo() throws Exception {
+		servicesManagerImpl.blockServiceOnDestination(sess, service.getId(), destination.getId());
+		servicesManagerImpl.blockServiceOnFacility(sess, service.getId(), facility.getId());
+	}
+
+	private void setUpEntities() throws Exception {
+		setUpFacility();
+		setUpService();
+		setUpDestination();
+
+		servicesManagerImpl.addDestination(sess, service, facility, destination);
+	}
+
+	private void setUpDestination() {
+		destination = new Destination();
+		destination.setDestination("destination");
+		destination.setType(Destination.DESTINATIONHOSTTYPE);
+		destination = servicesManagerImpl.createDestination(sess, destination);
+	}
+
+	private void setUpFacility() throws Exception {
+		facility = new Facility();
+		facility.setName("TestFacility");
+		facility.setDescription("des");
+		facility = perun.getFacilitiesManagerBl().createFacility(sess, facility);
+	}
+
+	private void setUpService() {
+		service = new Service();
+		service.setName("Test service");
+		service = servicesManagerImpl.createService(sess, service);
+	}
+
+	private void hasCorrectRichDestinationWithServiceDenial(List<RichDestination> richDestinations) {
+		assertThat(richDestinations).hasSize(1);
+
+		RichDestination richDestination = richDestinations.get(0);
+
+		assertThat(richDestination.getId()).isEqualTo(destination.getId());
+		assertThat(richDestination.getFacility()).isEqualTo(facility);
+		assertThat(richDestination.getService()).isEqualTo(service);
+		assertThat(richDestination.isBlocked()).isTrue();
+	}
+
+	private void hasCorrectRichDestinationWithoutServiceDenial(List<RichDestination> richDestinations) {
+
+		assertThat(richDestinations).hasSize(1);
+
+		RichDestination richDestination = richDestinations.get(0);
+
+		assertThat(richDestination.getId()).isEqualTo(destination.getId());
+		assertThat(richDestination.getFacility()).isEqualTo(facility);
+		assertThat(richDestination.getService()).isEqualTo(service);
+		assertThat(richDestination.isBlocked()).isFalse();
+	}
+}
