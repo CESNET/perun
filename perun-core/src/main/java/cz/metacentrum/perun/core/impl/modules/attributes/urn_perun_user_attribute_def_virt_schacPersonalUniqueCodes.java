f1ab904bf8fb2b1ab8be459d623061cc45a1de68
Peter Balcirak <peter.balcirak@gmail.com>
2020-09-14 08:18:42 +0200

    New LDAP attribute schacPersonalUniqueCode
    - new attribute deffinition was added to the perun ldap schema.
    - attribute was reflected also in modules and ldapc component.

---
+++ perun-core/src/main/java/cz/metacentrum/perun/core/impl/modules/attributes/urn_perun_user_attribute_def_virt_schacPersonalUniqueCodes.java
@@ -0,0 +1,23 @@
+package cz.metacentrum.perun.core.impl.modules.attributes;
+
+import cz.metacentrum.perun.core.implApi.modules.attributes.SkipValueCheckDuringDependencyCheck;
+import cz.metacentrum.perun.core.implApi.modules.attributes.UserVirtualAttributeCollectedFromUserExtSource;
+
+/**
+ * All user's unique codes collected from UserExtSources attributes.
+ *
+ */
+@SkipValueCheckDuringDependencyCheck
+public class urn_perun_user_attribute_def_virt_schacPersonalUniqueCodes extends UserVirtualAttributeCollectedFromUserExtSource {
+
+	@Override
+	public String getSourceAttributeFriendlyName() {
+		return "schacPersonalUniqueCode";
+	}
+
+	@Override
+	public String getDestinationAttributeFriendlyName() {
+		return "schacPersonalUniqueCodes";
+	}
+
+}
