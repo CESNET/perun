f5a1bddd5911e7a3cd31d64ad756561c7ab0a0ee
HejdaJakub <kuba.hejda97@gmail.com>
2020-10-26 17:33:09 +0100

    Application auto rejection
    * I created two expiration attributes - one for group and one for VO.
    * I also created methods for auto rejection of expired applications.
    * Now is posible to set number of days to expiration of applications in each group and VO. If application is expired, then will be auto rejected.

---
+++ perun-core/src/main/java/cz/metacentrum/perun/core/impl/modules/attributes/urn_perun_vo_attribute_def_def_applicationExpirationRules.java
@@ -0,0 +1,49 @@
+package cz.metacentrum.perun.core.impl.modules.attributes;
+
+import cz.metacentrum.perun.core.api.Attribute;
+import cz.metacentrum.perun.core.api.AttributeDefinition;
+import cz.metacentrum.perun.core.api.AttributesManager;
+import cz.metacentrum.perun.core.api.Vo;
+import cz.metacentrum.perun.core.api.exceptions.WrongAttributeValueException;
+import cz.metacentrum.perun.core.api.exceptions.WrongReferenceAttributeValueException;
+import cz.metacentrum.perun.core.impl.PerunSessionImpl;
+import cz.metacentrum.perun.core.implApi.modules.attributes.AbstractApplicationExpirationRulesModule;
+import cz.metacentrum.perun.core.implApi.modules.attributes.VoAttributesModuleImplApi;
+
+import java.util.LinkedHashMap;
+
+/**
+ * @author Jakub Hejda <Jakub.Hejda@cesnet.cz>
+ */
+public class urn_perun_vo_attribute_def_def_applicationExpirationRules extends AbstractApplicationExpirationRulesModule<Vo> implements VoAttributesModuleImplApi {
+	@Override
+	public Attribute fillAttribute(PerunSessionImpl perunSession, Vo vo, AttributeDefinition attribute) {
+		return new Attribute(attribute);
+	}
+
+	@Override
+	public void checkAttributeSyntax(PerunSessionImpl perunSession, Vo vo, Attribute attribute) throws WrongAttributeValueException {
+		super.checkAttributeSyntax(perunSession, vo, attribute);
+	}
+
+	@Override
+	public void checkAttributeSemantics(PerunSessionImpl perunSession, Vo vo, Attribute attribute) throws WrongReferenceAttributeValueException {
+
+	}
+
+	@Override
+	public void changedAttributeHook(PerunSessionImpl session, Vo vo, Attribute attribute) {
+
+	}
+
+	@Override
+	public AttributeDefinition getAttributeDefinition() {
+		AttributeDefinition attr = new AttributeDefinition();
+		attr.setNamespace(AttributesManager.NS_VO_ATTR_DEF);
+		attr.setFriendlyName("applicationExpirationRules");
+		attr.setDisplayName("Application expiration rules");
+		attr.setType(LinkedHashMap.class.getName());
+		attr.setDescription("Rules which define when auto reject application to Vo.");
+		return attr;
+	}
+}
---
+++ perun-core/src/main/java/cz/metacentrum/perun/core/implApi/modules/attributes/AbstractApplicationExpirationRulesModule.java
@@ -0,0 +1,47 @@
+package cz.metacentrum.perun.core.implApi.modules.attributes;
+
+import cz.metacentrum.perun.core.api.Attribute;
+import cz.metacentrum.perun.core.api.PerunBean;
+import cz.metacentrum.perun.core.api.exceptions.WrongAttributeValueException;
+import cz.metacentrum.perun.core.impl.PerunSessionImpl;
+
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author Jakub Hejda <Jakub.Hejda@cesnet.cz>
+ */
+public abstract class AbstractApplicationExpirationRulesModule<T extends PerunBean> extends AttributesModuleAbstract implements AttributesModuleImplApi {
+	public static final String applicationWaitingForEmailVerificationKeyName = "emailVerification";
+	public static final String applicationIgnoredByAdminKeyName = "ignoredByAdmin";
+	private static final Pattern daysToExpirationPattern = Pattern.compile("^\\d+$");
+
+	public void checkAttributeSyntax(PerunSessionImpl sess, T entity, Attribute attribute) throws WrongAttributeValueException {
+
+		Map<String, String> attrValue = attribute.valueAsMap();
+
+		if (attrValue == null || attrValue.isEmpty()) return;
+
+		// If is not empty, so i will check if all keys and values are correct
+		for (Map.Entry<String, String> entry : attrValue.entrySet()) {
+			if (!isAllowedParameter(entry.getKey()))
+				throw new WrongAttributeValueException(attribute, "There is not allowed parameter value: " + entry.getKey());
+			Matcher daysToExpirationMatcher = daysToExpirationPattern.matcher(entry.getValue());
+			if (!daysToExpirationMatcher.find())
+				throw new WrongAttributeValueException(attribute, "There is not allowed value for parameter '" + entry.getKey() + "': " + entry.getValue());
+		}
+
+	}
+
+	/**
+	 * If parameter (key) is allowed in HashMap for applicationExpirationRules
+	 *
+	 * @param parameter String
+	 * @return true if parameter is allowed, false if is not
+	 */
+	private boolean isAllowedParameter(String parameter) {
+		if(parameter == null) return false;
+		return parameter.equals(applicationWaitingForEmailVerificationKeyName) || parameter.equals(applicationIgnoredByAdminKeyName);
+	}
+}
