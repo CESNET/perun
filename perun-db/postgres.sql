060e9934f6e3489b1e551f3c282364fdb50ff349
Vojtěch Sassmann <vojtech.sassmann@gmail.com>
2021-04-26 09:31:46 +0200

    Update postgres.sql

--- perun-db/postgres.sql
+++ perun-db/postgres.sql
@@ -1,4 +1,4 @@
--- database version 3.1.79 (don't forget to update insert statement at the end of file)
+-- database version 3.1.80 (don't forget to update insert statement at the end of file)
 
 -- VOS - virtual organizations
 create table vos (
