c66fdba909a396e77eba7aa5677308bbc35ca1f4
Lucie Kurečková <luckureckova@gmail.com>
2021-05-04 17:00:09 +0200

    Bumped development version to 3.24.0-SNAPSHOT (#3196)
    - Updated version in pom.xml
    - Updated CLI API version.
    - Updated OpenAPI version.

--- perun-cli/Perun/Agent.pm
+++ perun-cli/Perun/Agent.pm
@@ -1,6 +1,6 @@
 package Perun::Agent;
 my $agentVersionMajor = '3';
-my $agentVersionMinor = '23';
+my $agentVersionMinor = '24';
 my $agentVersionPatch = '0';
 my $agentVersion = $agentVersionMajor . "." . $agentVersionMinor . "." . $agentVersionPatch;
 
