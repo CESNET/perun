ce0de74c3392f945b796d78bc5dba559b06b8a7a
Johaney-s <73999257+Johaney-s@users.noreply.github.com>
2021-05-12 09:42:58 +0200

    Check number of embedded groups form items (#3193)
    * Maximum one application form item for embedded groups is allowed in VO application
    * Exception is thrown if multiple definitions are found when updating application

---
+++ perun-base/src/main/java/cz/metacentrum/perun/core/api/exceptions/MultipleApplicationFormItemsException.java
@@ -0,0 +1,36 @@
+package cz.metacentrum.perun.core.api.exceptions;
+
+
+/**
+ * Thrown when application form contains multiple application form items of such type that maximum 1 is allowed.
+ *
+ * @author Johana Supikova
+ */
+public class MultipleApplicationFormItemsException extends PerunException {
+	static final long serialVersionUID = 0;
+
+	/**
+	 * Simple constructor with a message
+	 * @param message message with details about the cause
+	 */
+	public MultipleApplicationFormItemsException(String message) {
+		super(message);
+	}
+
+	/**
+	 * Constructor with a message and Throwable object
+	 * @param message message with details about the cause
+	 * @param cause Throwable that caused throwing of this exception
+	 */
+	public MultipleApplicationFormItemsException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	/**
+	 * Constructor with a Throwable object
+	 * @param cause Throwable that caused throwing of this exception
+	 */
+	public MultipleApplicationFormItemsException(Throwable cause) {
+		super(cause);
+	}
+}
