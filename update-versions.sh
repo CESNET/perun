c727706db839db5da52e6b4d798af70dc66c1678
Vojtech Sassmann <vojtech.sassmann@gmail.com>
2021-08-08 10:26:50 +0200

    ci(githubci): update CLI version on release
    * The update-versions.sh script was altered so it also updates the CLI version.

--- update-versions.sh
+++ update-versions.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 sed -E -i "s/^  version: [0-9]+\.[0-9]+\.[0-9]+$/  version: $1/" perun-openapi/openapi.yml
+sed -E -i "s/[0-9]+\.[0-9]+\.[0-9]+/$1/" perun-cli/Perun/Agent.pm
 mvn versions:set -DnewVersion=$1
