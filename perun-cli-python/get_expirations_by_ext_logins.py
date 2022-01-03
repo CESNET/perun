68dd3e2f4ae6d44bb7c6e68a4c2fec479e031737
Martin Kuba <makub@ics.muni.cz>
2022-01-03 19:40:07 +0100

    feat(openapi): openAPI Generator now works for Python CLI
    OpenAPI Generator updated to version 5.3.1 which supports object inheritance, fixed definition of
    some schemas to allow null values

--- perun-cli-python/get_expirations_by_ext_logins.py
+++ perun-cli-python/get_expirations_by_ext_logins.py
@@ -15,21 +15,23 @@ def main(args):
 
 	f = open(args['file'], "r")
 	for x in f:
-		x = x.strip(' \n')
+		x = x.strip(' \n') + '@idp.e-infra.cz'
 		user = users_manager.get_user_by_ext_source_name_and_ext_login(ext_login=x,ext_source_name=args['extSourceName'])
 		pprint(user)
-		#TODO Python OpenAPI generator does not support inheritance yest :-(
-		# see https://github.com/OpenAPITools/openapi-generator/pull/4446
+
 		member = members_manager.get_member_by_user(vo=args['voId'], user=user.id)
 		pprint(member)
-		attr = attributes_manager.get_member_attribute_by_name(member.id,"urn:perun:member:attribute-def:def:membershipExpiration")
-		pprint(attr)
+
+		# Attribute.value can be a JSON object, a string, an integer, a list, but OpenAPI 3.0 does not allow this
+		# see https://stackoverflow.com/questions/48111459/how-to-define-a-property-that-can-be-string-or-null-in-openapi-swagger
+		# attr = attributes_manager.get_member_attribute_by_name(member.id,"urn:perun:member:attribute-def:def:membershipExpiration")
+		# pprint(attr)
 	f.close()
 
 # calling main when invoked from CLI
 if __name__ == "__main__":
 	options = options("Outputs member expirations in given VO.")
 	options.add_argument('-f', '--file', help='file with user logins', required=True)
-	options.add_argument('-v', '--voId', help='id of VO', default=20, required=False, type=int)
-	options.add_argument('-E', '--extSourceName', help='name of external source', required=False, default='https://login.cesnet.cz/idp/')
+	options.add_argument('-v', '--voId', help='id of VO', default=21, required=False, type=int)
+	options.add_argument('-E', '--extSourceName', help='name of external source', required=False, default='https://idp.e-infra.cz/idp/')
 	sys.exit(main(vars(options.parse_args())) or 0)
