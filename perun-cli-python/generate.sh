bc63e5e53e3efe94f0f91005af9ded57c8ea0acc
Martin Kuba <makub@ics.muni.cz>
2022-10-07 16:18:01 +0200

    build(openapi): upgrade OpenAPI Generator to version 6.2.0
    OpenAPI Generator version used in modules perun-openapi and perun-cli-python was updated to 6.2.0.

--- perun-cli-python/generate.sh
+++ perun-cli-python/generate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-GENERATOR_VERSION=6.0.1
+GENERATOR_VERSION=6.2.0
 if [ ! -f  "openapi-generator-cli-$GENERATOR_VERSION.jar" ] ; then
   wget https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/$GENERATOR_VERSION/openapi-generator-cli-$GENERATOR_VERSION.jar
 fi
@@ -9,12 +9,14 @@ rm -rf perun_openapi
 # see https://openapi-generator.tech/docs/usage#generate
 # and https://openapi-generator.tech/docs/generators/python
 java \
+ --add-opens java.base/java.util=ALL-UNNAMED \
+ --add-opens java.base/java.lang=ALL-UNNAMED \
  -DapiDocs=false \
  -DapiTests=false \
  -DmodelDocs=false \
  -DmodelTests=false \
  -jar openapi-generator-cli-$GENERATOR_VERSION.jar generate \
- --generator-name python \
+ --generator-name python-prior \
  --input-spec ../perun-openapi/openapi.yml \
  --model-package model \
  --additional-properties=generateSourceCodeOnly=true,packageName=perun_openapi
