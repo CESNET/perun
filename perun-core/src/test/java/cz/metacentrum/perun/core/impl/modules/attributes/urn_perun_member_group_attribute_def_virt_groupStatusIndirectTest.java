806073bbfc3bdd93bd6e947db794ba8e78df2ade
Matej Hakoš <matejhakos@gmail.com>
2022-10-17 12:39:52 +0200

    feat(core): status indirect urn * created urn, that returns if the member provided is INDIRECT * urn is initialized in AttributesManager

---
+++ perun-core/src/test/java/cz/metacentrum/perun/core/impl/modules/attributes/urn_perun_member_group_attribute_def_virt_groupStatusIndirectTest.java
@@ -0,0 +1,40 @@
+package cz.metacentrum.perun.core.impl.modules.attributes;
+
+import cz.metacentrum.perun.core.AbstractPerunIntegrationTest;
+import cz.metacentrum.perun.core.api.Attribute;
+import cz.metacentrum.perun.core.api.Group;
+import cz.metacentrum.perun.core.api.Member;
+import cz.metacentrum.perun.core.api.exceptions.WrongAttributeValueException;
+import cz.metacentrum.perun.core.impl.PerunSessionImpl;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.mockito.Mockito.mock;
+
+public class urn_perun_member_group_attribute_def_virt_groupStatusIndirectTest extends AbstractPerunIntegrationTest {
+	private static urn_perun_member_group_attribute_def_virt_groupStatusIndirect classInstance;
+	private static PerunSessionImpl session;
+	private static Attribute attributeToCheck;
+	@Before
+	public void setUp() throws Exception {
+		classInstance = new urn_perun_member_group_attribute_def_virt_groupStatusIndirect();
+		attributeToCheck = new Attribute(classInstance.getAttributeDefinition());
+		session = mock(PerunSessionImpl.class);
+	}
+	@Test
+	public void testCheckAttributeValueNull() throws Exception {
+		System.out.println("testCheckAttributeValueNull()");
+		attributeToCheck.setValue(null);
+
+		classInstance.checkAttributeSyntax(session, new Member(), new Group(), attributeToCheck);
+	}
+
+	@Test
+	public void testCheckAttributeValueCorrect() throws Exception {
+		System.out.println("testCheckAttributeValueCorrect()");
+
+		attributeToCheck.setValue(true);
+		classInstance.checkAttributeSyntax(session, new Member(), new Group(), attributeToCheck);
+	}
+
+}
