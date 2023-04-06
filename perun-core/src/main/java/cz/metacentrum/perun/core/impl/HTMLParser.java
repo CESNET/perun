a5d6adb412db34af940748f4fff763ae32d866f9
Johana Supíková <johana.supikova@seznam.cz>
2023-04-06 14:34:25 +0200

    fix(core): allow horizontal rule <hr> in html attributes

--- perun-core/src/main/java/cz/metacentrum/perun/core/impl/HTMLParser.java
+++ perun-core/src/main/java/cz/metacentrum/perun/core/impl/HTMLParser.java
@@ -18,7 +18,7 @@ import java.util.regex.Pattern;
  * @author: Matej Hakoš <492968@mail.muni.cz>
  */
 public class HTMLParser {
-	private static final String[] allowedTags = {"a", "article", "aside", "b", "blockquote", "br", "button", "caption", "center", "cite", "decorator", "del", "details", "div", "em", "footer", "h1", "h2", "h3", "h4", "h5", "h6", "header", "i", "img", "kbd", "label", "li", "ol", "p", "pre", "section", "select", "span", "strong", "sup", "table", "tbody", "td", "textarea", "tfoot", "th", "thead", "tr", "ul"};
+	private static final String[] allowedTags = {"a", "article", "aside", "b", "blockquote", "br", "button", "caption", "center", "cite", "decorator", "del", "details", "div", "em", "footer", "h1", "h2", "h3", "h4", "h5", "h6", "header", "hr", "i", "img", "kbd", "label", "li", "ol", "p", "pre", "section", "select", "span", "strong", "sup", "table", "tbody", "td", "textarea", "tfoot", "th", "thead", "tr", "ul"};
 	private static final String[] allowedAttributes = {"align", "class", "color", "disabled", "height", "hidden", "href", "id", "label", "size", "span", "src", "srcset", "style", "width"};
 	private static final String[] allowedStyles = {"color", "background-color", "font-size", "font-family", "text-align", "margin", "padding", "border", "width", "height", "display", "position", "top", "bottom", "left", "right", "overflow", "float", "clear", "z-index"};
 	private static final String[] allowedUrlProtocols = {"https", "mailto"};
