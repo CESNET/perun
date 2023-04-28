fe87987ec168032fff4b6c3cf320757826a7c43d
Peter Balčirák <peter.balcirak@ics.muni.cz>
2023-04-28 13:12:28 +0200

    feat(core): added bbmriid persistent modules
    Created new login namespace bbmriid. Values will be generated but user extSources won't be created.

---
+++ perun-core/src/main/java/cz/metacentrum/perun/core/impl/modules/attributes/urn_perun_user_attribute_def_def_login_namespace_bbmriid_persistent_shadow.java
@@ -0,0 +1,45 @@
+package cz.metacentrum.perun.core.impl.modules.attributes;
+
+import cz.metacentrum.perun.core.api.Attribute;
+import cz.metacentrum.perun.core.api.AttributeDefinition;
+import cz.metacentrum.perun.core.api.AttributesManager;
+import cz.metacentrum.perun.core.api.User;
+import cz.metacentrum.perun.core.impl.PerunSessionImpl;
+
+import java.util.Random;
+
+/**
+ *
+ * Class for filling bbmriid shadow value.
+ * It is only storage! Use module login bbmriid_persistent for access the value.
+ *
+ */
+public class urn_perun_user_attribute_def_def_login_namespace_bbmriid_persistent_shadow
+	extends urn_perun_user_attribute_def_def_login_namespace {
+	private final static String attrFriendlyName = "login-namespace:bbmriid-persistent-shadow";
+	private final static int lowestId = 2000;
+	private final static int randomBound = 10000;
+	private final static Random random = new Random();
+	@Override
+	public Attribute fillAttribute(PerunSessionImpl perunSession, User user, AttributeDefinition attribute) {
+
+		Attribute filledAttribute = new Attribute(attribute);
+
+		if (attribute.getFriendlyName().equals(attrFriendlyName)) {
+			String computedValue = Long.toString(System.nanoTime() + random.nextLong(lowestId, randomBound));
+			filledAttribute.setValue(computedValue);
+		}
+		return filledAttribute;
+	}
+
+	@Override
+	public AttributeDefinition getAttributeDefinition() {
+		AttributeDefinition attr = new AttributeDefinition();
+		attr.setNamespace(AttributesManager.NS_USER_ATTR_DEF);
+		attr.setFriendlyName(attrFriendlyName);
+		attr.setDisplayName("BBMRI Computed ID");
+		attr.setType(String.class.getName());
+		attr.setDescription("BBMRI Computed ID used in proxy and BBMRI services.");
+		return attr;
+	}
+}
