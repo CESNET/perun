113ec07fcc2e9586cd4d10b447290eac1ca70f08
Johana Supíková <johana.supikova@seznam.cz>
2024-02-28 12:20:43 +0100

    feat(core): new extSource type JSON
    * works with jsonPath queries
    * so far only group subjects method implemented

---
+++ perun-core/src/test/java/cz/metacentrum/perun/core/impl/modules/attributes/urn_perun_group_attribute_def_def_groupSynchronizationFilenameTest.java
@@ -0,0 +1,61 @@
+package cz.metacentrum.perun.core.impl.modules.attributes;
+
+import cz.metacentrum.perun.core.api.Attribute;
+import cz.metacentrum.perun.core.api.Group;
+import cz.metacentrum.perun.core.api.exceptions.WrongAttributeValueException;
+import cz.metacentrum.perun.core.impl.PerunSessionImpl;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.mockito.Mockito.RETURNS_DEEP_STUBS;
+import static org.mockito.Mockito.mock;
+
+public class urn_perun_group_attribute_def_def_groupSynchronizationFilenameTest {
+
+	private static PerunSessionImpl session;
+	private static urn_perun_group_attribute_def_def_groupSynchronizationFilename classInstance;
+	private static Group group;
+
+	@Before
+	public void setUp() {
+		classInstance = new urn_perun_group_attribute_def_def_groupSynchronizationFilename();
+		group = new Group();
+		session = mock(PerunSessionImpl.class, RETURNS_DEEP_STUBS);
+	}
+
+	@Test
+	public void testCheckAttributeSyntax() throws Exception {
+		System.out.println("testCheckAttributeSyntax()");
+
+		Attribute attributeToCheck = new Attribute();
+
+		attributeToCheck.setValue("filename.txt");
+		classInstance.checkAttributeSemantics(session, group, attributeToCheck);
+
+		attributeToCheck.setValue("filename.json");
+		classInstance.checkAttributeSemantics(session, group, attributeToCheck);
+
+		attributeToCheck.setValue("Random_FILENAME_123456");
+		classInstance.checkAttributeSemantics(session, group, attributeToCheck);
+	}
+
+	@Test(expected = WrongAttributeValueException.class)
+	public void testCheckAttributeSyntaxWithWrongValueRootDirectory() throws Exception {
+		System.out.println("testCheckAttributeSyntaxWithWrongValueRootDirectory()");
+
+		Attribute attributeToCheck = new Attribute();
+
+		attributeToCheck.setValue("/filename");
+		classInstance.checkAttributeSyntax(session, group, attributeToCheck);
+	}
+
+	@Test(expected = WrongAttributeValueException.class)
+	public void testCheckAttributeSyntaxWithWrongValueDirectoryTraversal() throws Exception {
+		System.out.println("testCheckAttributeSyntaxWithWrongValueDirectoryTraversal()");
+
+		Attribute attributeToCheck = new Attribute();
+
+		attributeToCheck.setValue("../filename");
+		classInstance.checkAttributeSyntax(session, group, attributeToCheck);
+	}
+}
