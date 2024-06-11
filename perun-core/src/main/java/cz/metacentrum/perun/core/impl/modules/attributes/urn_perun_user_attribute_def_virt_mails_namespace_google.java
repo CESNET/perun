5517970030672f034b857db04cd9fcaac1dbd7d7
Rastislav Krutak <492918@mail.muni.cz>
2024-06-11 10:43:55 +0200

    fix(core): mails_namespace attributes now extend userVirtualAttributeCollectedFromUes
    - changed attribute modules mails_namespace:(microsoft/google)
    - modules now correctly trigger propagations on relevant changes

--- perun-core/src/main/java/cz/metacentrum/perun/core/impl/modules/attributes/urn_perun_user_attribute_def_virt_mails_namespace_google.java
+++ perun-core/src/main/java/cz/metacentrum/perun/core/impl/modules/attributes/urn_perun_user_attribute_def_virt_mails_namespace_google.java
@@ -1,25 +1,17 @@
 package cz.metacentrum.perun.core.impl.modules.attributes;
 
-import cz.metacentrum.perun.core.api.Attribute;
+import cz.metacentrum.perun.audit.events.AttributesManagerEvents.AllAttributesRemovedForUserExtSource;
+import cz.metacentrum.perun.audit.events.AttributesManagerEvents.AttributeRemovedForUes;
+import cz.metacentrum.perun.audit.events.AttributesManagerEvents.AttributeSetForUes;
 import cz.metacentrum.perun.core.api.AttributeDefinition;
 import cz.metacentrum.perun.core.api.AttributesManager;
-import cz.metacentrum.perun.core.api.User;
 import cz.metacentrum.perun.core.api.UserExtSource;
-import cz.metacentrum.perun.core.api.exceptions.AttributeNotExistsException;
-import cz.metacentrum.perun.core.api.exceptions.InternalErrorException;
-import cz.metacentrum.perun.core.api.exceptions.WrongAttributeAssignmentException;
 import cz.metacentrum.perun.core.impl.PerunSessionImpl;
 import cz.metacentrum.perun.core.implApi.modules.attributes.SkipValueCheckDuringDependencyCheck;
-import cz.metacentrum.perun.core.implApi.modules.attributes.UserVirtualAttributesModuleAbstract;
-import cz.metacentrum.perun.core.implApi.modules.attributes.UserVirtualAttributesModuleImplApi;
+import cz.metacentrum.perun.core.implApi.modules.attributes.UserVirtualAttributeCollectedFromUserExtSource;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.util.function.Predicate;
 
 /**
  * Checks specified users mails in google namespace.
@@ -27,19 +19,15 @@ import org.slf4j.LoggerFactory;
  * @author Michal Stava  &lt;stavamichal@gmail.com&gt;
  */
 @SkipValueCheckDuringDependencyCheck
-public class urn_perun_user_attribute_def_virt_mails_namespace_google extends UserVirtualAttributesModuleAbstract
-    implements UserVirtualAttributesModuleImplApi {
+public class urn_perun_user_attribute_def_virt_mails_namespace_google extends
+    UserVirtualAttributeCollectedFromUserExtSource {
 
-  private static final Logger LOG =
-      LoggerFactory.getLogger(urn_perun_user_attribute_def_virt_mails_namespace_google.class);
   private static final String EXTSOURCE = "https://login.cesnet.cz/google-idp/";
-  private static final Pattern pattern = Pattern.compile("^.+[@]google[.]extidp[.]cesnet[.]cz$");
-  private static final String A_UES_MAIL = AttributesManager.NS_UES_ATTR_DEF + ":mail";
 
   @Override
   public AttributeDefinition getAttributeDefinition() {
     AttributeDefinition attr = new AttributeDefinition();
-    attr.setNamespace(AttributesManager.NS_USER_FACILITY_ATTR_VIRT);
+    attr.setNamespace(AttributesManager.NS_USER_ATTR_VIRT);
     attr.setFriendlyName("mails-namespace:google");
     attr.setDisplayName("Emails in namespace:google");
     attr.setType(ArrayList.class.getName());
@@ -48,35 +36,52 @@ public class urn_perun_user_attribute_def_virt_mails_namespace_google extends Us
   }
 
   @Override
-  public Attribute getAttributeValue(PerunSessionImpl sess, User user, AttributeDefinition attributeDefinition) {
-    Set<String> googleMails = new HashSet<>();
-    List<UserExtSource> userExtSources = sess.getPerunBl().getUsersManagerBl().getUserExtSources(sess, user);
+  public String getDestinationAttributeFriendlyName() {
+    return "mails-namespace:google";
+  }
 
-    for (UserExtSource ues : userExtSources) {
-      if (ues.getExtSource() != null && EXTSOURCE.equals(ues.getExtSource().getName())) {
-        String login = ues.getLogin();
-        if (login != null && !login.isEmpty()) {
-          Matcher matcher = pattern.matcher(login);
-          if (matcher.matches()) {
-            try {
-              Attribute attribute = sess.getPerunBl().getAttributesManagerBl().getAttribute(sess, ues, A_UES_MAIL);
-              if (attribute.getValue() != null) {
-                googleMails.add((String) attribute.getValue());
-              }
-            } catch (AttributeNotExistsException ex) {
-              //This should not happen, but if yes, skip this value
-              LOG.error("Attribute {} not exists in Perun so value {} google mails can't be get correctly.", A_UES_MAIL,
-                  user);
-            } catch (WrongAttributeAssignmentException ex) {
-              throw new InternalErrorException(ex);
-            }
-          }
-        }
+  @Override
+  public String getSourceAttributeFriendlyName() {
+    return "mail";
+  }
+
+  @Override
+  public List<AttributeHandleIdentifier> getHandleIdentifiers() {
+    List<AttributeHandleIdentifier> handleIdentifiers = new ArrayList<>();
+    handleIdentifiers.add(auditEvent -> {
+      if (auditEvent instanceof AllAttributesRemovedForUserExtSource &&
+              ((AllAttributesRemovedForUserExtSource) auditEvent).getUserExtSource().getExtSource().getName()
+                  .equals(EXTSOURCE)) {
+        return ((AllAttributesRemovedForUserExtSource) auditEvent).getUserExtSource().getUserId();
+      } else {
+        return null;
       }
-    }
+    });
+    handleIdentifiers.add(auditEvent -> {
+      if (auditEvent instanceof AttributeSetForUes &&
+              ((AttributeSetForUes) auditEvent).getAttribute().getFriendlyName()
+                  .equals(getSourceAttributeFriendlyName()) &&
+              ((AttributeSetForUes) auditEvent).getUes().getExtSource().getName().equals(EXTSOURCE)) {
+        return ((AttributeSetForUes) auditEvent).getUes().getUserId();
+      } else {
+        return null;
+      }
+    });
+    handleIdentifiers.add(auditEvent -> {
+      if (auditEvent instanceof AttributeRemovedForUes &&
+              ((AttributeRemovedForUes) auditEvent).getAttribute().getFriendlyName()
+                  .equals(getSourceAttributeFriendlyName()) &&
+              ((AttributeRemovedForUes) auditEvent).getUes().getExtSource().getName().equals(EXTSOURCE)) {
+        return ((AttributeRemovedForUes) auditEvent).getUes().getUserId();
+      } else {
+        return null;
+      }
+    });
+    return handleIdentifiers;
+  }
 
-    Attribute attribute = new Attribute(attributeDefinition);
-    attribute.setValue(new ArrayList<>(googleMails));
-    return attribute;
+  @Override
+  public Predicate<UserExtSource> getExtSourceFilter(PerunSessionImpl sess) {
+    return ues -> (ues != null && EXTSOURCE.equals(ues.getExtSource().getName()));
   }
 }
