ebc2babc79c5f7dd21105f6f4c2e66541780533f
David Flor <493294@muni.cz>
2025-04-01 10:57:56 +0200

    refactor(core): clean up commented code in preferredMail module

--- perun-core/src/main/java/cz/metacentrum/perun/core/impl/modules/attributes/urn_perun_user_attribute_def_def_preferredMail.java
+++ perun-core/src/main/java/cz/metacentrum/perun/core/impl/modules/attributes/urn_perun_user_attribute_def_def_preferredMail.java
@@ -41,72 +41,7 @@ public class urn_perun_user_attribute_def_def_preferredMail extends UserAttribut
     if (!emailMatcher.find()) {
       throw new WrongAttributeValueException(attribute, user, "Email is not in correct form.");
     }
-
-    /* User preferredMail now can be anything
-        //user prefferedMail can be only one of memberMails if any
-        List<Member> membersOfUser = sess.getPerunBl().getMembersManagerBl().getMembersByUser(sess, user);
-        StringBuilder possiblePrefferedMailValues = new StringBuilder();
-        for(Member m: membersOfUser) {
-        Attribute memberMail = null;
-        try {
-        memberMail = sess.getPerunBl().getAttributesManagerBl().getAttribute(sess, m, A_M_mail);
-        } catch (AttributeNotExistsException ex) {
-        throw new ConsistencyErrorException(ex);
-        } catch (WrongAttributeAssignmentException ex) {
-        throw new InternalErrorException(ex);
-        }
-
-        if(attribute.getValue().equals(memberMail.getValue())) return;
-        if(memberMail != null) {
-        if(possiblePrefferedMailValues.length() != 0) possiblePrefferedMailValues.append(", ");
-        possiblePrefferedMailValues.append("'" + memberMail.getValue() + "'");
-        }
-        }
-        throw new WrongAttributeValueException("Attribute user preffered mail can be null (if no members mail exists)
-        or one of the existing member's mails [" + possiblePrefferedMailValues.toString() + "]. " + attribute);
-        */
   }
-
-  /* Not needed now this funcionality
-         @Override
-         public void changedAttributeHook(PerunSessionImpl session, User user, Attribute attribute) throws
-         InternalErrorException, WrongReferenceAttributeValueException {
-//If this is removing and userPrefferedMail is going to be null, try to get one of the member mail and set it to user
- Preffered Mail
-if(attribute.getValue() == null) {
-List<Member> membersOfUser = session.getPerunBl().getMembersManagerBl().getMembersByUser(session, user);
-
-String anyNotNullMemberMail = null;
-Attribute memberMail = null;
-for(Member m: membersOfUser) {
-try {
-memberMail = session.getPerunBl().getAttributesManagerBl().getAttribute(session, m, A_M_mail);
-} catch (AttributeNotExistsException ex) {
-throw new ConsistencyErrorException(ex);
-} catch (WrongAttributeAssignmentException ex) {
-throw new InternalErrorException(ex);
-}
-if(memberMail.getValue() != null) {
-anyNotNullMemberMail = (String) memberMail.getValue();
-break;
-}
-}
-
-//if there is no notNull memberMail so this email can't be set
-if(anyNotNullMemberMail == null) return;
-else {
-attribute.setValue(anyNotNullMemberMail);
-try {
-session.getPerunBl().getAttributesManagerBl().setAttribute(session, user, attribute);
-} catch (WrongAttributeValueException ex) {
-throw new WrongReferenceAttributeValueException(attribute, memberMail, "Member mail is not in correct form to save it
- into user prefferedMail.", ex);
-} catch (WrongAttributeAssignmentException ex) {
-throw new InternalErrorException(ex);
-}
-}
-}
-}*/
 
   @Override
   public AttributeDefinition getAttributeDefinition() {
